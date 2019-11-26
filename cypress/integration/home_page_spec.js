cdd0c4ad56263c11e1c9f3998ad703868f20df3f
Lena Krasnova <olkras03@gmail.com>
2019-11-26 16:29:58 +0000

    failing feeature test for user signup

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,6 +1,14 @@
-describe('Home page', function() {
-  it('has a title', function() {
-    cy.visit('/');
-    cy.get('.title').should('contain', 'Acebook');
+describe("Home page", function() {
+  it("has a title", function() {
+    cy.visit("/");
+    cy.get(".title").should("contain", "Acebook");
+  });
+
+  it("user can sign up", function() {
+    cy.visit("/");
+    cy.get('input[name="Terry"]').then($text => {
+      const txt = $text.text();
+      cy.log(txt);
+    });
   });
 });
