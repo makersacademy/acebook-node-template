d6d69dab04a21ecdb995111f792a08b3b9610827
Lena Krasnova <olkras03@gmail.com>
2019-11-26 16:06:36 +0000

    unit test to check if it can save a user

--- spec/models/user.spec.js
+++ spec/models/user.spec.js
@@ -1,29 +1,56 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
-require('../mongodb_helper')
-var User = require('../../models/user');
+require("../mongodb_helper");
+var User = require("../../models/user");
 
-describe('User model', function() {
-    beforeEach(function(done) {
-        mongoose.connection.collections.users.drop(function() {
-            done();
-        });
+describe("User model", function() {
+  beforeEach(function(done) {
+    mongoose.connection.collections.users.drop(function() {
+      done();
+    });
+  });
+
+  it("has a name, email and dob", () => {
+    const user = new User({
+      name: "Terry Wogan",
+      email: "terry@wogan.com",
+      dob: "03/08/1938"
     });
 
-    it('has a name, email and dob', () => {
-        const user = new User({ name: "Terry Wogan", email: "terry@wogan.com", dob: "03/08/1938"});
+    expect(user.name).toEqual("Terry Wogan");
+    expect(user.email).toEqual("terry@wogan.com");
+    expect(user.dob).toEqual("03/08/1938");
+  });
 
-        expect(user.name).toEqual("Terry Wogan");
-        expect(user.email).toEqual("terry@wogan.com");
-        expect(user.dob).toEqual("03/08/1938");
+  it("can list all users", function(done) {
+    User.find(function(err, users) {
+      expect(err).toBeNull();
+      expect(users).toEqual([]);
+      done();
+    });
+  });
+
+  it("can save a user", function(done) {
+    const user = new User({
+      name: "Terry Wogan",
+      email: "terry@wogan.com",
+      dob: "03/08/1938"
     });
 
-    it('can list all users', function(done) {
-        User.find(function(err, users) {
-          expect(err).toBeNull();
-          expect(users).toEqual([]);
-          done();
+    user.save(function(err) {
+      expect(err).toBeNull();
+
+      User.find(function(err, users) {
+        expect(err).toBeNull();
+
+        expect(users[0]).toMatchObject({
+          name: "Terry Wogan",
+          email: "terry@wogan.com",
+          dob: "03/08/1938"
         });
+
+        done();
       });
-
+    });
+  });
 });
