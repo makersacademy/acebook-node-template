3d730473718bf649b826046d1106cffa9e20761d
Rob Baile <robbaile12@outlook.com>
2019-11-27 15:10:02 +0000

    added test to check only one user is added

--- spec/models/user.spec.js
+++ spec/models/user.spec.js
@@ -5,21 +5,25 @@ require("../mongodb_helper");
 var User = require("../../models/user");
 
 describe("User model", function() {
+  var user;
+  var salt = bcrypt.genSaltSync(10);
+  var passwordHash = bcrypt.hashSync("pudsey", salt);
+
+
   beforeEach(function(done) {
     mongoose.connection.collections.users.drop(function() {
       done();
+    });    
+    user = new User({
+      firstName: "Terry",
+      surname: "Wogan",
+      email: "terry@wogan.com",
+      password: passwordHash,
+      dob: "03/08/1938"
     });
   });
 
   it("has a First name, surname, email, password and dob", () => {
-    const user = new User({
-      firstName: "Terry",
-      surname: "Wogan",
-      email: "terry@wogan.com",
-      password: "pudsey",
-      dob: "03/08/1938"
-    });
-
     expect(user.firstName).toEqual("Terry");
     expect(user.surname).toEqual("Wogan");
     expect(user.email).toEqual("terry@wogan.com");
@@ -35,16 +39,6 @@ describe("User model", function() {
   });
 
   it("can save a user", function(done) {
-    var salt = bcrypt.genSaltSync(10);
-    var passwordHash = bcrypt.hashSync("pudsey", salt);
-    const user = new User({
-      firstName: "Terry",
-      surname: "Wogan",
-      email: "terry@wogan.com",
-      password: passwordHash,
-      dob: "03/08/1938"
-    });
-
     user.save(function(err) {
       expect(err).toBeNull();
 
@@ -59,8 +53,34 @@ describe("User model", function() {
         });
 
         expect(bcrypt.compareSync("pudsey", users[0].password)).toBe(true);
+        expect(users.length).toBe(1)
         done();
       });
     });
   });
+
+  it("wont save user twice", function(done) {
+    const user2 = new User({
+      firstName: "Terry",
+      surname: "Wogan",
+      email: "terry@wogan.com",
+      password: passwordHash,
+      dob: "03/08/1938"
+    });
+
+    user.save(function(err) {
+      expect(err).toBeNull();
+    });
+
+    user2.save(function(err) {
+      console.log(err)
+      User.find(function(err, users) {
+        expect(err).toBeNull();
+        expect(users.length).toEqual(1);
+        done();
+      });
+    });
+  });
+
+  
 });
