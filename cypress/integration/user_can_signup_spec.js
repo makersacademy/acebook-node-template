baf295bc239fb2377ea186a01b45dbaf6fb82634
Lena Krasnova <olkras03@gmail.com>
2019-11-28 12:12:20 +0000

    fixed before each for cypress testing

--- cypress/integration/user_can_signup_spec.js
+++ cypress/integration/user_can_signup_spec.js
@@ -2,32 +2,26 @@ require("../../spec/mongodb_dropusers");
 
 describe('Signup', function() {
     beforeEach(function () {
-        cy.task('npm run db:reset')
+        cy.exec('npm run db:reset')
     })
     it('can submit sign up and show success', function() {
         cy.visit('/');
-  
         cy
             .get('input[name="firstName"]')
             .type('terry')
             .should("have.value", "terry");
-        
         cy
             .get('input[name="surname"]')
             .type('wogan')
             .should("have.value", "wogan");
-
         cy
             .get('input[name="email"]')
             .type("terry@wogan.com")
             .should("have.value", "terry@wogan.com");
-
         cy
             .get('input[name="password"]')
             .type('pudsey')
             .should("have.value", "pudsey");
-
-
         cy
             .get('input[name="dob"]')
             .type('03/08/1938')
@@ -36,5 +30,58 @@ describe('Signup', function() {
         cy.get('#submit-button').click();
   
         cy.contains('success');
+    });
+    it('doesn\'t allow you to sign up twice with the same user details', () => {
+        cy.visit('/');
+        cy
+            .get('input[name="firstName"]')
+            .type('terry')
+            .should("have.value", "terry");
+        cy
+            .get('input[name="surname"]')
+            .type('wogan')
+            .should("have.value", "wogan");
+        cy
+            .get('input[name="email"]')
+            .type("terry@wogan.com")
+            .should("have.value", "terry@wogan.com");
+        cy
+            .get('input[name="password"]')
+            .type('pudsey')
+            .should("have.value", "pudsey");
+        cy
+            .get('input[name="dob"]')
+            .type('03/08/1938')
+            .should("have.value", "03/08/1938");
+
+        cy.get('#submit-button').click();
+  
+        cy.contains('success');
+
+        cy.visit('/');
+        cy
+            .get('input[name="firstName"]')
+            .type('barry')
+            .should("have.value", "barry");
+        cy
+            .get('input[name="surname"]')
+            .type('white')
+            .should("have.value", "white");
+        cy
+            .get('input[name="email"]')
+            .type("terry@wogan.com")
+            .should("have.value", "terry@wogan.com");
+        cy
+            .get('input[name="password"]')
+            .type('pudsey')
+            .should("have.value", "pudsey");
+        cy
+            .get('input[name="dob"]')
+            .type('03/08/1938')
+            .should("have.value", "03/08/1938");
+
+        cy.get('#submit-button').click();
+  
+        cy.contains('Error. User already exist');
     });
 });
