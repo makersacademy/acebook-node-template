b757edba267756955c44451b2c6a3d45efac28c0
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-11-29 15:48:00 +0000

    some tweaks with cookie funcitonality

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,8 @@
 var HomeController = {
   Index: function(req, res) {
+    if(req.cookie.userId) {
+      res.redirect("/posts");
+    }
     res.render('home/index', { title: 'Acebook' });
   }
 };
