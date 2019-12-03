b74ef4e1cd648641f48ee662686783e5ae1233db
Rob Baile <robbaile12@outlook.com>
2019-12-03 13:52:07 +0000

    added master into branch

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,7 @@ var UserController = require('../controllers/user')
 router.post('/', UserController.Create);
 router.post('/login', UserController.Index);
 router.get('/', UserController.All);
+router.get('/:id', UserController.Profile);
 router.get('/logout', UserController.LogOut);
 
 module.exports = router;
