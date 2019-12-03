c0df8f3cbd771372eeecef6301e2cc5452c3b808
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-12-03 09:24:32 +0000

    conflicts

--- models/post.js
+++ models/post.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var PostSchema = new mongoose.Schema({
   message: String,
   userId: {type: mongoose.Schema.Types.ObjectId},
+  user: String,
   timeDate: String,
 });
 
