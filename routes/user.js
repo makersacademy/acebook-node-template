821c88b94446587bdc076973abc9c2d8fac233b8
Rob Baile <robbaile12@outlook.com>
2019-12-03 13:50:01 +0000

    merged master into branch

--- routes/user.js
+++ routes/user.js
@@ -4,15 +4,9 @@ var router = express.Router();
 var UserController = require('../controllers/user')
 
 router.post('/', UserController.Create);
-<<<<<<< HEAD
 router.post('/login', UserController.Index);
 router.get('/', UserController.All);
 router.get('/logout', UserController.LogOut);
-=======
-router.post('/login', UserController.Index)
-router.get('/logout', UserController.LogOut)
-router.get('/:id', UserController.Profile)
->>>>>>> master
 
 module.exports = router;
 
