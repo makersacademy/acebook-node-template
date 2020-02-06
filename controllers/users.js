0c3ba160ace16bfe89928c8260b02fa967418afe
Max O'Sullivan <maxosully@gmail.com>
2020-02-06 15:22:28 +0000

    finished log out

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt')
+const cookies = require('cookie-parser')
 // const popupS = require('popups')
 
 
@@ -13,11 +14,9 @@ var UsersController = {
     Create: function(req, res) {
 
         if (req.body.password === req.body.repeat_password) {
-           
-       
 
             let passwordHash = bcrypt.hashSync(req.body.password, 10); // encrypts password using bcrypt module
-            
+
             var users = new Users ({ // creates a new instance of users with the text, with respeats to the form that is created in the model &view
 
                 fullname: req.body.fullname,
@@ -25,32 +24,51 @@ var UsersController = {
                 username: req.body.username,
                 password: passwordHash
 
-            });  
-
-            
-            users.save(function(err) { // saves the new post
-                if (err) { throw err; } 
-                
-                res.status(201).redirect('/users/login');      // if it works show you the list of posts 
-        
             });
 
-        } else { 
+            users.save(function(err) { // saves the new post
+                if (err) { throw err; }
+
+                res.status(201).redirect('/users/login');      // if it works show you the list of posts
+
+            });
+
+        } else {
                 // document.getElementById('demo').innerHTML = "You have entered the wrong password!";
             //     popupS.window({
             //         mode: 'alert',
             //         content: "Your password did not Match"
-            //     });  
-                res.redirect('users/register');
-            } 
-    
-    
+            //     });
+                res.redirect('/users/register');
+            }
     },
-    
+
     Login: function(req, res) {
       res.render('users/login', {});  // render the user index view folder
     },
-     
+
+    Authenticate: function(req, res) {
+      Users.findOne({ email: req.body.email }, function(err, user) {
+        if(err) {throw err;}
+
+        if(user) {
+          if(bcrypt.compareSync(req.body.password, user.password)) {
+            res.cookie('username', user.username)            // getting cookie for the current logged in user
+            var username = req.cookies['username'];     // sets username variable to the cookies username
+            res.redirect('/posts')
+          }
+          else {
+            res.redirect('/users/login');
+          };
+        }
+      })
+    },
+
+    Logout: function(req, res) {
+      res.clearCookie('username')
+      var check = req.cookies['username'];
+      res.redirect('/')
+    }
 };
 
   module.exports = UsersController;
