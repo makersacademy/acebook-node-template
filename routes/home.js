010c9e31593e113d79b383f6ecd81d2117f63c67
Julia Jones <j.jones.13@ucl.ac.uk>
2020-02-07 16:19:18 +0000

    This the erros page updated

--- routes/home.js
+++ routes/home.js
@@ -5,6 +5,4 @@ var HomeController = require('../controllers/home')  // links to the home contro
 
 router.get('/', HomeController.Index);  // get request, return the HomeController code
 
-router.get('/error', HomeController.Error); // This will take you to home/error (as this is requiredin the controller)- This is now the linke to the home errors page.
-
 module.exports = router;  // the connection to the web framework
