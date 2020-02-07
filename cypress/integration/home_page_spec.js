6ee0a89e58ea44b23d10829bd4aa62c3d50747ed
Callum McDoiugall <cal-mcd@outlook.com>
2020-02-07 11:18:05 +0000

    adding two feature tests to access sign up page, both passing

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -3,4 +3,10 @@ describe('Home page', function() {
     cy.visit('/');
     cy.get('.title').should('contain', 'Acebook');
   });
+
+  it('can visit the sign up page through the link', function() {
+    cy.visit('/');
+    cy.get('.signup-link').click();
+    cy.get('.signup-title').should('contain', 'Sign up to AceBook');
+  });
 });
---
+++ cypress/integration/sign_up_page.js
@@ -0,0 +1,6 @@
+describe('Signup page', function() {
+  it('can see the sign up page', function() {
+    cy.visit('/signup');
+    cy.get('.signup-title').should('contain', 'Sign up to AceBook');
+  });
+});
