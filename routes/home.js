2a0d3b20d5565600619cf0e6b248b72349e6e53f
Julia Jones <j.jones.13@ucl.ac.uk>
2020-02-07 16:14:06 +0000

    This is the new errors page

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var HomeController = require('../controllers/home');  // links to the home controller 
+var HomeController = require('../controllers/home')  // links to the home controller 
 
 router.get('/', HomeController.Index);  // get request, return the HomeController code
+
+router.get('/error', HomeController.Error); // This will take you to home/error (as this is requiredin the controller)- This is now the linke to the home errors page.
 
 module.exports = router;  // the connection to the web framework
