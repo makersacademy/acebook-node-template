2a66331dd5628d926a4a6f35358137a730068730
Julia Jones <j.jones.13@ucl.ac.uk>
2020-02-12 13:50:54 +0000

    this is the the friend request link that we created earlier today.

--- public/javascripts/toggle.js
+++ public/javascripts/toggle.js
@@ -7,3 +7,7 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+function change() {
+    document.getElementById("myButton1").value="Friend request sent"; 
+}
