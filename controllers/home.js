368e06a45233def4d2a2668c0ee1932a7689f788
Iryna Jurkiv <iryna.jurkiv@gmail.com>
2020-02-13 14:38:38 +0000

    added comments to code

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,5 @@
-var HomeController = {      // class
-  Index: function(req, res) {     // function / method
+var HomeController = {      // this refers to the class
+  Index: function(req, res) {     // this refers to function/method within this class
     res.render('home/index', { title: 'Acebook' });  // render the home index from the folder and pass through 'Acebook' as a value so it can read it from the key
   }
 };
