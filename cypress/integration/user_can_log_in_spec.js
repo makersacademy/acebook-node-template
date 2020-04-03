ad535970d61ae468f2184b335a717c793e0302b0
Aysha Babur <ayshababur@yahoo.com>
2020-04-03 14:01:31 +0100

    login working

--- cypress/integration/user_can_log_in_spec.js
+++ cypress/integration/user_can_log_in_spec.js
@@ -1,7 +1,18 @@
 describe('Log in', function(){
   it('has a fields to log in', function(){
-    cy.visit('/');
-    cy.get('#username').find('[type="text"]').type('sheep');
-    cy.get('#username').contains('sheep');
+    cy.get('input[name=username]').type('jane.lane')
+    cy.get('input[name=password]').type('password')
+    cy.get('form').submit()
   });
-});
+  });
+
+
+    //cy.visit('/');
+    //cy.contains('Log in').click();
+    //cy.get('#username').find('[type="text"]').type('sheep');
+    //cy.get('#username').find('[type="text"]').type('password');
+    //cy.get('#username').type('sheep');
+    //expect(cy.get('#username').submit());
+    //cy.get('.posts2').should('contain', 'Hello, world!');
+  //});
+//});
