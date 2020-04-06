dcaae709efce06a76903059056ee41ac4a5119f2
Student <student@makerss-air-2.home>
2020-04-06 16:11:23 +0100

    user.spec test it has a password fail

--- spec/models/user.spec.js
+++ spec/models/user.spec.js
@@ -3,16 +3,20 @@ var mongoose = require('mongoose');
 require('../mongodb_helper')
 var User = require('../../models/user');
 
-describe('User model', function() {
-  beforeEach(function(done) {
-      mongoose.connection.collections.users.drop(function() {
+describe('User model', () => {
+  beforeEach((done) => {
+      mongoose.connection.collections.users.drop(() => {
           done();
       });
   });
-  it('has a user', function() {
+  it('has a user', () => {
     var user = new User({ username: 'derpygary'});
     expect(user.username).toEqual('derpygary');
   });
 
+  it('has a password', () => {
+    var user = new User({ password: 'qwerty'});
+    expect(user.password).toEqual('derp');
+  });
 
 });
