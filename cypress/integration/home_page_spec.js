d975fb8fe2f05763a345b85e3388939a0dc7928c
Olivia Thetgyi <othetgyi@gmail.com>
2020-04-08 21:23:24 +0100

    Making login branch match master branch

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,6 +1,7 @@
 describe('Home page', function() {
   it('has a title', function() {
     cy.visit('/');
-    cy.get('.title').should('contain', 'Acebook');
+    cy.get('.title').should('contain', 'WTfacebook');
   });
+  
 });
--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -1,6 +1,6 @@
 describe('Sign up', function(){
   it('has a fields to sign up', function(){
-    cy.visit('/user');
+    cy.visit('/user/signup');
     cy.contains('Sign up').click();
     cy.get('input[name=username]').type('jane.lane')
     cy.get('input[name=password]').type('password')
