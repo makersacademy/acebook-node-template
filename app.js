70fc6891741fdbfa1b5ffa395127ac947ad04bfd
Olivia Thetgyi <othetgyi@gmail.com>
2020-04-09 14:42:22 +0100

    Cleaned up styling

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname + 'public')));
 
 app.use(session({
   secret: 'secret',
