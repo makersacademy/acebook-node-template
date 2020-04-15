9dad6e5900011907d460d6ccb7ae45f7c425b15c
Hodan <hodanahmed0000@gmail.com>
2020-04-15 11:25:55 +0100

    created a passing jest test for creating a user

--- spec/models/user.spec.js
+++ spec/models/user.spec.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 require('../mongodb_helper')
-var User = require('../../models/post');
+var User = require('../../models/user');
 
 describe('User model', function() {
   beforeEach(function(done) {
@@ -15,20 +15,5 @@ describe('User model', function() {
     expect(user.name).toEqual('user1');
     expect(user.email).toEqual('user1@gmail.com');
     expect(user.password).toEqual('321');
-  });
-
-  it('can save new user details', function(done) {
-    var user = new User({ name: 'user1', email: 'user1@gmail.com', password: '321'});
-
-    user.save(function(err) {
-      expect(err).toBeNull();
-
-      User.find(function(err, user) {
-        expect(err).toBeNull();
-
-        expect(users[0]).toMatchObject({ name: 'user1', email: 'user1@gmail.com', password: '321' });
-        done();
-      });
-    });
   });
 });
