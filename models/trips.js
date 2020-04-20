0fc8808b4cca2484c3820d2cb8004cb044508124
Olivia Thetgyi <othetgyi@gmail.com>
2020-04-20 11:03:19 +0100

    Fixed AddFlight function

--- models/trips.js
+++ models/trips.js
@@ -1,22 +1,22 @@
 var mongoose = require('mongoose');
 
 //FlightSchema is the child schema of TripSchema
-var FlightSchema = new mongoose.Schema({
-  departureCity: String,
-  airport: String,
-  airline: String,
-  date: Date,
-  flightNumber: String,
-  departureTime: String,
-  bookingReference: String
-})
+// var FlightSchema = new mongoose.Schema({
+//   departureCity: String,
+//   airport: String,
+//   airline: String,
+//   date: Date,
+//   flightNumber: String,
+//   departureTime: String,
+//   bookingReference: String
+// })
 
 var TripSchema = new mongoose.Schema({
   username: String,
   destination: String,
   startDate: Date,
   endDate: Date,
-  flights: [FlightSchema]
+  flights: Array
 });
 
 var Trip = mongoose.model('Trip', TripSchema);
