3ebd6785667494a1b49feca9741300d5aecdafdd
Edmond <edmond.b@hotmail.co.uk>
2020-06-05 10:12:15 +0100

    Removed comments

--- controllers/home.js
+++ controllers/home.js
@@ -6,38 +6,3 @@ var HomeController = {
 };
 
 module.exports = HomeController;
-
-
-
-
-
-
-// req.session.user_id = 1 (Fry)
-// users = arr of hash
-
-
-
-
-
-// redirect '/' if User.current_user == nil
-
-      // var id = req.session.user_id
-      // var name = ""
-
-      // for(let index = 0; index < users.length; index++){
-      //   if(users[index]._id === id){
-      //     return name = users[index].firstName;
-      //   }
-      //   return name
-      // }
-
-
-
-      // var id = req.session.user_id
-      // var name = ""
-
-      // for(let index = 0; index < users.length; index++){
-      //   if(users[index]._id === id){
-      //     return name = users[index].firstName;
-      //   }
-      // }
