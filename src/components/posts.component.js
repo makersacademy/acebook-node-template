567028e169d45dab7a53c0c8b9e5ae13106a2f44
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-05 12:19:02 +0100

    merged front end and back end files and sent successful get request to /posts url

---
+++ src/components/posts.component.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// import axios from 'axios';
+
+export default class Posts extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.componentDidMount = this.componentDidMount.bind(this);
+
+    this.state = {posts: []};
+  }
+
+// will run before the page is rendered and add the exercises to the list
+  componentDidMount(){
+    fetch('http://localhost:3000/posts')
+    .then(response => { return response })
+    .then(function(data){
+      console.log(data)
+      window.data = data
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <p>these are my posts</p>
+      </div>
+    )
+  }
+}
