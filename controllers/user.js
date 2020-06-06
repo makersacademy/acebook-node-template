b2c3afa930c52673605bc693309509980f9b0814
tracht <marija.trachtenberg@gmail.com>
2020-06-06 19:51:52 +0100

    fixed bugs, signup works

--- controllers/user.js
+++ controllers/user.js
@@ -1,43 +1,43 @@
+'use strict'
+
 var User = require('../models/user');
 
 var UserController = {
   
   Index:function(req, res){
-    res.render('user/signup', { title: 'Signup to Acebook', loginTitle: 'Login to Acebook'}); // is this useruser grabbing entire instance
+    res.render('user/signup', { title: 'Signup to Acebook', loginTitle: 'Login to Acebook'});
   },
 
   Create: function(req, res) {
     var user = new User({firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, password: req.body.password});
 
-
-    User.findOne( {email: req.body.email}, function(err, result) {
-      if (result.email == req.body.email) {
-        res.render('user/validateSignUp', { signupMessage: "This email is already registered."} )
-      } else {
-          user.save(function(err) {
-            if (err) { throw err; }
-      
-            // route was formerly '/validate'
-            // res.status(201).redirect('/validateSignUp');
-            res.render('user/validateSignUp', { signupMessage: "Sign up sucessful.", firstName: req.body.firstName} )
-          });
-          req.session.user = user._id
+    User.findOne( {email: req.body.email}, function(err, result) { 
+      // findOne will return "null" if emal is not found in the database
+      // so it will skip this IF statement and move on to next code block
+      if (result != null && result.email != null && req.body.email == result.email) { 
+        res.render('user/validateSignUp', { signupMessage: "This email is already registered."}); 
       }
     })
+
+    user.save(function(err) {
+
+      if (err) { console.log(err) }
+      // route was formerly '/validate'
+      // res.status(201).redirect('/validateSignUp');
+      res.render('user/validateSignUp', { signupMessage: "Sign up sucessful.", firstName: req.body.firstName} )
+    });
+
+    req.session.user = user._id
+
   },
 
   Validate: function(req, res) {
-
-    console.log(req.body.email);
-    console.log(req.body.password);
-
+    console.log(req)
     User.findOne( {email: req.body.email}, function(err, result) {
 
       if(result.password == req.body.password) {
-        // redirect user logged in
         res.render('user/validateLogin', { loginMessage: "Login sucessful." } )
       } else {
-        // incorrect login details
         res.render('user/validateLogin', { loginMessage: "Login unsuccessful: incorrect email or password."})
       }
     })
