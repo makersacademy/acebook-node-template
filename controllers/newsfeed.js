caa03344ca582139e87909ec969e15c4963bc2c0
Edmond <edmond.b@hotmail.co.uk>
2020-06-08 16:09:44 +0100

    Name saved to post, user id Hardcoded

--- controllers/newsfeed.js
+++ controllers/newsfeed.js
@@ -15,10 +15,10 @@ var NewsfeedController = {
   Create: function(req, res) {
     var userName
 
-    User.findOne({ _id: "5ede176bbab17f447004aae3" }, function(err, user){
+    User.findOne({ _id: "5ede4b9410071e435c9601d6" }, function(err, user){
       userName = user.firstName + " " + user.lastName
 
-      var newPost = new Post({userID: "5ede176bbab17f447004aae3", name: userName, body: req.body.body, datePosted: Date.now()});
+      var newPost = new Post({userID: "5ede4b9410071e435c9601d6", name: userName, body: req.body.body, datePosted: Date.now()});
       newPost.save(function(err){
         res.send('saved')
       });
