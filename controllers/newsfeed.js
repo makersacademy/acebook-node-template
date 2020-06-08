5633c75bc3598da3799b0c773b9b88807f7f0ed7
Edmond <edmond.b@hotmail.co.uk>
2020-06-08 15:12:54 +0100

    Removed comments

--- controllers/newsfeed.js
+++ controllers/newsfeed.js
@@ -8,21 +8,8 @@ var NewsfeedController = {
 
   Posts: function(req, res) {
     Post.find(function(err, result) {
-      // result.forEach((item) => {
-        // Database request to Users
-        // User.findOne({ _id: item.userID }, function(err, user) {
-        //   item.name = user.firstName;
-        // });
-        // item.name = 'Jimothy Saladberg'
-      // });
       res.send(result);
     });
-  },
-
-  TempPostInsertion: function(req, res) {
-    var newPost = new Post({body: 'I met a lovely dog today', datePosted:'2020-06-03'});
-    newPost.save();
-    res.send('saved');
   },
 
   Create: function(req, res) {
--- routes/newsfeed.js
+++ routes/newsfeed.js
@@ -5,6 +5,5 @@ var NewsfeedController = require('../controllers/newsfeed');
 
 router.get('/', NewsfeedController.Index); //returning skeleton hbs
 router.get('/posts', NewsfeedController.Posts); // <-- JSON returning ajax route
-router.get('/tempsave', NewsfeedController.TempPostInsertion);
 router.post('/create', NewsfeedController.Create);
 module.exports = router; //exposes the newsfeed router to app.js
