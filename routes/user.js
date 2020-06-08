9619d571c643d80b3fee61bedd4a6ec96f63c7a3
tanilserbes <serbestanil@gmail.com>
2020-06-08 13:39:26 +0100

    login routes added

--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,7 @@ var UserController = require('../controllers/user')
 console.log(2)
 router.get('/new', UserController.New); // currently it is at '/user' then it adds /new to the end
 router.post('/new', UserController.Create);
+router.get('/login', UserController.Index);
+router.post('/login', UserController.Authenticate);
 
 module.exports = router;
