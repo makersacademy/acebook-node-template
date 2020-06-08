348096c74adc0db6ed582b745d15c8f561ae2dab
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-08 11:21:23 +0100

    added posts timeline

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import axios from 'axios';
 
+// Components are like functions that return HTML elements.
 class App extends React.Component{
 
-  state = {
-    post: ''
-  };
+  // component properties should be kept in an object called state
+  constructor() {
+    super();
+    this.state = {
+      post: '',
+      posts: []
+    };
+  }
+
+  componentDidMount = () => {
+    this.getBlogPost();
+  }
+
+  getBlogPost = () => {
+    axios.get('/api/posts')
+    .then((response) => {
+      const data = response.data
+      this.setState({posts: data})
+      console.log('Data has been recieved');
+    })
+    .catch(() => {
+      console.log('Error')
+    })
+  }
 
   handleChange = (event) => {
     const target = event.target;
@@ -38,25 +60,41 @@ class App extends React.Component{
     });
   }
 
+  displayPosts = (posts) => {
+    if (!posts.length) return null;
+
+    return posts.map((post, index) => (
+      <div key={index} className="blog-post_display">
+      <h4>{post.message}</h4>
+      </div>
+    ));
+  };
+
+// purpose of render is to display the specified HTML code inside the specified HTML element
   render(){
     console.log('State: ', this.state)
     return(
       <div>
-      <h2> Welcome to my app </h2>
-      <form onSubmit={this.submit}>
-        <div className="form-input">
-          <textarea
-          name="post"
-          placeholder="Enter your post"
-          cols="30"
-          rows="10"
-          value={this.state.post}
-          onChange={this.handleChange}>
-          </textarea>
-        </div>
+        <h2> Welcome to Acebook </h2>
+        <form onSubmit={this.submit}>
+          <div className="form-input">
+            <textarea
+            name="post"
+            placeholder="Enter your post"
+            cols="30"
+            rows="10"
+            value={this.state.post}
+            onChange={this.handleChange}>
+            </textarea>
+          </div>
 
-        <button>Submit</button>
-      </form>
+          <button>Submit</button>
+        </form>
+
+        <div className="newsfeed">
+          <h2>Timeline</h2>
+          {this.displayPosts(this.state.posts)}
+        </div>
       </div>
     );
   }
