98a142f170de36838859675d744db36755bcb830
tracht <marija.trachtenberg@gmail.com>
2020-06-09 11:50:15 +0100

    updated login spec test to use correct html classes

--- cypress/integration/user_can_log_in_spec.js
+++ cypress/integration/user_can_log_in_spec.js
@@ -1,16 +1,16 @@
 describe("Login page", function(){
   it("login success if user credentials are correct", function(){
-    cy.visit('/signup'); //will change route on refactor
+    cy.visit('/');
     cy.get('#login-form').find('[id="email"]').type('Lomothy.Tomins@example.com')
     cy.get('#login-form').find('[id="password"]').type('12345')
     cy.get('#login-form').submit();
 
-    cy.get('.loginMessage').should('contain', 'Login sucessful.');
+    cy.get('.login-message').should('contain', 'Welcome');
     // cy.get('.loginMessage').should('contain', 'Login sucessful')
   })
 
   it("login fails if user credentials are incorrect", function() {
-    cy.visit('/signup'); //will change route on refactor
+    cy.visit('/'); //will change route on refactor
     cy.get('#login-form').find('[id="email"]').type('Lomothy.Tomins@example.com')
     cy.get('#login-form').find('[id="password"]').type('wrong')
     cy.get('#login-form').submit();
