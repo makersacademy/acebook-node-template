3c350f9b8611f34025d0497227ee95bb31624e42
tracht <marija.trachtenberg@gmail.com>
2020-06-10 14:33:22 +0100

    passing test: user can submit a post

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -1,6 +1,12 @@
 describe('Newsfeed', function() {
   it('can submit posts and view them', function() {
-    cy.visit('/newsfeed');
+    cy.task('emptyUsers');
+    cy.task('emptyPosts');
+    cy.task('insertUser', {firstName: 'Lomothy', lastName: 'Mockins', email: 'Lomothy.Tomins@example.com', password: '12345'});
+    cy.visit('/');
+    cy.get('#login-form').find('[id="email"]').type('Lomothy.Tomins@example.com')
+    cy.get('#login-form').find('[id="password"]').type('12345')
+    cy.get('#login-form').submit();
 
     cy.get('#new-post-form').find('[id="new-post"]').type('Hello, world!');
     cy.get('#new-post-form').submit();
