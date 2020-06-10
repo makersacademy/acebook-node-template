acc8eb90e44b5942b3809188ee27d7a96d5b3be0
Edmond <edmond.b@hotmail.co.uk>
2020-06-10 16:31:55 +0100

    Added time stamp to comments + Refactoring

--- public/react/postComponent.js
+++ public/react/postComponent.js
@@ -5,69 +5,21 @@ class Post extends React.Component {
     super();
     this.state = {}
   }
+
   formatDate() {
     let date = new Date(this.props.data.datePosted)
     return date.toLocaleDateString()
-  }
-
-  myChangeHandler = (event) => {
-
-    this.setState({
-      body: event.target.value,
-      postId: this.props.data._id
-    })
-  }
-
-  mySubmitHandler = (event) => {
-
-    // saving body & post ID into the state   vvv
-    // fetch to post data to server - newsfeed/comments/new controller
-    // new controller postscontroller.comments
-    //   we are sending res.body & res.id
-    // findOne when res.id === post.id
-    // findOne
-    // var comment = new Post({comment: {body: res.body, userId: req.session.user.id, userName: }})
-    // update post with comment data
-
-    event.preventDefault();
-
-    let newComment = {
-      body: this.state.body,
-      id: this.state.postId
-     }
-
-    fetch('/newsfeed/comments/new', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify(newComment)
-    })
-    .then((response) => {
-      this.props.updatemethod();
-      //update method to fetch updated posts
-    })
   }
 
   render() {
     let data = this.props.data
     return(
       <div>
+      <div>
         <p class="postContent">{data.body} - by {data.name} - <span class="posted">Posted on {this.formatDate()}</span></p>
-        <p><span class="fa fa-comments fa-2x"></span></p>
-
-        <form id={`comment-form-${data._id}`} onSubmit={this.mySubmitHandler}>
-          <input id="new-comment" class="commentInput" type="text" placeholder="Type your comment here" onChange={this.myChangeHandler}></input>
-          <input type="submit" id="commentButton" class="button" value="Submit"></input>
-        </form>
-
-        { data.comments.map((comment) => {
-          return (
-            <div class="commentBox">
-              <p>{comment.body} - by {comment.commentUserName}</p>
-            </div>
-          )
-        })
-        }
-        </div>
+      </div>
+      <Comment data={data} updatemethod={this.props.updatemethod}/>
+      </div>
     );
   }
 }
