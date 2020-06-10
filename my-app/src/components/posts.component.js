0c32e42301249bb34959bcfbe6912e53fb6cea0a
Joanne Young <joanneyoung_@hotmail.com>
2020-06-10 17:09:53 +0100

    user can see all posts, but only delete/edit their own posts

--- my-app/src/components/posts.component.js
+++ my-app/src/components/posts.component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+// import e from 'express';
 
 // Components are like functions that return HTML elements.
 export default class Posts extends React.Component{
@@ -11,14 +12,19 @@ export default class Posts extends React.Component{
       post: '',
       posts: [],
       updateMessage: '',
-      updateID:''
+      updateID:'',
     };
+
+  }
+
+  getParams() {
+    const { match: { params } } = this.props;
+    console.log("in the get params method")
+    console.log(params)
+    return params
   }
 
   componentDidMount = () => {
-    const { match: { params } } = this.props;
-    // console.log(params);
-    console.log("we are here")
     this.getBlogPost();
   }
 
@@ -46,10 +52,12 @@ export default class Posts extends React.Component{
 
   submit = (event) => {
     event.preventDefault();
+    const { match: { params } } = this.props;
 
     const post = {
       message: this.state.post,
-      date: new Date(Date.now())
+      date: new Date(Date.now()),
+      userId: params.id
     };
 
     axios({
@@ -85,10 +93,7 @@ export default class Posts extends React.Component{
     })
 
     .then((response) => {
-      console.log(response.data)
       this.setState({updateMessage: response.data.message, updateID: response.data._id})
-      console.log(this.state.updateMessage)
-      console.log(this.state.updateID)
     })
     .catch(() => {
       console.log('Error')
@@ -148,27 +153,37 @@ export default class Posts extends React.Component{
   }
 
   resetUserInputs = () => {
-   this.setState({
-      post: '',
-      updateMessage: '',
-      updateID: ''
-    });
-  };
-
+    this.setState({
+       post: '',
+       updateMessage: '',
+       updateID: ''
+     });
+   };
 
   displayPosts = (posts) => {
+    // pull in params
+    const { match: { params } } = this.props;
+
     if (!posts.length) return null;
     return posts.map((post, index) => (
       <div key={index} className="post_display">
         <h4>{post.message}</h4>
-          {console.log(typeof post.date)}
         <h6> Posted at {post.date} </h6>
-        <form data-id={post._id} onSubmit={ this.delete }>
-          <input type="submit" value="Delete"/>
-        </form>
-        <form data-id={post._id} onSubmit={ this.update}>
-          <input type="submit" value="Edit"/>
-        </form>
+        {(() => {
+          if (post.userId !== params.id) {
+            return null
+          } else {
+            return(
+            <div>
+              <form data-id={post._id} onSubmit={ this.delete }>
+                <input type="submit" value="Delete"/>
+              </form>
+              <form data-id={post._id} onSubmit={ this.update}>
+                <input type="submit" value="Edit"/>
+              </form>
+            </div>)
+          }
+        })()}
       </div>
     ));
     
