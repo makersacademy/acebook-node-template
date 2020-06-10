3c7808696a090a8f4c41ec67f12df295ec669542
tracht <marija.trachtenberg@gmail.com>
2020-06-10 14:54:58 +0100

    passing test: user can read comments on a post

--- cypress/integration/user_can_read_posts_with_comments_spec.js
+++ cypress/integration/user_can_read_posts_with_comments_spec.js
@@ -1,17 +1,20 @@
 describe('Newsfeed', function() {
-  it('can view all posts with comments', function( {
-    cy.visit('/signup');
-    cy.get('#new-user-form').find('[id="firstName"]').type('Lomothy')
-    cy.get('#new-user-form').find('[id="lastName"]').type('Tomins')
-    cy.get('#new-user-form').find('[id="email"]').type('lt@example.com')
-    cy.get('#new-user-form').find('[id="password"]').type('1234')
-    cy.get('#new-user-form').submit();
-
-    cy.get('#login-form').find('[id="email"]').type('lt@example.com')
-    cy.get('#login-form').find('[id="password"]').type('1234');
+  it('can view all posts with comments', function() {
+    cy.task('emptyUsers');
+    cy.task('emptyPosts');
+    cy.task('insertUser', {firstName: 'Lomothy', lastName: 'Mockins', email: 'Lomothy.Tomins@example.com', password: '12345'});
+    cy.task('getUser', {firstName: 'Lomothy'})
+    .then(function(user) {
+      var fullName = user.firstName + ' ' + user.lastName;
+      cy.task('insertPost', {body: 'I met a lovely dog today', datePosted:'2020-06-03', name: fullName, userID: user._id.toString(), 
+      comments: [{body: "Amazing!", timePosted: "2020-06-04", commentUserName: fullName, commentUserID: user._id.toString()}]} ); 
+    });    
+    cy.visit('/')
+    cy.get('#login-form').find('[id="email"]').type('Lomothy.Tomins@example.com')
+    cy.get('#login-form').find('[id="password"]').type('12345');
     cy.get('#login-form').submit();
 
-    cy.get('#posts').should('contain', 'Amazing!');
+    cy.get('.commentBox').should('contain', 'Amazing!');
     //cy.get('#comment-form').find('[id="comment"]').type("a");
-  }))
-})
+  });
+});
