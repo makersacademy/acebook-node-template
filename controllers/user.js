35c978077623e6e321da79f7c23e556754efd23e
dpwdec <declankolakowski@gmail.com>
2020-06-11 12:19:20 +0100

    refactored user controller to use sendFlashMessage method.

--- controllers/user.js
+++ controllers/user.js
@@ -9,14 +9,13 @@ var UserController = {
 
   Create: function(req, res) {
     User.findOne( {email: req.body.email}, function(err, result) {
-      if(result) { sendErrorFlashMessage(res, req, '/', 'This email is already registered.'); return; }
+      if(result) { sendFlashMessage(res, req, '/', 'This email is already registered.'); return; }
 
       bcrypt.hash(req.body.password, 10, function(err, hash) {
         var user = new User({firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, password: hash});
         user.save(function(err) {
           if (err) { console.log(err) }
-          req.session.errorMessage = "Sign up successful."
-          res.redirect('/');
+          sendFlashMessage(res, req, '/', "Sign up successful.");
         });
       });
     });
@@ -24,27 +23,20 @@ var UserController = {
 
   Login: function(req, res) {
     User.findOne( {email: req.body.email}, function(err, result) {
-      if (result == null) { sendErrorFlashMessage(res, req, '/', "Login unsuccessful: incorrect email or password."); return; }
+      if (result == null) { sendFlashMessage(res, req, '/', "Login unsuccessful: incorrect email or password."); return; }
       bcrypt.compare(req.body.password, result.password, function(err, match) {
-        if (match) {
-          req.session.user = result;
-          res.redirect('/');
-        } else {
-          if (err) { console.log(err) }
-          req.session.errorMessage = "Login unsuccessful: incorrect email or password."
-          res.redirect('/');
-        }
-      })
-
-    })
+        if (match) { req.session.user = result; res.redirect('/'); return; }
+        sendFlashMessage(res, req, '/', "Login unsuccessful: incorrect email or password.")
+      });
+    });
   }, 
   Logout: function(request, response) {
     request.session.user = null;
-    sendErrorFlashMessage(response, request, '/', 'You logged out.');
+    sendFlashMessage(response, request, '/', 'You logged out.');
   } 
 }
 
-var sendErrorFlashMessage = (response, request, route, message) => {
+var sendFlashMessage = (response, request, route, message) => {
   request.session.errorMessage = message;
   response.redirect(route);
 };
