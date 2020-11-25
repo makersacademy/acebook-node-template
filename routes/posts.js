df045c783b9e3261c71046b04a360c494f9b645c
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-11-25 16:05:24 +0000

    added a back end delete feature to the seed app

--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,6 @@ var PostsController = require('../controllers/posts')
 router.get('/', PostsController.Index);
 router.post('/', PostsController.Create);
 router.get('/new', PostsController.New);
+router.delete('/:postId', PostsController.Delete); //added
 
 module.exports = router;
