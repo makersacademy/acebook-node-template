71de22884364c37d007dd262f11d8b3d5aff2a96
Joseph <jpkershaw17@gmail.com>
2020-11-25 15:59:21 +0000

    updating the login variable!

--- controllers/home.js
+++ controllers/home.js
@@ -12,25 +12,32 @@ var HomeController = {
     res.render('home/login', { title: 'Acebook' });
   },
   LoginUser: function(req,res) {
-    var user = User.findOne({ name: req.body.username }).exec();
-        if(!user) {
-            return res.status(400).send({ message: "The username does not exist" });
-        }
-        if(!bcrypt.compare(req.body.password, user.password)) {
-            return res.status(400).send({ message: "The password is invalid" });
-        }
-        res.send({ message: "The username and password combination is correct!" });
+    User.findOne({ name: req.body.username }).exec().then(data => {
+      if(!data) {
+          return res.status(400).send({ message: "Your username or password is incorrect" });
+      }
+      if(!bcrypt.compareSync(req.body.password, data.password)) {
+          return res.status(400).send({ message: "Your username or password is incorrect" });
+      }
+      User.updateOne({ name: req.body.username}, {
+        Status: true
+      }, function(err, affected, resp) {
+        console.log(resp);
+      })
+      res.send({ message: "The username and password combination is correct!" });
+     });
   },
   CreateUser: function(req, res) {
     req.body.Gender = req.body.Gender[0] === "Other" ? req.body.Gender[1] : req.body.Gender[0]
     req.body.password = bcrypt.hashSync(req.body.password, 10);
+    req.body.Status = false
     var user = new User(req.body);
     user.save(function(err) {
       if (err) { throw err; }
 
       res.status(201).redirect('/posts');
     });
-  }
+  },
 };
 
 module.exports = HomeController;
--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ var UserSchema = new mongoose.Schema({
   Gender: String,
   Birthday: String,
   'About-me': String,
+  Status: Boolean,
 });
 
 var User = mongoose.model('User', UserSchema);
