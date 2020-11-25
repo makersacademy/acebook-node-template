43e6ca6abf51328bf1386b9e107e10cdccd47db3
deborah <deborahadebiyi@hotmail.co.uk>
2020-11-25 14:09:06 +0000

    logout start and deleted profile files

--- models/profile.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var ProfileSchema = new mongoose.Schema({
-  message: String,
-});
-
-var Profile = mongoose.model('Profile', ProfileSchema);
-
-module.exports = Profile;
