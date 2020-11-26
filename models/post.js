8324194fa885a6eaf0f1ba80f3a18245cb935ebc
Lorna Roberts <lornaroberts99@gmail.com>
2020-11-26 14:26:23 +0000

    sorting out scheme

--- models/post.js
+++ models/post.js
@@ -1,7 +1,15 @@
 var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
-  message: String,
+  
+  message: {
+    type: String,
+    required: true
+  },
+  date:{
+    type: Date,
+    default: Date.now
+  }
 });
 
 var Post = mongoose.model('Post', PostSchema);
