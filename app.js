6990ee1d256088d522a21f2f5be16cdce72d2c01
Eunice Kontor <Eunice.Kontor@ft.com>
2020-11-30 19:02:37 +0000

    userschema created

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyparser = require("body-parser");
 
 var homeRouter = require('./routes/home');
 var postsRouter = require('./routes/posts');
