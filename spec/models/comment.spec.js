1d3d9b4e580877c8f95764d99e199f04cc305ac3
Ben Jones <benwjones76@outlook.com>
2020-12-02 11:14:57 +0000

    added all of the tests to comment spec

--- spec/models/comment.spec.js
+++ spec/models/comment.spec.js
@@ -14,4 +14,43 @@ describe('Comment model', function() {
     var comment = new Comment({ comment: "Hi stinkers", author: "Mr Smelly" });
     expect(comment.comment).toEqual('Hi stinkers');
   });
+
+  it('can list all comments', function(done) {
+    Comment.find(function(err, comments) {
+      expect(err).toBeNull();
+      expect(comments).toEqual([]);
+      done();
+    });
+  });
+
+  it('can save a comment', function(done) {
+    var comment = new Comment({ comment: 'some comment', author: "Mr Smelly" });
+
+    comment.save(function(err) {
+      expect(err).toBeNull();
+
+      Comment.find(function(err, comment) {
+        expect(err).toBeNull();
+
+        expect(comment[0]).toMatchObject({ comment: 'some comment' });
+        done();
+      });
+    });
+  });
+
+  it('can delete a comment', function(done) {
+    var comment = new Comment({ comment: 'some comment', author: "Mr Smelly" });
+
+    comment.save(function(err) {
+      expect(err).toBeNull();
+
+      Comment.deleteOne(comment,function(err, comment) {
+        expect(err).toBeNull();
+
+        expect(comment).toEqual({"deletedCount": 1, "n": 1, "ok": 1});
+        done();
+      });
+    });
+ });
+
 });
