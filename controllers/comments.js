6882a09c85a7352f684dbacafe4860487ebedf36
Lorna Roberts <lornaroberts99@gmail.com>
2020-12-03 09:01:56 +0000

    can now retrieve list of comments for post

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,10 +1,11 @@
 var Comment = require('../models/comment');
 
+
 var CommentsController = {
     Index: function(req, res) {
         Comment.find(function(err, comments) {
             if (err) { throw err; }
-            
+
             res.json({comments: comments});
           });
     },
@@ -18,7 +19,14 @@ var CommentsController = {
             if (err) { throw err; }
           });
           res.json({comment: comment});
-    }
-  };
-  
+    },
+    Find: function(req, res) {
+      var postId = req.params.postId;
+      Comment.find({postId: postId},function(err, comments) {
+          if (err) { throw err; }
+
+          res.json({comments: comments});
+        });
+  }
+}
   module.exports = CommentsController;
--- routes/comments.js
+++ routes/comments.js
@@ -5,5 +5,6 @@ var CommentsController = require('../controllers/comments');
 
 router.get('/', CommentsController.Index);
 router.post('/', CommentsController.Create);
+router.get('/:postId', CommentsController.Find); 
 
 module.exports = router;
