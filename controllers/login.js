4bd7011e960d95677706eab292d3b74a60ea7878
Tuyet Le <toowee.le@gmail.com>
2021-02-08 12:55:12 +0000

    update error messages if user not found

--- controllers/login.js
+++ controllers/login.js
@@ -11,12 +11,12 @@ var LoginController = {
     User.findOne({username: username, password: password}, (err, user) => {
       if (err) {
         console.log(err);
-        return res.status(500).send();
+        return res.status(500).send('Internal Server Error');
       }
 
       // if user object doesn't exist in the database, send 404 status back to client
       if (!user) {
-        return res.status(404).send();
+        return res.status(404).send('User not found');
       }
 
       // if user object exist in the database, redirect user to the dashboard
