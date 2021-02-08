5c80861bd34e0de4d281d8da9851e04791892b3a
Tuyet Le <toowee.le@gmail.com>
2021-02-08 12:14:16 +0000

    add simple login functionality

--- controllers/login.js
+++ controllers/login.js
@@ -4,13 +4,24 @@ var LoginController = {
   Index: function(req, res) {
     res.render('home/login', { title: 'Acebook' });
     },
-  Login: function(req, res) {
-    User.find({}, (err, user) => {
+  Login: function(req, res) {      
+    username = req.body.username;
+    password = req.body.password;
+
+    User.findOne({username: username, password: password}, (err, user) => {
       if (err) {
-        res.send(err)
-      } else {
-        res.json(user)
+        console.log(err);
+        return res.status(500).send();
       }
+
+      // if user object doesn't exist in the database, send 404 status back to client
+      if (!user) {
+        return res.status(404).send();
+      }
+
+      // if user object exist in the database, redirect user to the dashboard
+      console.log(user)
+      return res.status(201).redirect('/content');
     })
   }
 };
