1f5af83559030cc02cbb48c921f18ce61f677aeb
Tuyet Le <toowee.le@gmail.com>
2021-02-08 23:09:21 +0000

    redirect user back to login page if password incorrect

--- controllers/login.js
+++ controllers/login.js
@@ -5,23 +5,26 @@ var LoginController = {
   Index: function(req, res) {
     res.render('home/login', { title: 'Acebook' });
     },
-  Login: async (req, res) => {  
+  Login: async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
     try {
       const user = await User.findOne({ username: username }).exec();
 
+      // If user object does not exist, return 404 to client
       if (!user) {
         return res.status(404).redirect('/login').send({ message: 'The user does not exist. '});
       }
 
+      // Validate the password
       const match = await bcrypt.compare(password, user.password);
 
       if (!match) {
-        return res.status(404).send('The password is invalid');
+        return res.status(404).redirect('/login');
       } else {
-        res.send('The username and password combination is correct!');
+        res.status(201).redirect('/content');
+        console.log('Username and password are both correct!');
       }
     } catch {
       res.status(500).send('Internal Server Error');
