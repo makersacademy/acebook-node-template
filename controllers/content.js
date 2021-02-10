22e7f00b71f054ff775ca06becad70ed626b25d9
Elena <elenaevans10@gmail.com>
2021-02-10 12:26:14 +0000

    navigating round site with links/buttons maintains session, session is destroyed with logout button

--- controllers/content.js
+++ controllers/content.js
@@ -7,7 +7,7 @@ var ContentController = {
     Content.find(function(err, content) {
       if (err) { throw err; }
       if (!req.session.user){
-        console.log('There is no session')
+        //console.log('There is no session')
         return res.status(401).redirect('/');
       }
       else{
@@ -17,8 +17,13 @@ var ContentController = {
     });
   },
   New: function(req, res) {
-    res.render('content/new', {});
-  },
+    if (!req.session.user){
+      //console.log('There is no session')
+      return res.status(401).redirect('/');
+    }
+    else{
+      res.render('content/new', {});
+  }},
   Create: function(req, res) {
     var content = new Content(req.body);
     content.save(function(err) {
