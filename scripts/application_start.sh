0e0053ad70a9e470da0a2e685b524e6a8545f9a3
Edward Andress <eddie_andress@hotmail.com>
2021-02-19 16:16:17 +0000

    do it all in app start

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,1 +1,16 @@
+sudo touch /etc/yum.repos.d/mongodb-org-4.4.repo
+sudo tee -a /etc/yum.repos.d/mongodb-org-4.4.repo > /dev/null <<EOT
+[mongodb-org-4.4]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.4/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc
+EOT
+sudo yum install -y mongodb-org
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+sudo ~/.nvm/versions/node/v15.9.0/bin/npm install
+sudo systemctl start mongod
 ~/.nvm/versions/node/v15.9.0/bin/npm start
