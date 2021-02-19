0974351931ebaa0fe4ecced264463bd3d3bd3fd1
Edward Andress <eddie_andress@hotmail.com>
2021-02-19 15:13:07 +0000

    start app in after install script?

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,5 @@
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
-sudo npm install
-sudo npm install forever -g
+sudo ~/.nvm/versions/node/v15.9.0/bin/npm install
 sudo systemctl start mongod
