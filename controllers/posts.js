9888a47d7c21721c6e0d85fd56259b35160be435
yy140 <74052695+yy140@users.noreply.github.com>
2021-03-09 16:56:56 +0000

    sort posts by date

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,13 +1,21 @@
 var Post = require('../models/post');
 var mongoose = require('mongoose');
 var PostsController = {
+  
+  
   Index: function(req, res) {
-    Post.find(function(err, posts) {
+    Post.find().sort( {date: -1}).exec(function(err, posts) {
       if (err) { throw err; }
-
       res.render('posts/index', { posts: posts });
     });
   },
+  
+  // Index: function(req, res) {
+  //   Post.find(function(err, posts) {
+  //     if (err) { throw err; }
+  //     res.render('posts/index', { posts: posts });
+  //   });
+  // },
   New: function(req, res) {
     res.render('posts/new', {});
   },
@@ -21,7 +29,7 @@ var PostsController = {
   },
   Delete: function(req, res) {
     Post.findOneAndDelete( {
-       _id: mongoose.Types.ObjectId(req.body.id)}, function(err, post) {
+       _id: mongoose.Types.ObjectId(req.body.id)}, function(err, posts) {
       res.status(201).redirect('/posts');
     });
       
