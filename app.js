8f47b83b4b1c8a263d1297d7738e2a255aefaf4b
chelsey hill <chelsey.c.hill@outlook.com>
2021-03-09 12:31:58 -0500

    signup form

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var homeRouter = require('./routes/home');
 var postsRouter = require('./routes/posts');
-
+var signupRouter = require('./routes/signup');
 var app = express();
 
 // view engine setup
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // route setup
 app.use('/', homeRouter);
 app.use('/posts', postsRouter);
+app.use('/signup',signupRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/signup.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+var PostsController = require('../controllers/posts')
+
+router.get('/', PostsController.Index);
+
+module.exports = router;
