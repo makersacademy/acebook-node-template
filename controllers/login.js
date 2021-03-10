bee42de2d086c74a8d583062a7f92320739d4e68
chelsey hill <chelsey.c.hill@outlook.com>
2021-03-10 07:41:59 -0500

    login redirect

--- controllers/login.js
+++ controllers/login.js
@@ -1,6 +1,9 @@
 var LoginController = {
     Index: function(req, res) {
       res.render('login/index');
+    }, 
+    Dashboard: function(req,res) {
+      res.redirect('posts');
     }
   };
   
--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var LoginController = require('../controllers/login')
 
 router.get('/', LoginController.Index);
+router.post('/', LoginController.Dashboard);
 
 module.exports = router;
 
