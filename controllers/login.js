eb8228fe36643cf65686f8811367dc92870bbeae
girl-interrupted <d.pavliuchyk@gmail.com>
2021-03-10 16:20:47 +0000

    password validation

--- controllers/login.js
+++ controllers/login.js
@@ -1,10 +1,34 @@
+var User = require('../models/user');
+var bcrypt = require('bcrypt');
+
+
 var LoginController = {
     Index: function(req, res) {
       res.render('login/index');
     }, 
+
     Dashboard: function(req,res) {
-      res.redirect('posts');
+      User.findOne({ 
+        username: req.body.username
+                  })
+        .then(function (user) {
+          if (!user) {
+             res.redirect('/');
+          } else {
+        bcrypt.compare(req.body.password, user.password, function (err, result) {
+         if (result == true) {
+             res.redirect('/posts');
+         } else {
+          res.send('Incorrect password');
+          res.redirect('/');
+         }
+       });
+      }
+   });
     }
+
   };
-  
+    
+
+
   module.exports = LoginController;
