1dec605cd534b80724e2f8c6672a2f3ca4305e2a
girl-interrupted <d.pavliuchyk@gmail.com>
2021-03-17 11:43:15 +0000

    regex match added for password

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ var uniqueValidator = require('mongoose-unique-validator');
 
 var UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true, match: [/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{5,}$/, 'is invalid'] }
 });
 UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 
