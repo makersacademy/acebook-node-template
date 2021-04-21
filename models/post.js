fd03b7fc27ad15472bd7ead6d334855d3babf4da
Maria Eleni Tsirpanli <tsirpanlimariaeleni@gmail.com>
2021-04-21 17:34:53 +0100

    comments schema creation and tests

--- models/post.js
+++ models/post.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var PostSchema = new mongoose.Schema({
   message: String,
   date: { type: Date, default: Date.now },
+  user: String,
 });
 
 var Post = mongoose.model('Post', PostSchema);
