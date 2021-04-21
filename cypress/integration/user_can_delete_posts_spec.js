721e03078784183901be909975142774279d7a09
Maria Eleni Tsirpanli <tsirpanlimariaeleni@gmail.com>
2021-04-21 17:05:40 +0100

    Integration test for deleting posts

---
+++ cypress/integration/user_can_delete_posts_spec.js
@@ -0,0 +1,12 @@
+describe('Timeline', function() {
+    it('can delete posts', function() {
+      cy.visit('/posts');
+      cy.contains('New post').click();
+  
+      cy.get('#new-post-form').find('[type="text"]').type('Hello, world!');
+      cy.get('#new-post-form').submit();
+      cy.contains('Delete').click();
+  
+      cy.get('.posts').should('not.contain', 'Hello, world!');
+    });
+  });
