78b3cebdb2ca8477190de3e81d64c5e2d4ecd012
Alice Lui <aytlui@gmail.com>
2021-04-27 14:19:24 +0100

    tests passing for edit

--- cypress/integration/user_profile_spec.js
+++ cypress/integration/user_profile_spec.js
@@ -1,18 +1,33 @@
-describe('User profile', function() {
+describe('Profile', function() {
   before(async (done) => {
-      await cy.task("db:drop");
-      done();
+    await cy.task("db:drop");
+    done();
   })
+	
+	describe('profile views', function(){
+		beforeEach(() => {
+			cy.signupAndLogin()
+		});
 
-  beforeEach(() => {
-      cy.signupAndLogin()
-  });
+		it('can view profile', function() {
+			cy.visit('/posts');
+			cy.contains('user1').click();
+	
+			cy.contains('user1');
+			cy.contains('I have not written a bio yet');
+		});
 
-  it('can view profile', function() {
-      cy.visit('/posts');
-      cy.contains('user1').click();
+		it('can edit a profile', function(){
+			cy.visit('/posts');
+			cy.contains('user1').click();
+	
+			cy.contains('Edit Bio').click();
+			cy.get('#edit-profile-form').find('#message').type('I am a robot');
+			cy.contains('Edit Bio').click();
 
-      cy.contains('user1');
-      cy.contains('I have not written a bio yet');
-  })
+			cy.contains('I am a robot');
+	
+		});
+	})
+  
 })
