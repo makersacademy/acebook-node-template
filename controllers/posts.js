b8e38633f3106dbbf2d5094f8ebcf0e2230d542a
Renaé St Louis <renaestlouis@Renaes-MacBook-Pro.local>
2021-04-27 14:24:03 +0100

    updated login function to handle unrecognised user error

--- controllers/posts.js
+++ controllers/posts.js
@@ -29,17 +29,15 @@ var PostsController = {
     var post = Post.findById(req.params.id)
     post.deleteOne( function(err) {
       if (err) { throw err;}
-      const response = {
-        message: "Post successfully deleted",
-        id: post._id
-      };
+
+
       res.status(201).redirect('/posts');
     });
   },
 
   Edit: function(req, res) {
-    id = req.params.id;
-    edit = req.body.edited;
+    var id = req.params.id;
+    var edit = req.body.edited;
     console.log(id)
     console.log(edit)
     Post.findByIdAndUpdate({_id: id},{message: edit}, (err) => {
@@ -75,8 +73,8 @@ var PostsController = {
   },
 
   EditComment: function(req, res) {
-    id = req.params.id;
-    edit = req.body.edited;
+    var id = req.params.id;
+    var edit = req.body.edited;
     console.log(id)
     console.log(edit)
     Comment.findByIdAndUpdate({_id: id},{comment: edit}, (err) => {
