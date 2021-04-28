3dea12d80c203a6cfe472441f14b33ebc772bb3a
Maria Eleni Tsirpanli <tsirpanlimariaeleni@gmail.com>
2021-04-28 16:02:33 +0100

    integration tests for user association

--- cypress/integration/edit_posts_spec.js
+++ cypress/integration/edit_posts_spec.js
@@ -1,19 +1,50 @@
-describe('Timeline', function() {
-    it('can edit posts', function() {
-      cy.visit('/')
-      cy.get('input.fname').type('Edit')
-      cy.get('input#pword').type('Edit')
-      cy.get('.registration-form').submit();
-      
-      cy.visit('/posts');
-      cy.contains('New post').click();
-      cy.get('#new-post-form').find('[type="text"]').type('Hello, world!');
-      cy.get('#new-post-form').submit();
-  
-      cy.get('.cards').should('contain', 'Hello, world!');
+describe('Timeline', function () {
+  beforeEach(async (done) => {
+    await cy.task("db:drop:all");
+    done();
+  })
+  it('can edit posts', function () {
+    cy.visit('/')
+    cy.get('input.fname').type('Edit')
+    cy.get('input#pword').type('Edit')
+    cy.get('.registration-form').submit();
 
-      cy.get('#edit-post-form').find('#message').type('Foxes');
-      cy.get('#edit-post-form').submit();
-      cy.get('.cards').should('contain', 'Foxes');
-    });
+    cy.visit('/posts');
+    cy.contains('New post').click();
+    cy.get('#new-post-form').find('[type="text"]').type('Hello, world!');
+    cy.get('#new-post-form').submit();
+
+    cy.get('.cards').should('contain', 'Hello, world!');
+
+    cy.get('#edit-post-form').find('#message').type('Foxes');
+    cy.get('#edit-post-form').submit();
+    cy.get('.cards').should('contain', 'Foxes');
   });
+
+  it('can only edit own post', function () {
+    cy.visit('/')
+    cy.get('input.fname').type('Edit Com')
+    cy.get('input#pword').type('Edit')
+    cy.get('.registration-form').submit();
+
+    cy.visit('/posts');
+    cy.contains('New post').click();
+
+    cy.get('#new-post-form').find('[type="text"]').type('Hello, world!');
+    cy.get('#new-post-form').submit();
+
+    cy.get('#logoutbutton').submit();
+
+    cy.get('input.fname').type('other_user')
+    cy.get('input#pword').type('otheruser')
+    cy.get('.registration-form').submit();
+
+
+    cy.get('#edit-post-form').type('Edited post');
+    cy.get('#edit-post-form').submit();
+
+    cy.on('window:alert', (txt) => {
+      expect(txt).to.contains('Bruh! You cant be editing ppls posts like that!');
+    })
+  });
+});
