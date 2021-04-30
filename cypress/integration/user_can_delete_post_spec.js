aaaf75c23575f6ac1b651674417c089841bdfd87
Alice Lui <aytlui@gmail.com>
2021-04-30 13:19:37 +0100

    delete test working

--- cypress/integration/user_can_delete_post_spec.js
+++ cypress/integration/user_can_delete_post_spec.js
@@ -10,7 +10,7 @@ describe('Delete posts', function() {
 
     it('can delete a post and view the updated list', function() {
         cy.visit('/posts');
-        cy.contains('New post').click();
+        cy.get('.new-post-link').click();
         cy.get('#new-post-form').find('[type="text"]').type('Goodbye, world!');
         cy.get('#new-post-form').submit();
         cy.contains('Delete').click();
