9688e6a5a8d5ab2bd6f7c0e59764e9dd80df3659
anitaRead <areadman91@gmail.com>
2021-07-14 12:38:38 +0100

    Pass feature test - delete the latest post

--- cypress/integration/user_can_delete_post.js
+++ cypress/integration/user_can_delete_post.js
@@ -3,11 +3,11 @@ describe('Timeline', function() {
     cy.visit('/posts');
     cy.contains('New post').click();
 
-    cy.get('#new-post-form').find('[type="text"]').type('Hello, world!');
+    cy.get('#new-post-form').find('[type="text"]').type('Fresh slate!');
     cy.get('#new-post-form').submit();
+    
+    cy.get('.delete-button:last').click();
 
-    cy.get('#delete').click();
-    cy.reload();
-    cy.get('.posts').should('not.contain', 'Hello, world!');
+    cy.get('.posts').should('not.contain', 'Fresh slate!');
   });
 });  
