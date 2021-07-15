9fc6cac99c18bfc156c0003fc96999974df5a97d
Kelvin Munro <kmunro4@ford.com>
2021-07-15 13:26:15 +0100

    update function now working with tests passing, updates the correct entry, very happy

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,18 +2,18 @@ var Post = require('../models/post');
 // const { post } = require('../routes/posts');
 
 var PostsController = {
-  
+
   Index: function(req, res) {
     Post.find(function(err, posts) {
       if (err) { throw err; }
       res.render('posts/index', { posts: posts });
     });
   },
-  
+
   New: function(req, res) {
     res.render('posts/new', {});
   },
-  
+
   Create: function(req, res) {
     var post = new Post(req.body);
 
@@ -25,9 +25,22 @@ var PostsController = {
     });
   },
 
+  UpdatePage: function(req, res) {
+    var id = req.params.id;
+    Post.findById(id, function(err, post){
+      if (err) {throw err;}
+
+      res.render('posts/update', {id: post.id, message: post.message});
+    })
+    console.log("Start of the log");
+  },
+
   Update: function(req, res){
-    var id = req.body.id
+    var id = req.params.id
+    console.log(req.params)
     var message = req.body.message
+    console.log(message)
+    console.log("End of the log")
     Post.updateOne({"_id" : id}, {$set: {"message": message}}, {upsert: true}, function(err){
       if(err) { throw err; }
 
@@ -35,21 +48,15 @@ var PostsController = {
     })
   },
 
-  UpdatePage: function(req, res) {
-    console.log('************************UPDATE PAGE *************')
-    var post = Post.findById(req.params.id)
-    res.render('posts/update', {post: post});
-  },
-
-  Delete: function(req, res){ 
-    var id = req.params.id; 
+  Delete: function(req, res){
+    var id = req.params.id;
     Post.findByIdAndRemove(id, function(err){
       if(err){
         console.log(err);
       }
       res.status(201).redirect('/posts');
     });
-  } 
+  }
 };
 
 module.exports = PostsController;
--- routes/posts.js
+++ routes/posts.js
@@ -7,8 +7,19 @@ router.get('/', PostsController.Index);
 router.post('/', PostsController.Create);
 router.delete('/:id', PostsController.Delete);
 router.get('/new', PostsController.New);
-router.post('/update', PostsController.Update);
+router.post('/update/:id', PostsController.Update);
 router.get('/update/:id', PostsController.UpdatePage);
+
+// router.get('/searchbar/:id', checkAuth, (req, res) => {
+//   let userId = req.params.id
+//   Invoice.find({'user': userId})
+//     .then(invoice => {
+//         res.status(200).json({
+//           userDetails: invoice.userDetails,
+//           invoiceCost: invoice.Cost
+//         })
+//     });
+// });
 
 
 module.exports = router;
