240a1297f915f6eb770171595097e1b7adc19ac4
Kelvin Munro <kmunro4@ford.com>
2021-07-16 10:24:25 +0100

    small refactor to update and create tests

--- cypress/integration/user_can_update_post_spec.js
+++ cypress/integration/user_can_update_post_spec.js
@@ -1,16 +1,17 @@
 describe('Timeline', function() {
   it('can update a post', function() {
     cy.visit('/posts');
-    
+
     cy.contains('New post').click();
     cy.get('#new-post-form').find('[type="text"]').type('Hello, world!');
     cy.get('#new-post-form').submit();
-    
+
     cy.contains('Update post').click();
     cy.get('#update-post-form').find('[type="text"]').type('This was changed');
     cy.get('#update-post-form').submit();
 
     cy.get('.posts').should('contain', 'This was changed');
+    cy.get('.delete-button:last').click();
 
   });
 });
