9b6ccab84898a74c49b84ebf2d2241d59e1c54be
Kelvin Munro <kmunro4@ford.com>
2021-07-16 16:51:54 +0100

    ongoing work on trying to get like button and its counter to work correctly, currently index.js file is cluttered with different approaches and needs further wall banging to try and resolve

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,40 @@
 window.onload = function() {
     var counter = 1;
-    var likeCounter = document.getElementById("like-counter");
-    var likeButton = document.getElementById("like-button");
-    likeCounter.innerHTML = ""
+    // var likeCounter = document.getElementById("like-counter");
+    var likeCounters = document.getElementsByClassName("like-counter")
+    // var likeButton = document.getElementById("like-button");
+    var likeButtons = document.getElementsByClassName("like-button")
+    // console.log(likeCounters);
 
-    likeButton.addEventListener("click", function(){
-      console.log(sessionStorage.clickcount)
-        sessionStorage.clickcount = Number(sessionStorage.clickcount) + counter;
-        likeCounter.innerHTML = sessionStorage.clickcount;
-    });
+    for(var k = 0; k < likeCounters.length; k++) {
+      likeCounters[k] = ""
+    }
+
+    for(var i = 0; i < likeButtons.length; i++) {
+      likeButtons[i].addEventListener("click", function(){
+        for(var j = 0; j < likeCounters.length; j++) {
+          // if (i === j) {
+            likeCounters[i] = counter++;
+            console.log(likeCounters[i]);
+            likeCounters[i].innerHTML;
+          // }
+        }
+
+
+
+
+
+        // hold onto this code as contains the sessionStorage code
+        // if (sessionStorage.clickcount) {
+        //   sessionStorage.clickcount = Number(sessionStorage.clickcount) + counter;
+        //   likeCounters[i].innerHTML = sessionStorage.clickcount;
+        // } else {
+        //   sessionStorage.clickcount = counter;
+        //   likeCounters[i].innerHTML = sessionStorage.clickcount;
+        // }
+      });
+    }
+
+
+
 }
