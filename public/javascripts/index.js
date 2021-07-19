e8f039a4d6972cade42038eff244535830cb3b25
Kelvin Munro <kmunro4@ford.com>
2021-07-19 11:17:43 +0100

    added a dislike button and a counter to track number of dislikes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,6 +8,8 @@ likeElements.forEach(function(navElement) {
 window.onload = function() {
   var likeCounters = document.querySelectorAll("#like-counter");
   var likeButtons = document.querySelectorAll("#like-button");
+  var dislikeButtons = document.querySelectorAll("#dislike-button");
+  var dislikeCounters = document.querySelectorAll("#dislike-counter");
 
 likeButtons.forEach(function(likeButton, i) {
   var counter = 1;
@@ -15,4 +17,12 @@ likeButtons.forEach(function(likeButton, i) {
       likeCounters[i].innerHTML = counter++;
     })
   });
+
+  dislikeButtons.forEach(function(dislikeButton, i) {
+    var counter = 1;
+    dislikeButton.addEventListener("click", function(){
+      dislikeCounters[i].innerHTML = counter++;
+    })
+  });
+
 }
