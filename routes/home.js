d00ede4d2e921a2e9c2bc45ad069723a5fb530ad
anitaRead <areadman91@gmail.com>
2021-07-19 17:12:19 +0100

    Pass feature test user can sign in but missing unit test

--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,7 @@ var HomeController = require('../controllers/home');
 
 router.get('/', HomeController.Index);
 router.post('/new', HomeController.Create);
-
-
+router.get('/sessions', HomeController.SigninPage);
+router.post('/sessions/new', HomeController.Signin);
 
 module.exports = router;
