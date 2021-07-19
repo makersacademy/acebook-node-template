43555e3e518c98f57115b695f3653cedf48e9a9e
Kelvin Munro <kmunro4@ford.com>
2021-07-19 11:08:58 +0100

    changed like button function to not have sessions but it correctly updates for each post, it shall not save so new page clears like counter, but it works otherwise

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,40 +1,18 @@
+
+var likeElements = document.querySelectorAll('#likeCounter');
+likeElements.forEach(function(navElement) {
+  console.log('navElement: ', navElement);
+  })
+
+
 window.onload = function() {
-    var counter = 1;
-    // var likeCounter = document.getElementById("like-counter");
-    var likeCounters = document.getElementsByClassName("like-counter")
-    // var likeButton = document.getElementById("like-button");
-    var likeButtons = document.getElementsByClassName("like-button")
-    // console.log(likeCounters);
+  var likeCounters = document.querySelectorAll("#like-counter");
+  var likeButtons = document.querySelectorAll("#like-button");
 
-    for(var k = 0; k < likeCounters.length; k++) {
-      likeCounters[k] = ""
-    }
-
-    for(var i = 0; i < likeButtons.length; i++) {
-      likeButtons[i].addEventListener("click", function(){
-        for(var j = 0; j < likeCounters.length; j++) {
-          // if (i === j) {
-            likeCounters[i] = counter++;
-            console.log(likeCounters[i]);
-            likeCounters[i].innerHTML;
-          // }
-        }
-
-
-
-
-
-        // hold onto this code as contains the sessionStorage code
-        // if (sessionStorage.clickcount) {
-        //   sessionStorage.clickcount = Number(sessionStorage.clickcount) + counter;
-        //   likeCounters[i].innerHTML = sessionStorage.clickcount;
-        // } else {
-        //   sessionStorage.clickcount = counter;
-        //   likeCounters[i].innerHTML = sessionStorage.clickcount;
-        // }
-      });
-    }
-
-
-
+likeButtons.forEach(function(likeButton, i) {
+  var counter = 1;
+    likeButton.addEventListener("click", function(){
+      likeCounters[i].innerHTML = counter++;
+    })
+  });
 }
