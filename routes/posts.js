1763a15e839351c70d41a202dfcca5e55629b340
Kelvin Munro <kmunro4@ford.com>
2021-07-21 15:59:27 +0100

    fixing stupid branch

--- routes/posts.js
+++ routes/posts.js
@@ -8,6 +8,8 @@ router.post('/', PostsController.Create);
 router.delete('/:id', PostsController.Delete);
 router.get('/new', PostsController.New);
 router.post('/update/:id', PostsController.Update);
+router.get('/update/:id', PostsController.UpdatePage);
+
 router.post('/updateLikes/:id', PostsController.UpdateLikes);
 
 
