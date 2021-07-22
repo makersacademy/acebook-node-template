660ad3ff9d6a7e7178c342f016d4e1562374b0c8
ehelsan <ehelsan@hotmail.co.uk>
2021-07-22 15:15:31 +0100

    atests passing - comment box

--- cypress/integration/comment_box_spec.js
+++ cypress/integration/comment_box_spec.js
@@ -1,12 +1,13 @@
 describe('Timeline', function() {
-  it('can view comments for a post', function() {
+  it('can view comments on a post', function() {
 
     cy.visit('/posts');
+    cy.get('.new-post-link').click();
     cy.get('#new-post-form').find('[type="text"]').type('comment test');
     cy.get('#new-post-form').submit();
-    cy.get('.comment-toggle:last').click();
-    cy.get('#commentform-{{this._id}}').find('[type="text"]').type('COMMENT-ONE');
-    cy.get('#commentform-{{this._id}}').submit();
-    //cy.get('.posts').should('contain', 'COMMENT-ONE');    
+    cy.get('.comment-toggle').click();
+    cy.get('.form-container').find('[type="text"]').type('COMMENT-ONE');
+    cy.get('.form-container').submit();
+    cy.get('.comment-list').should('contain', 'COMMENT-ONE');    
   });
 });
