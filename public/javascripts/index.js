f5f1af25b75012211d115d2cd7bb2c8395239939
Kelvin Munro <kmunro4@ford.com>
2021-07-23 09:59:46 +0100

    small refactor to remove comments

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,25 +1,8 @@
 
 
 window.onload = function() {
-  // var likeCounters = document.querySelectorAll("#like-counter");
-  // var likeButtons = document.querySelectorAll("#like-button");
-  // var dislikeButtons = document.querySelectorAll("#dislike-button");
-  // var dislikeCounters = document.querySelectorAll("#dislike-counter");
-  var commentToggles = document.querySelectorAll(".comment-toggle");
 
-// likeButtons.forEach(function(likeButton, i) {
-//   var counter = 1;
-//     likeButton.addEventListener("click", function(){
-//       likeCounters[i].innerHTML = counter++;
-//     })
-//   });
-//
-//   dislikeButtons.forEach(function(dislikeButton, i) {
-//     var counter = 1;
-//     dislikeButton.addEventListener("click", function(){
-//       dislikeCounters[i].innerHTML = counter++;
-//     })
-//   });
+  var commentToggles = document.querySelectorAll(".comment-toggle");
 
   commentToggles.forEach(function(commentToggle) {
     commentToggle.addEventListener("click", function(e){
