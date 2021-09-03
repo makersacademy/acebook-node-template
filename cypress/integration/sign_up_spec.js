1d1ba6ddf80c76d0ac0d347a4a79506d1e6b60da
Heather Dalton <79050392+hjdalton@users.noreply.github.com>
2021-09-03 10:53:08 +0100

    sign up test written

---
+++ cypress/integration/sign_up_spec.js
@@ -0,0 +1,11 @@
+describe('Sign Up', function() {
+  it('allows the user to enter their name', function() {
+    cy.visit('/');
+    
+    cy.get('#sign-up-form').find('[type="text"]').type('Jim');
+    cy.get('#sign-up-form').find('[type="email"]').type('email@email.com');
+    cy.get('#sign-up-form').find('[type="password"]').type('1234');
+    
+    cy.submit('#sign-up-form').submit();
+  });
+});
