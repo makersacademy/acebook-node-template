04b1170f8548b8933c71546c238d2155d32f050b
Heather Dalton <79050392+hjdalton@users.noreply.github.com>
2021-09-03 14:58:16 +0100

    date added to post model

--- models/post.js
+++ models/post.js
@@ -1,9 +1,14 @@
 var mongoose = require('mongoose');
 
-var PostSchema = new mongoose.Schema({
-  message: String,
+var PostSchema = new mongoose.Schema({ 
+  message: String,},
+  { timestamps: { createdAt: 'created_on', updatedAt: 'updated_on' } 
 });
 
 var Post = mongoose.model('Post', PostSchema);
 
 module.exports = Post;
+
+
+//   { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+// );
