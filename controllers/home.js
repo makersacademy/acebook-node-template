9fec2ef6669c7e68f765e01499f3fed831e11971
Heather Dalton <79050392+hjdalton@users.noreply.github.com>
2021-09-03 12:18:20 +0100

    mjest and cypress tests pass, user schema to be implemented

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,17 @@
+var User = require('../models/users');
+
 var HomeController = {
   Index: function(req, res) {
     res.render('home/index', { title: 'Acebook' });
+  },
+
+  New: function(req, res) {
+    var user = new User(req.body);
+    user.save(function(err) {
+      if (err) { throw err; }
+
+      res.status(201).redirect('/posts');
+    });
   }
 };
 
