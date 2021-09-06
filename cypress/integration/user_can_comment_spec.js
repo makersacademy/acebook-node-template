a1f911c0a429b40dcfc25a74297a261ee1dc6083
Dylan <Dylan@eequ.org>
2021-09-06 11:14:34 +0100

    added a feature test for commenting

---
+++ cypress/integration/user_can_comment_spec.js
@@ -0,0 +1,12 @@
+describe('Comments', function() {
+  it('can comment and see there comments displayed', function() {
+    cy.visit('/posts');
+    cy.contains('comment').click();
+
+    cy.get('#new-comment-form').find('[type="text"]').type('Comment1');
+    cy.get('#new-comment-form').submit();
+
+    // This test should be refactored to test for the comment by id instead of possibly allowing coincidences to past the test
+    cy.get('/posts/comments').should('contain', 'Comment1');
+  });
+});
