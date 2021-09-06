850094d6c73598bc847eeff781fc214134529215
Dylan <Dylan@eequ.org>
2021-09-06 11:55:25 +0100

    fixed routing moved comments.hbs into posts folder as it makes more sense, going to create a comment controller

--- routes/posts.js
+++ routes/posts.js
@@ -7,5 +7,6 @@ router.get('/', PostsController.Index);
 router.post('/', PostsController.Create);
 router.get('/new', PostsController.New);
 router.post('/delete', PostsController.Remove);
+router.get('/comments', PostsController.Comment);
 
 module.exports = router;
