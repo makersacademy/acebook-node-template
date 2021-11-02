0e6d15c502492fe996d15050b7041c61fa5a4079
Dan Marshall <daniel.o.marshall@gmail.com>
2021-11-02 16:07:51 +0000

    Added created at date to posts and small formatting for list of posts

--- models/post.js
+++ models/post.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
   message: String,
-});
+  },
+  { timestamps: true }  
+);
 
 var Post = mongoose.model('Post', PostSchema);
 
