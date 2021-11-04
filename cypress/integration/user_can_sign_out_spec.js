b25408695b6d4cf40c924e65c9054c4b007f7804
Dan Marshall <daniel.o.marshall@gmail.com>
2021-11-04 14:37:34 +0000

    implemented if/else on layout page

--- cypress/integration/user_can_sign_out_spec.js
+++ cypress/integration/user_can_sign_out_spec.js
@@ -8,5 +8,18 @@ describe("Sign out", function() {
         cy.visit("/posts");
         cy.get(".login-form").should("exist");
     });
-    // it("should not have a sign out button for non-logged in users");
+
+    it("should not have a sign out button for non-logged in users", function() {
+        cy.visit('/');
+        cy.get('#no-session-state').should('exist');
+        cy.get('#logout-form').should('not.exist')
+    });
+
+    it("should have a logout button for logged in users", function() {
+        cy.visit('/');
+        cy.signInUser("freddy", "12345");
+        
+        cy.get('#no-session-state').should('not.exist');
+        cy.get('#logout-form').should('exist')
+    });
 });
