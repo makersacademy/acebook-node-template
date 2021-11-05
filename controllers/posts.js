97d9bec2661c96354bafb8c47f99cab213cb09dd
parsa <parsamesg@gmail.com>
2021-11-05 11:15:58 +0000

    create unique image file names for images in posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,45 +1,56 @@
-var Post = require('../models/post');
+var Post = require("../models/post");
+var { nanoid } = require("nanoid");
 
 var PostsController = {
-  Index: function(req, res) {
-    Post.find(function(err, posts) {
-      if (err) { 
-        throw err; 
+  Index: function (req, res) {
+    Post.find(function (err, posts) {
+      if (err) {
+        throw err;
       }
-      res.render('posts/index', { posts: posts, title: "Posts" });
-    }).sort({ createdAt: 'desc' });
+      res.render("posts/index", { posts: posts, title: "Posts" });
+    }).sort({ createdAt: "desc" });
   },
 
-  New: function(req, res) {
-    res.render('posts/new', { title: "New Post" });
+  New: function (req, res) {
+    res.render("posts/new", { title: "New Post" });
   },
-  
-  Create: function(req, res) {
+
+  Create: function (req, res) {
     if (req.files && req.files.image) {
       const img = req.files.image;
       img.name = img.name.replaceAll(/\s/g, "_");
+      // keep image extension (like .jpeg) to later append onto the unique image name
+      const imageNameExtension = img.name.split(".")[1];
+      console.log(imageNameExtension);
+      console.log(img.name);
+      // nanoid returns random string, and append the original image extension onto it
+      img.name = `${nanoid()}.${imageNameExtension}`;
       console.log(img.name);
       const uploadPath = `/images/post_imgs/${img.name}`;
-      img.mv(`public${uploadPath}`, function(err) {
+      img.mv(`public${uploadPath}`, function (err) {
         if (err) {
           return res.status(500).send(err);
         }
         req.body.imageLink = uploadPath;
         const post = new Post(req.body);
-        post.save(function(err) {
-          if (err) { throw err; }
+        post.save(function (err) {
+          if (err) {
+            throw err;
+          }
 
-          res.status(201).redirect('/posts');
+          res.status(201).redirect("/posts");
         });
       });
     } else {
       const post = new Post(req.body);
       post.save((err) => {
-        if (err) { throw err; }
-        res.status(201).redirect('/posts');
+        if (err) {
+          throw err;
+        }
+        res.status(201).redirect("/posts");
       });
     }
-  }
+  },
 };
 
 module.exports = PostsController;
