ac47cf165b97493575b69c01b98983214f6d6243
Dan Marshall <daniel.o.marshall@gmail.com>
2021-11-05 11:42:30 +0000

    Added date formatting for posts to be relative to current time

---
+++ js_helpers.js
@@ -0,0 +1,26 @@
+const timeDifference = (previous) => {
+  var current = Date.now();
+  var msPerMinute = 60 * 1000;
+  var msPerHour = msPerMinute * 60;
+  var msPerDay = msPerHour * 24;
+
+  var elapsed = current - previous;
+
+  if (elapsed < msPerMinute) {
+    return Math.round(elapsed / 1000) + " seconds ago";
+  } else if (elapsed < msPerHour) {
+    return Math.round(elapsed / msPerMinute) + " minutes ago";
+  } else if (elapsed < msPerDay) {
+    return Math.round(elapsed / msPerHour) + " hours ago";
+  } else {
+    var options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return previous.toLocaleDateString("en-UK", options);
+  }
+};
+
+module.exports = timeDifference;
