0b71016a49f950168293db282719f15bf5fcb585
Anmol <bhatianmol566@yahoo.co.uk>
2021-11-08 15:10:42 +0000

    Add tests for like functionality

--- cypress/integration/user_likes_spec.js
+++ cypress/integration/user_likes_spec.js
@@ -1,18 +1,51 @@
-describe("Creating posts", function () {
-  before(async (done) => {
-    await cy.task("db:drop:all");
+describe('Creating posts', function () {
+  beforeEach(async done => {
+    await cy.task('db:drop:all');
     done();
   });
 
-  it("allows a user to like a post", function () {
+  // eslint-disable-next-line cypress/no-async-tests
+  it('allows a user to like a post', function () {
     cy.visitSignUpPage();
-    cy.signUpNewUser("Barry Scott", "example");
+    cy.signUpNewUser('Barry Scott', 'example');
 
-    cy.makeTestPost();
+    cy.makeTestPostWithoutImage();
 
-    cy.contains("Barry Scott").get(".like-icon").click();
-    // cy.find("contains", "Barry Scott").get(".like-counter").should ('eq', 1);
+    cy.contains('Barry Scott').get('.like-button').click();
+    cy.reload();
+    cy.contains('Barry Scott').get('.like-counter').should('contain', '1');
   });
 
-});
+  it('allows multiple users to like a post', function () {
+    cy.visitSignUpPage();
+    cy.signUpNewUser('Barry Scott', 'example');
 
+    cy.makeTestPostWithoutImage();
+
+    cy.contains('Barry Scott').get('.like-button').click();
+
+    cy.logOutUser();
+    cy.visitSignUpPage();
+    cy.signUpNewUser('John Wick', 'pewpew');
+
+    cy.contains('Barry Scott').get('.like-counter').should('contain', '1');
+
+    cy.contains('Barry Scott').get('.like-button').click();
+    cy.contains('Barry Scott').get('.like-counter').should('contain', '2');
+  });
+
+  it('allows a user to unlike a post', function () {
+    cy.visitSignUpPage();
+    cy.signUpNewUser('Barry Scott', 'example');
+
+    cy.makeTestPostWithoutImage();
+
+    cy.contains('Barry Scott').get('.like-button').click();
+    cy.reload();
+    cy.contains('Barry Scott').get('.like-counter').should('contain', '1');
+
+    cy.contains('Barry Scott').get('.like-button').click();
+    cy.reload();
+    cy.contains('Barry Scott').get('.like-counter').should('contain', '0');
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -73,3 +73,9 @@ Cypress.Commands.add('makeTestPost', function () {
   cy.get('#new-post-form').find('[type="file"]').attachFile('../panda.jpg');
   cy.get('#new-post-form').submit();
 });
+
+Cypress.Commands.add('makeTestPostWithoutImage', function () {
+  cy.visit('/posts/new');
+  cy.get('#new-post-form').find('[type="text"]').type('Panda');
+  cy.get('#new-post-form').submit();
+});
