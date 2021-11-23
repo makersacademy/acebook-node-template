53a060952c0fdcc95d6a3b0d4fd57812c2af5dd7
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-11-23 16:49:12 +0000

    user.spec test passes and data is saved in the test database

--- spec/models/user.spec.js
+++ spec/models/user.spec.js
@@ -4,7 +4,7 @@ require('../mongodb_helper')
 var User = require('../../models/user');
 
  
-describe('Post model', function() {
+describe('User model', function() {
     beforeEach(function(done) {
         mongoose.connection.collections.users.drop(function() {
             done();
@@ -17,5 +17,20 @@ describe('Post model', function() {
       expect(user.email).toEqual('test@test.com');
       expect(user.password).toEqual('1234');
     });
+
+    it('can save a account', function(done) {
+        var user = new User({ email: 'test@test.com', password: '1234' });
+    
+        user.save(function(err) {
+          expect(err).toBeNull();
+    
+          User.find(function(err, user) {
+            expect(err).toBeNull();
+    
+            expect(user[0]).toMatchObject({ email: 'test@test.com', password: '1234' });
+            done();
+          });
+        });
+      });
 
 });
