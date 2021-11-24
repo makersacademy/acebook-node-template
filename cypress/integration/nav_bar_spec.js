5a0e7312d5531a29be73bbdb318328287eb88890
PatrickBarsch <PatrickGB@gmx.de>
2021-11-24 13:40:16 +0100

    Fixed failing Navbar tests, deleted redundant home link

--- cypress/integration/nav_bar_spec.js
+++ cypress/integration/nav_bar_spec.js
@@ -1,20 +1,20 @@
 describe('Navbar', function() {
   it('shows up on homepage', function() {
     cy.visit('/');
-    cy.get('nav#aceBookNavBar').should('contain', 'Navbar');
+    cy.get('nav#aceBookNavBar').should('contain', 'Acebook');
   });
   it('can be used to navigate to the signup page', function() {
     cy.visit('/');
     cy.get('nav#aceBookNavBar').should('contain', 'Sign Up');
     cy.get('a#sign-up').click()
     cy.url().should('eq', 'http://localhost:3030/users/new');
-    cy.get('nav#aceBookNavBar').should('contain', 'Navbar');
+    cy.get('nav#aceBookNavBar').should('contain', 'Acebook');
   });
   it('can be used to navigate to the signin page', function() {
     cy.visit('/');
     cy.get('nav#aceBookNavBar').should('contain', 'Sign In');
     cy.get('a#sign-in').click()
     cy.url().should('eq', 'http://localhost:3030/sessions/new');
-    cy.get('nav#aceBookNavBar').should('contain', 'Navbar');
+    cy.get('nav#aceBookNavBar').should('contain', 'Acebook');
   });
 });
