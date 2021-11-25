8edda85c57cfe87dc2cb9443934293847315de74
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-11-25 07:38:12 +0000

    user spec tests still in progress

--- spec/models/user.spec.js
+++ spec/models/user.spec.js
@@ -1,35 +1,65 @@
 var mongoose = require('mongoose');
+var User = require('../../models/user');
 
 require('../mongodb_helper')
-var User = require('../../models/user');
 
  
 describe('User model', function() {
     beforeEach(function(done) {
         mongoose.connection.collections.users.drop(function() {
             done();
-        });
-    });
-  
+        }); //close function 
+    }); //closed before 
+
     it('user has an email and password property', function() {
       var user = new User({ email: 'test@test.com', password: '1234' });
       expect(user.email).toEqual('test@test.com');
       expect(user.password).toEqual('1234');
-    });
+    }); //closes it 
  
-    it('can save a user and retrive user from the database', function(done) {
-        var user = new User({ email: 'test@test.com', password: '1234' });
-    //this saves the data into the database 
-        user.save(function(err) {
-          expect(err).toBeNull();
-    //retrieving user from the database
-          User.find(function(err, user) {
-            expect(err).toBeNull();
+    // it('can save a user and retrive user from the database', function(done) { 
+    //   var user = new User({ email: 'test@test.com', password: '1234' });
+      
+    //     user.save(function(err) { 
+    //       expect(err).toBeNull();
+        
+    //       User.find(function(err, user) {
+    //         expect(err).toBeNull();
     
-            expect(user[0]).toMatchObject({ email: 'test@test.com', password: '1234' });
-            done();
-          });
-        });
-      });
+    //         expect(user[0]).toMatchObject({ email: 'test@test.com', password: '1234' });
+    //         done();
+    //       }); 
+      
+    //   });
+    // }); 
 
-});
+      it('Can test if password is encrypted', function(done) {
+        var user = new User({ email: 'happy@test.com', password: '098' });
+        user.save(function(err) {
+          expect(err).toBeNull(); 
+          done();  // should be created in the helper? 
+        });
+     
+        
+
+        User.findOne({ email: 'happy@test.com' }, function(err, user) {
+          if (err) throw err; 
+
+          user.comparePassword('098', function(err, isMatch) {
+            if (err) throw err;
+            console.log('098:', isMatch); // -> Password123: true
+          }); 
+        }); 
+       
+        user.comparePassword('4321', function(err, isMatch) {
+          if (err) throw err;
+          console.log('4321:', isMatch); // -> 123Password: false
+        });
+        
+
+      }); 
+
+}); // describe closed 
+
+
+
