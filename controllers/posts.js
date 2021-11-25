de54c99fb9edb2ea604ae044579e4b8fe4d8805f
PatrickBarsch <PatrickGB@gmx.de>
2021-11-25 16:03:40 +0100

    WIP - username next to post

--- controllers/posts.js
+++ controllers/posts.js
@@ -12,7 +12,11 @@ var PostsController = {
     res.render('posts/new', {});
   },
   Create: function(req, res) {
-    var post = new Post(req.body);
+    // which user is creating this post?
+    const user_name = null
+    const user_id = null
+    // how do we find out which user loged in?
+    var post = new Post(req.body, user_name, user_id);
     post.save(function(err) {
       if (err) { throw err; }
 
