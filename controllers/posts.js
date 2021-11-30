c55cd10d2795a3a9bafb5b418144dc3fb9999531
Mia Gordon <mia.gordon@digital.justice.gov.uk>
2021-11-30 10:47:20 +0000

    navbar is now dynamic

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,14 +1,17 @@
 var Post = require('../models/post');
 
+// add new var for dynamic nav bar and added to line 10 and 14
+var activeUser = true;
+
 var PostsController = {
   Index: function(req, res) {
     Post.find({}).sort('-createdAt').exec(function(err, posts) {
       if (err) { throw err; }
-      res.render('posts/index', { posts: posts });
+      res.render('posts/index', {loggedIn: activeUser, posts: posts });
     });
   },
   New: function(req, res) {
-    res.render('posts/new', {});
+    res.render('posts/new', {loggedIn: activeUser});
   },
   Create: function(req, res) {
     const post = new Post({
