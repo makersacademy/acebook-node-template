bed5527d78383c671649a1217fa4dbb4cdf7bf93
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-12-02 15:13:17 +0000

    nav bar is dynamic and all tests pass

--- cypress/integration/nav_bar_spec.js
+++ cypress/integration/nav_bar_spec.js
@@ -3,7 +3,7 @@ const testHelper = require('../support/commands.js')
 describe('Navbar', function() {
   it('shows up on homepage', function() {
     cy.visit('/');
-    cy.get('nav#aceBookNavBar').should('contain', 'Acebook');
+    cy.get('nav#aceBookNavBar').should('be.visible');
   });
 
   it('can be used to navigate to the signup page', function() {
@@ -11,7 +11,7 @@ describe('Navbar', function() {
     cy.get('nav#aceBookNavBar').should('contain', 'Sign Up');
     cy.get('a#sign-up').click()
     cy.url().should('eq', 'http://localhost:3030/users/new');
-    cy.get('nav#aceBookNavBar').should('contain', 'Acebook');
+    cy.get('nav#aceBookNavBar').should('be.visible');
   });
 
   it('can be used to navigate to the signin page', function() {
@@ -19,15 +19,15 @@ describe('Navbar', function() {
     cy.get('nav#aceBookNavBar').should('contain', 'Sign In');
     cy.get('a#sign-in').click()
     cy.url().should('eq', 'http://localhost:3030/sessions/new');
-    cy.get('nav#aceBookNavBar').should('contain', 'Acebook');
+    cy.get('nav#aceBookNavBar').should('be.visible');
   });
 
-  it('only shows log out and post button once signed in', function() {
+  it('only shows log out and timeline button once signed in', function() {
 
-    testHelper.signUpTestUser('123@123.com', '123')
-    testHelper.loginTestUser('123@123.com', '123')
-    cy.get('nav#aceBookNavBar').should('contain', 'Log Out');
-    cy.get('nav#aceBookNavBar').should('contain', 'Timeline');
+    testHelper.signUpTestUser('123@123.com', '123'); 
+    testHelper.loginTestUser('123@123.com', '123');
+    cy.get('a#new-post').should('contain', 'Timeline');  
+    cy.get('a#log-out').should('contain', 'Log Out'); 
   });
 
   it('shows active button ', function() {
@@ -35,7 +35,7 @@ describe('Navbar', function() {
     cy.get('nav#aceBookNavBar').should('contain', 'Sign In');
     cy.get('nav#aceBookNavBar').should('contain', 'Sign Up');
     cy.get('a#sign-in').click();
-    cy.get('a#sign-in').should('be.active');
+    cy.get('a#sign-in').should('have.class', 'nav-item nav-link active');
   });
 
-});
+}); 
--- public/javascripts/activePage.js
+++ public/javascripts/activePage.js
@@ -5,9 +5,12 @@ window.addEventListener('DOMContentLoaded', () => {
   const SIGN_IN_PAGE = document.getElementById("sign-in");
   const SIGN_UP_PAGE = document.getElementById("sign-up");
 
-  if(title === "Sign-in"){
-    SIGN_IN_PAGE.classList.add("active")
-  } else {
-    SIGN_UP_PAGE.classList.add("active");
+
+  if(title === "Sign-in" || title === "Sign-up") {
+    if(title === "Sign-in"){
+      SIGN_IN_PAGE.classList.add("active")
+    } else {
+      SIGN_UP_PAGE.classList.add("active");
+    }
   }
 })
