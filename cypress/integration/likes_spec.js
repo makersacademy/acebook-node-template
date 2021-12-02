c825bfb949f21b6d54fe9b5c9d9ce0331335a499
PatrickBarsch <PatrickGB@gmx.de>
2021-12-02 11:38:37 +0100

    Made one like per user test more thorough

--- cypress/integration/likes_spec.js
+++ cypress/integration/likes_spec.js
@@ -37,13 +37,22 @@ describe('Likes', function() {
       cy.get('#0').find('span').should('be.empty')
       cy.get('#0').find('.like').click();
       cy.get('#0').find('span').should('contain', '1')
-
+      
       cy.get('#LogOut').click();
-
+      
       testHelper.loginTestUser('123@123.com', '123')
       cy.visit('/posts');
       
       cy.get('#0').find('span').should('contain', '1')
+      cy.get('#LogOut').click();
+
+      testHelper.signUpTestUser('456@456.com', '456')
+      testHelper.loginTestUser('456@456.com', '456')
+      
+      cy.get('#0').find('span').should('contain', '1')
+      cy.get('#0').find('.like').click();
+      cy.get('#0').find('span').should('contain', '2')
+
     });
 
 });
