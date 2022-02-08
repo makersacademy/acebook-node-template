7862e9ef33b583404c3c652b687834a73ab05036
Jackie Benn <jackiebenn84@hotmail.com>
2022-02-08 12:38:49 +0000

    fixed error

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -1,8 +1,15 @@
 describe('Timeline', function() {
   it('can submit posts and view them', function() {
+    cy.visit('/sessions/new');
+    cy.get('#email')
+      .type('jackie@jackie.com')
+    cy.get('#password')
+      .type('jackie')
+    cy.get('#new-session-form').submit();
     cy.visit('/posts');
-    cy.contains('New post').click();
-
+    cy.url().should('include', '/posts');
+    cy.contains('Timeline').click();
+    cy.get('.new-post-link').click()
     cy.get('#new-post-form').find('[type="text"]').type('Hello, world!');
     cy.get('#new-post-form').submit();
 
