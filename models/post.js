18066f80ebec8dc6480a49df4a7f45662e943082
jyoung-gds <jonathan.young@digital.cabinet-office.gov.uk>
2022-02-10 17:26:34 +0000

    Changed data types in database to String from Object

--- models/post.js
+++ models/post.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
-  message: Object,
-  postedBy: Object
+  message: String,
+  postedBy: String
 });
 
 var Post = mongoose.model('Post', PostSchema);
