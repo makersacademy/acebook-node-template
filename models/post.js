159d0499e4e09da0b60dc24dc83fb6ff8b03f5b1
jyoung-gds <jonathan.young@digital.cabinet-office.gov.uk>
2022-02-11 10:22:09 +0000

    Added timestamp to posts, added first name and surname to signup page

--- models/post.js
+++ models/post.js
@@ -3,7 +3,9 @@ var mongoose = require('mongoose');
 var PostSchema = new mongoose.Schema({
   message: String,
   postedBy: String
-});
+  },
+  { timestamps: true},
+);
 
 var Post = mongoose.model('Post', PostSchema);
 
--- models/user.js
+++ models/user.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  firstname: String,
+  surname: String
 });
 
 var User = mongoose.model('User', UserSchema);
