c53a6011a632676927af32656b9fb615afc34924
jyoung-gds <jonathan.young@digital.cabinet-office.gov.uk>
2022-02-15 10:28:15 +0000

    Working comments and formatted posts page

--- controllers/posts.js
+++ controllers/posts.js
@@ -12,15 +12,37 @@ var PostsController = {
     res.render('posts/new', {});
   },
   Create: function(req, res) {
-    var post = new Post({ 
-      message: req.body.message, 
-      postedBy: req.session.user._id
+    var post = new Post({
+      message: req.body.message,
+      posterID: req.session.user._id,
+      posterName: req.session.user.firstname,
+      comments: {}
     });
     post.save(function(err) {
       if (err) { throw err; }
 
       res.status(201).redirect('/posts');
     });
+  },
+  NewComment: function(req, res) {
+    const options = { month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }
+    Post.findByIdAndUpdate(
+      req.body.postid, 
+      { $push: {
+         comments: {
+            comment: req.body.comment,
+            user: req.session.user.firstname,
+            datetime: new Date().toLocaleDateString("en-GB", options),
+         }
+        }
+      }, 
+      {new:true}, 
+      function(err) {
+        if (err) { throw err; }
+      }
+    )
+
+    res.redirect('/posts');
   }
 };
 
