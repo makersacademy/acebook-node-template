fc9e80b207c627aa6b83a7cb5cec40294b1ac2af
Jack <winterschladen.j@gmail.com>
2022-03-15 17:22:50 +0000

    Updated tests to reflect new changes

--- cypress/integration/user_can_sign_in_spec.js
+++ cypress/integration/user_can_sign_in_spec.js
@@ -1,9 +1,11 @@
 describe("Authentication", () => {
   it("A user signs in and is redirected to /posts", () => {
     // sign up
-    cy.visit("/users/new");
+    cy.visit("/");
+    cy.get("#signup").click();
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
+    cy.get("#username").type("someone123");
     cy.get("#submit").click();
 
     // sign in
