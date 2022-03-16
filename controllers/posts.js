0960df77bf23b166f77dc6330184e5448c0b390c
Charlotte Buchan <cebuc@talktalk.net>
2022-03-16 16:30:02 +0000

    added a user to the posts. Need to convert it to a name rather than ID

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,11 +2,10 @@ const Post = require("../models/post");
 
 const PostsController = {
   Index: (req, res) => {
-    Post.find({}, 'message createdAt', {sort: {'createdAt': -1}},(err, posts) => {
+    Post.find({}, 'message createdAt user', {sort: {'createdAt': -1}},(err, posts) => {
       if (err) {
         throw err;
       }
-
       res.render("posts/index", { posts: posts });
     });
   },
@@ -14,7 +13,8 @@ const PostsController = {
     res.render("posts/new", {});
   },
   Create: (req, res) => {
-    const post = new Post(req.body);
+    const post = new Post({user: req.session.user._id, message: req.body.message});
+    console.log(req.user)
     post.save((err) => {
       if (err) {
         throw err;
