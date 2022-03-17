a7ab118001de7b2f3e0e24942dc8e5c869502875
Chris Middleton <ctjmiddleton@gmail.com>
2022-03-17 15:45:08 +0000

    fixed 1 test

--- cypress/integration/nav_bar_spec.js
+++ cypress/integration/nav_bar_spec.js
@@ -1,14 +1,18 @@
+
+
   // describe('Nav bar', () => {
+
+  
   //   it('should jump to the next page', () => {
   //     // sign up
   //     cy.visit("/users/new");
-  //     cy.get("#email").type("someone@example.com");
+  //     cy.get("#email").type("someone1@example.com");
   //     cy.get("#password").type("password");
   //     cy.get("#submit").click();
 
   //     // sign in
   //     cy.visit("/sessions/new");
-  //     cy.get("#email").type("someone@example.com");
+  //     cy.get("#email").type("someone1@example.com");
   //     cy.get("#password").type("password");
   //     cy.get("#submit").click();
 
