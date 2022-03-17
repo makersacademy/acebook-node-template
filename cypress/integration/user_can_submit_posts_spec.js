4efebcf22b1e8dcf5bf98623fbae14a8087db79a
Ben Partington <bdpartington@gmail.com>
2022-03-17 11:54:37 +0000

    Started to write test for adding comment box

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -50,4 +50,29 @@ describe("Timeline", () => {
     cy.get("#new-post-form").submit();
     cy.get(".posts").eq(0).should("contain", "Hello Universe!")
   });
+
+  it('allows a user to comment on a post', () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // submit a post
+    cy.visit("/posts");
+    cy.contains("New post").click();
+
+    cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
+    cy.get("#new-post-form").submit();
+
+    /* click on comment button
+    
+    */
+  })
 });
