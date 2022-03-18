ff7410f2410e13980ad826721ea3f65989e5b03c
Ben Partington <bdpartington@gmail.com>
2022-03-18 16:46:11 +0000

    Like button functionality

--- models/post.js
+++ models/post.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema({
   message: String,
   comment: String,
+  likes: { type: Number, default: 0 },
   user: 
     { type: mongoose.Schema.Types.ObjectId, ref: 'User'}
   
