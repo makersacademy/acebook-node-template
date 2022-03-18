ff7410bfddc94fae3b4d88b9b49f47de1c8cd6b8
Ben Partington <bdpartington@gmail.com>
2022-03-18 10:28:18 +0000

    Added Comment Model and tests to enable commenting on post

--- models/comment.js
+++ models/comment.js
@@ -2,13 +2,12 @@ const mongoose = require("mongoose");
 
 const CommentSchema = new mongoose.Schema({
   comment: String,
-  // user: 
-  //   { type: mongoose.Schema.Types.ObjectId, ref: 'User'}
-});
-
+  user: 
+    { type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+},
+{ timestamps: true });
 
 const Comment = mongoose.model("Comment", CommentSchema);
 
 module.exports = Comment;
 
-// { timestamps: true });
--- spec/models/comment.spec.js
+++ spec/models/comment.spec.js
@@ -1,22 +1,60 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 require("../mongodb_helper");
-var Post = require("../../models/post");
+const Post = require("../../models/post");
 const User = require("../../models/user");
 const Comment = require("../../models/comment");
 
 describe("Comment model", () => {
-  beforeEach((done) => {
-    mongoose.connection.collections.users.drop(() => {
-      done();
-    });
+  beforeEach( async () => {
+
+    await  mongoose.connection.collections.comments.remove({})
   });
 
-  it("has a comment", () => {
-    var remark = new Comment({ comment: "I'm a comment" });
+  it("has a comment", async () => {
+    const remark = new Comment({ comment: "I'm a comment" }); 
+    await remark.save();
     expect(remark.comment).toEqual("I'm a comment");
+    
   });
 
+  it("has a timestamp", async () => {
+    const remark = new Comment({ comment: "Testing timestamp on comment" });
+    await remark.save();
+    const currentTime = new Date();
+    expect(remark.createdAt.setMilliseconds(0)).toEqual(currentTime.setMilliseconds(0));
+  });
 
+  it("has a user assigned to the comment", async () => {
+    const user = new User({ firstName: "first name", 
+    surName: "surname", 
+    email: "email", 
+    password: "password"})
+    await user.save() 
+    
+    const remark = new Comment({
+    comment: "comment for testing",
+    user: user._id 
+     
+    });
+    await remark.save()
+    
+ 
+    expect(remark.user).toEqual(user._id)
+   });
 
+   it("can list all comments on a post", async () => {
+    let remarks = await Comment.find(); 
+    expect(remarks).toEqual([]);
+  });
+
+  it("can save a comment", async ()  => {
+  
+    const remark = new Comment({ comment: "some comment" });
+    await remark.save();      
+    let comments = await Comment.find()   
+    
+    expect(comments[0].comment).toEqual("some comment");       
+      
+  });
 });
