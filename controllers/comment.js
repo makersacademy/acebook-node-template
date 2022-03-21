8704f0e15a5d382b0c5033dc713c9a630ef9463b
Chloe Stokes <chloeemstokes@gmail.com>
2022-03-21 14:42:45 +0000

    Created a TODO list for the comments page

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,18 +1,22 @@
 const Comment = require("../models/comment");
 
 const CommentController = {
+  Index: (req, res) => {
+    Comment.find({}, 'comment createdAt', )
+  }
+
   New: (req, res) => {
     res.render("comment/new", {} );
   },
 
   Create: (req,res) => {
-    const comment = new Comment({comment: req.body.comment});
+    const comment = new Comment({post: req.session.post._id, comment: req.body.comment});
     comment.save((err) => {
       if (err) {
         throw err;
       }
 
-      res.status(201).redirect("/posts");
+      res.status(201).redirect("comment/new");
     });
   },
 };
