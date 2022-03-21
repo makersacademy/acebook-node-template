5b37b6aa8865642a9307296e9e1213aaf0ba3a6d
Charlotte Buchan <cebuc@talktalk.net>
2022-03-21 16:42:54 +0000

    created a helper

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -50,34 +50,4 @@ describe("Timeline", () => {
     cy.get("#new-post-form").submit();
     cy.get(".posts").eq(0).should("contain", "Hello Universe!")
   });
-
-  it('allows a user to comment on a post', () => {
-    // sign up
-    cy.visit("/users/new");
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
-
-    // sign in
-    cy.visit("/sessions/new");
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
-
-    // submit a post
-    cy.visit("/posts");
-    cy.contains("New post").click();
-
-    cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
-    cy.get("#new-post-form").submit();
-
-    //  click on comment button
-    cy.get("#comment").click();
-    cy.url().should("include", "/comment/new");
-
-    cy.get("#comment-form").find('[type="text"]').type("This is a comment on a post");
-    cy.get("#add-comment").submit();
-
-    cy.url().should("include", "/posts");
-  })
 });
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,5 @@
+require('./signUp.js')
+
 // ***********************************************************
 // This example support/index.js is processed and
 // loaded automatically before your test files.
@@ -31,3 +33,4 @@ beforeEach(() => {
 after(() => {
   cy.task('closeDbConnection')
 })
+
