60f8f89b1b72767ff87bce1d62885223583c151c
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-03-22 16:01:45 +0000

    Aded hyperlink to profile page from a post

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -38,4 +38,17 @@ describe("Timeline", () => {
     });
 
   });
+
+  it("Can viewprofile from a post", () => {
+    cy.signUp()
+
+    cy.visit("/posts");
+    cy.contains("New post").click();
+
+    cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
+    cy.get("#new-post-form").submit();
+    cy.get('#postedByLink').eq(0).click()
+
+    cy.get("#profile").should("contain", "test name");
+  })
 });
