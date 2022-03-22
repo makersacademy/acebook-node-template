f35494e8dae02334932ea6a09705097e14150dac
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-03-22 21:27:14 +0000

    Added tests for searching, single and mulitple search results

--- cypress/integration/search_for_users_spec.js
+++ cypress/integration/search_for_users_spec.js
@@ -1,15 +1,32 @@
-describe("Search for users", () => {
-    it('searches for a user by exact match', () => {
-        cy.signUp()
-        
-        cy.contains("New post").click();
-        cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
-        cy.get("#new-post-form").submit();
 
-        // cy.get("#commentForm").submit();
-        cy.get("#new-comment-form").find('[type="text"]').type("This is a comment on a post.");
-        cy.get("#submitComment").click();
-        cy.get("#numberOfComments").should("contain", "1");
-        cy.get(".comments").should("contain", "This is a comment on a post.");
-    })
+describe("Search for users", () => {
+  it('searches for a user ', () => {
+    cy.signUp()
+
+    cy.get("#search").type("test");
+    cy.get("#search-form").submit();
+
+    cy.url().should("include", "/users");
+    cy.get("#user").should("contain", "test name");
+  })
+
+  it('searches for a user and finds two matches', () => {
+    cy.get("#signup").click();
+    cy.get("#email").type("another@example.com");
+    cy.get("#password").type("password");
+    cy.get("#username").type("someone123");
+    cy.get("#name").type("another tester")
+    cy.get("#submit").click();
+
+
+    cy.get("#search").type("test");
+    cy.get("#search-form").submit();
+
+    cy.url().should("include", "/users");
+    //cy.get('#user').should('have.length', 2)
+
+    cy.get("#users").first().should("contain", "test name");
+    cy.get("#users").last().should("contain", "another tester");
+})
+
 })
