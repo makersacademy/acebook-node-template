693113e9facabc02335b7745ba562e547215034c
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-03-23 11:39:21 +0000

    Passing test for commenter name next to a comment

--- cypress/integration/user_can_comment_on_a_post_spec.js
+++ cypress/integration/user_can_comment_on_a_post_spec.js
@@ -5,10 +5,10 @@ describe("comment on a post", () => {
         cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
         cy.get("#new-post-form").submit();
 
-        // cy.get("#commentForm").submit();
         cy.get("#new-comment-form").find('[type="text"]').type("This is a comment on a post.");
         cy.get("#submitComment").click();
         cy.get("#numberOfComments").should("contain", "1");
+        cy.get("#commenter").should("contain", "test name");
         cy.get(".comments").should("contain", "This is a comment on a post.");
     })
 })
