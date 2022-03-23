bd1a9c5fca62bc5fa33533edf4732ef275d09588
camilla <cgreene553@gmail.com>
2022-03-23 17:29:44 +0000

    like button appears but no functionality

--- makermateclient/src/components/screen/Timeline.js
+++ makermateclient/src/components/screen/Timeline.js
@@ -1,5 +1,10 @@
 //import req from 'express/lib/request';
 import React, {useState, useEffect} from 'react';
+// import cn from "classnames";
+// import { ReactComponent as Hand } from "./hand.svg";
+
+// import "./styles.scss";
+
 
 const Timeline = () => { 
   const [data, setData] = useState([])
@@ -12,25 +17,48 @@ const Timeline = () => {
       .then(result=>{
         console.log(result)
       setData(result.posts)
-    })
-  }, [])
+  })}, [])
+  
+  const likePost = (id) => {
+    // const [likes, setLiked] = useState(null);
 
+    fetch("/posts/like",{
+      method: 'put',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify({
+        _id: id
+      })
+    }).then(res=>res.json())
+    .then(result=>{
+      console.log(result)
+      }).catch(err=>{
+        console.log(err)
 
+      // setLiked(result.posts)
+    })}
 
 return(
   <div>
     {data.map(item => {
       return (
-      <>
-      <h5>{item.user}</h5>
-      <h5>{item.message}</h5>
-      <h5>{item.createdAt}</h5>
-      <h5>{item.comments}</h5>
+        <>
+      <h5 key="one">{item.user}</h5>
+      <h5 key="two">{item.message}</h5>
+      <h5 key="three">{item.createdAt}</h5>
+      <h5 key="four">{item.comments}</h5>
+      <i key="five" className="like-button" 
+      onClick={()=>{likePost()}}>
+        like
+      </i>
       </>
       )
       })}
   </div>
-)}
+) 
+    }
 
+  
 
-export default Timeline
+export default Timeline;
--- routes/posts.js
+++ routes/posts.js
@@ -13,7 +13,7 @@ router.post('/:_id/comment', PostsController.CreateComment)
 // untested new code 
 
 router.post('/delete/:_id', PostsController.Delete)
-router.post('/like/:_id', PostsController.LikeComment)
+router.put('/like/:_id', PostsController.LikeComment)
 
 // end
 
