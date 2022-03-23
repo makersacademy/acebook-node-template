964384571c94f9bb71a0b5df86b6711794dd4250
Ben Partington <bdpartington@gmail.com>
2022-03-23 17:03:08 +0000

    Profile Page finished

--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-// describe("Registration", () => {
-//   it("A user signs up and is redirected to sign in", () => {
-//     // sign up
-//     cy.visit("/users/new");
-//     cy.get("#email").type("sign_in@example.com");
-//     cy.get("#password").type("password");
-//     cy.get('#firstName').type("first name");
-//     cy.get('#surName').type('surname');
-//     cy.get("#submit").click();
-
-//     cy.url().should("include", "/sessions/new");
-//   });
-=======
 describe("Registration", () => {
   it("A user signs up and is redirected to posts", () => {
     // sign up
@@ -24,7 +10,6 @@ describe("Registration", () => {
 
     cy.url().should("include", "/posts");
   });
->>>>>>> master
 
 //   it("should return a message if the email is already in use", () => {
 //     cy.visit("/users/new");
@@ -39,5 +24,4 @@ describe("Registration", () => {
 
 //     cy.get("#error").should("have.text", "This email is already in use, please try again")
 //   });
-// });
-
+});
