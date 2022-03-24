67e0b09c69abe5d7c4c6c8017e267d1f53a3aff4
Francesco <francesco@monzo.com>
2022-03-24 11:49:36 +0100

    cleared up conflicts

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const path = require('path')
 const multer = require('multer')
-const ProfileImage = require('../models/user')
+// const ProfileImage = require('../models/user')
+const User = require('../models/user')
+const uploadTo = path.join('public', User.imagePath)
+const imageMimeTypes = ['image/jpeg', 'image/png'];
+const UsersController = require("../controllers/users");
 
 const storage = multer.diskStorage({
   destination:function(req, file, callback) {
@@ -18,22 +22,12 @@ const upload = multer({
   storage:storage,
   limits:{
     fieldSize: 1024 * 1024 * 3,
-  }
-});
-
-const User = require('../models/user')
-const path = require('path')
-const multer = require('multer')
-const uploadTo = path.join('public', User.imagePath)
-const imageMimeTypes = ['image/jpeg', 'image/png'];
-const upload = multer({
+  },
   dest: uploadTo,
   fileFilter: (req, file, callback) => {
     callback(null, imageMimeTypes.includes(file.mimetype))
   }
 })
-
-const UsersController = require("../controllers/users");
 
 router.get("/new", UsersController.New);
 router.post("/", upload.single('file'), UsersController.Create);
