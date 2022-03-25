23e77d3813999738a40576226c24245d03198562
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-03-25 09:24:32 +0000

    Test split into each sign-up element

--- cypress/integration/user_password_validation_spec.js
+++ cypress/integration/user_password_validation_spec.js
@@ -1,18 +1,23 @@
-describe("Password Validation", () => {
-  it("A user cannot sign up if providing invalid password", () => {
-    // sign up
+describe("Sign-up Validation", () => {
+  it("A user cannot sign up if providing invalid email", () => {
     cy.visit("/users/new");
     cy.get("#email").type("someoneexample.com");
     cy.get("#email").should('have.class', 'is-invalid');
+  });
 
+  it("A user cannot sign up if providing invalid first name", () => {
     cy.visit("/users/new");
     cy.get("#firstName").type("X");
     cy.get("#firstName").should('have.class', 'is-invalid');
+  });
 
+  it("A user cannot sign up if providing invalid lastname", () => {
     cy.visit("/users/new");
     cy.get("#surName").type("X");
     cy.get("#surName").should('have.class', 'is-invalid');
+  });
 
+  it("A user cannot sign up if providing invalid password", () => {
     cy.visit("/users/new");
     cy.get("#password").type("password");
     cy.get("#password").should('have.class', 'is-invalid');
