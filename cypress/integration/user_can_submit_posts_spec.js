1602f35bfa23e7a222d1063516310b917b875e8f
Christian Pinna <christian.a.pinna@gmail.com>
2022-04-12 17:17:58 +0100

    added like form to posts view including like button test can find like button and click it further steps in progreass
    Co-authored-by: jam9304 <jam9304@no-reply.github.com>

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -21,4 +21,29 @@ describe("Timeline", () => {
 
     cy.get(".posts").should("contain", "Hello, world!");
   });
+
+  it('can like a post and see the counter go up', () => {
+     // sign up
+     cy.visit("/users/new");
+     cy.get("#email").type("someone@example.com");
+     cy.get("#password").type("password");
+     cy.get("#submit").click();
+ 
+     // sign in
+     cy.visit("/sessions/new");
+     cy.get("#email").type("someone@example.com");
+     cy.get("#password").type("password");
+     cy.get("#submit").click();
+ 
+     // submit a post
+     cy.visit("/posts");
+     cy.contains("New post").click();
+ 
+     cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
+     cy.get("#new-post-form").submit();
+
+     cy.get("#like-form").find('[type="submit"]').click();
+
+
+  }); 
 });
