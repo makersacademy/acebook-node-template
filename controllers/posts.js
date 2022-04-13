ee5e0c3e2c354403e15dd3235e6fb219f9d7be90
arhussain1 <a.r.hussain@hotmail.com>
2022-04-13 23:06:46 +0100

    Comments are now visable on the main page
    - I used setTimeout with a 100ms delay to ensure to ensure comments are not missed out when page refreshes
    
    Co-authored-by: Dennis Houston <dennihous@users.noreply.github.com>

--- controllers/posts.js
+++ controllers/posts.js
@@ -15,6 +15,7 @@ const PostsController = {
   },
   Create: (req, res) => {
     const post = new Post(req.body);
+
     post.save((err) => {
       if (err) {
         throw err;
@@ -22,6 +23,21 @@ const PostsController = {
 
       res.status(201).redirect("/posts");
     });
+  },
+  Comment: (req, res) => {
+    const comment = req.body.comment;
+    const post_id = req.body.post_id;
+
+    Post.updateOne(
+      {'_id': post_id}, // filter - how to find post
+      { $push: { comments: comment}}, // update - what to update
+      (err, doc) => { // last but not least a callback because nothing works without a callback
+        if (err) {
+          throw err;
+        }
+        console.log(doc)
+      });
+    setTimeout(() => {res.redirect("/posts")}, 100);
   },
 };
 
--- cypress/integration/user_can_comment_on_posts_spec.js
+++ cypress/integration/user_can_comment_on_posts_spec.js
@@ -1,5 +1,9 @@
-describe("Timeline", () => {
-  it("can submit posts, when signed in, and view them", () => {
+describe("Commenting on posts", () => {
+  beforeEach(() => {
+    cy.task('clearposts')
+  })
+
+  it("should have a comment button which leads to comment page", () => {
     // sign up
     cy.signUp()
 
@@ -13,6 +17,30 @@ describe("Timeline", () => {
     cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
     cy.get("#new-post-form").submit();
 
-    cy.get(".posts").should("contain", "Hello, world!");
+    //this post should have a comment button
+    cy.get(".post").should("contain", "Hello, world!");
+
+    cy.get(".post").contains("#comment-button", "comment")
+  });
+
+  it("Submitting a comment should add the comment to the post", () => {
+    // sign up
+    cy.signUp()
+
+    // sign in
+    cy.signIn()
+
+    // submit a post
+    cy.visit("/posts");
+    cy.contains("New post").click();
+
+    cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
+    cy.get("#new-post-form").submit();
+
+    // submit a comment
+    cy.get("#add-comment-form").find('[type="text"]').type("Goodbye, world!");
+    cy.get("#add-comment-form").submit();
+
+    cy.get(".post").get(".comment").contains("Goodbye, world!")
   });
 });
