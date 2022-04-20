6c6f2493879c94860f6cf4dca30af1f447625db0
agagotowiec <agnieszka.gtwc@gmail.com>
2022-04-20 11:15:35 +0100

    complete

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -21,4 +21,26 @@ describe("Timeline", () => {
 
     cy.get(".posts").should("contain", "Hello, world!");
   });
+  it("a user cannot create an empty post", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // cannot submit a post
+    cy.visit("/posts");
+    cy.contains("New post").click();
+
+    cy.get("#new-post-form").find('[type="text"]').type(" ");
+    cy.get("#new-post-form").submit();
+
+    //cy.url().should("include", "/posts");
+  });
 });
