58e823c9ef014b3ce776e5fb2e2aa6e06f7adac3
Lucy Poulton <lucy@lucypoulton.net>
2022-04-20 15:11:15 +0100

    added like button to posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,13 +7,11 @@ const PostsController = {
         throw err;
       }
 
-      res.render("posts/index", { posts: posts });
+      res.render("posts/index", {posts: posts});
     });
-  },
-  New: (req, res) => {
+  }, New: (req, res) => {
     res.render("posts/new", {});
-  },
-  Create: (req, res) => {
+  }, Create: (req, res) => {
     const post = new Post(req.body);
     post.save((err) => {
       if (err) {
@@ -23,6 +21,26 @@ const PostsController = {
       res.status(201).redirect("/posts");
     });
   },
+
+  async Like(req, res) {
+    if (req.session.user && req.body.post) {
+      const user = req.session.user._id
+      /**
+       * - finds a post by id
+       * - if the user's id is in the likes array, add it
+       * - otherwise, remove it
+       */
+      await Post.findOneAndUpdate({_id: req.body.post}, [{
+        $set: {
+          likes: {
+            $cond: [{$in: [user, "$likes"]}, {$setDifference: ["$likes", [user]]}, {$concatArrays: ["$likes", [user]]}]
+          }
+        }
+      }]).exec()
+    }
+
+    res.redirect('/posts')
+  }
 };
 
 module.exports = PostsController;
