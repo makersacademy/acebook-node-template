31ab445821f1d617db43eac9aa9a7c41b9bb5255
Elham Idriss <elhamidriss@Elhams-Air.connect>
2022-04-20 11:59:01 +0100

    tests for posts in ascending order

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -21,6 +21,7 @@ describe("Timeline", () => {
 
     cy.get(".posts").should("contain", "Hello, world!");
   });
+
   it("a user cannot create an empty post", () => {
     // sign up
     cy.visit("/users/new");
@@ -42,5 +43,40 @@ describe("Timeline", () => {
     cy.get("#new-post-form").submit();
 
     //cy.url().should("include", "/posts");
+  });
+
+  it.only("can submit several posts that are in ascending order", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // submit several posts
+    cy.visit("/posts");
+    cy.contains("New post").click();
+
+    cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
+    cy.get("#new-post-form").submit();
+
+    cy.contains("New post").click();
+    cy.get("#new-post-form").find('[type="text"]').type("Good Morning!");
+    cy.get("#new-post-form").submit();
+
+    cy.contains("New post").click();
+    cy.get("#new-post-form").find('[type="text"]').type("Have a great day");
+    cy.get("#new-post-form").submit();
+    cy.log(cy.get(".posts"))
+
+    cy.get(".posts").eq(0).should("contain.text", "Have a great day")
+    cy.get(".posts").eq(1).should("contain.text", "Good Morning!")
+    cy.get(".posts").eq(2).should("contain.text", "Hello, world!")
+
   });
 });
