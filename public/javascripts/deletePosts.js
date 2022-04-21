9b7f23fcd43ca666448fb731e2cded1e3d55e0ba
GDRMVRM <gdrmvrm@gmail.com>
2022-04-21 17:11:17 +0100

    commented out the hide delete button

--- public/javascripts/deletePosts.js
+++ public/javascripts/deletePosts.js
@@ -1,7 +1,7 @@
-document.querySelectorAll(".delete-post").forEach((div) => {
-  if (!div.attributes.username.value === div.attributes.postuser.value) {
-    div.style.display = "none";
-  } else {
-    div.style.display = "block";
-  }
-});
+// document.querySelectorAll(".delete-post").forEach((div) => {
+//   if (!div.attributes.username.value === div.attributes.postuser.value) {
+//     div.style.display = "none";
+//   } else {
+//     div.style.display = "block";
+//   }
+// });
