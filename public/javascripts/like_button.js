190a8ddf894433c33ee22fbbb90f8850c2ae08b0
Daniel Taplin <98479830+dantaplin992@users.noreply.github.com>
2022-04-21 17:29:45 +0100

    new likes appear on page

--- public/javascripts/like_button.js
+++ public/javascripts/like_button.js
@@ -6,6 +6,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const userId = document.getElementsByName("user_id")[0].content
   console.log(userId)
+
+  const updateLikesText = (alreadyLiked, likesCount) => {
+    let numberOfLikes = parseInt(likesCount.textContent)
+    if(alreadyLiked) {
+      numberOfLikes--
+    } else {
+      numberOfLikes++
+    }
+    likesCount.innerText = numberOfLikes
+    console.log(`New Likes: ${numberOfLikes}`)
+  }
+
+  const updateLikesCount = (postId, likesCount) => {
+    let alreadyLiked = likesAdded.includes(postId)
+    if (alreadyLiked) {
+      for (j = 0; j < likesAdded.length; j++) {
+        if (likesAdded[j] === postId) { likesAdded.splice(j, 1) }
+      }
+    } else {
+      likesAdded.push(postId)
+    }
+    updateLikesText(alreadyLiked, likesCount)
+  }
+
+  
   
   const likeButtons = document.querySelectorAll('.like_button')
   
@@ -16,9 +41,10 @@ document.addEventListener('DOMContentLoaded', () => {
       const parent = event.target.parentElement
       const postId = parent.children[0].textContent
       const likesCount = parent.children[1]
+      updateLikesCount(postId, likesCount)
+
       console.log(likesCount.textContent)
       console.log(postId)
-      likesAdded.push(postId)
       console.log(likesAdded)
     });
   }
