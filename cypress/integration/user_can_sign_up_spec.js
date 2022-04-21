c2fae777300c6a466608ebb74a88499a56da6bff
Orlane <orlanedoumbe@gmail.com>
2022-04-21 10:35:50 +0100

    Users can't add an empty email or password and written integration tests

--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -8,4 +8,21 @@ describe("Registration", () => {
 
     cy.url().should("include", "/sessions/new");
   });
+
+  it("A user tries to sign up without an email", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    cy.url().should("include", "/users/new");
+  });
+
+  it("A user tries to sign up without a password", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#submit").click();
+    cy.url().should("include", "/users/new");
+  });
 });
