0f95cc56b18105fd399f48232d3c3198f5a9c76b
Peter Hattyar <peter.hattyar@gmail.com>
2022-04-21 17:23:59 +0100

    timestamp

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -54,10 +54,31 @@ describe("Timeline", () => {
 
     cy.get(".posts").first().should("contain", "This is a third message");
     cy.get(".posts").last().should("contain", "This is a first message");
-  //   cy.get(".posts").should("contain", "This is a third message")
-  //   .should('have.length.gt', 1)
-  //   .then((posts) => {
-  //   cy.get('.posts').should('have.length', posts.length);
-  // })
   });
+
+  it("display time of post", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // check for timestamp
+    cy.visit("/posts");
+    cy.contains("New post").click();
+
+    cy.get("#new-post-form").find('[type="text"]').type("This is a first message");
+    cy.get("#new-post-form").submit();
+
+    cy.get(".posts").should("contain", "Hello, world!");
+
+    
+  });
+
 })
--- models/post.js
+++ models/post.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema({
   message: String,
   photo: String,
+  _id: String,
 });
 
 const Post = mongoose.model("Post", PostSchema);
