1741e146b1bff49586c4acfd6e1ce1836b478b1c
Bradley Deakin <bradleydeakin2@gmail.com>
2022-04-21 12:51:24 +0100

    added test for order of posts

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -41,4 +41,34 @@ describe("Timeline", () => {
 
     cy.url().should("include", "/posts/new");
   });
+
+  it("posts appear in order with latest post first", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+    
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+    
+    // submit a post
+    cy.visit("/posts");
+    cy.contains("New post").click();
+    
+    cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
+    cy.get("#new-post-form").submit();
+
+    // submit another post
+    cy.contains("New post").click();
+
+    cy.get("#new-post-form").find('[type="text"]').type("post 2");
+    cy.get("#new-post-form").submit();
+
+    //checks order of posts
+    cy.get(".posts").first().should("contain", "post 2");
+  })
 });
