70101475d7ebad82bc6fe671c7fb880d72d1a980
GDRMVRM <gdrmvrm@gmail.com>
2022-04-22 00:30:31 +0100

    styling post, signup and signin pages

--- public/javascripts/comment.js
+++ public/javascripts/comment.js
@@ -1,1 +1,14 @@
+document.querySelectorAll("#comment-btn").forEach((button) => {
+  let postId = button.getAttribute("postID");
+  let divId = "#comment-" + postId;
 
+  let commentDiv = document.querySelector(divId);
+
+  button.addEventListener("click", () => {
+    if (commentDiv.style.display === "none") {
+      commentDiv.style.display = "block";
+    } else {
+      commentDiv.style.display = "none";
+    }
+  });
+});
--- public/javascripts/likeOnOff.js
+++ public/javascripts/likeOnOff.js
@@ -1,62 +1,58 @@
-document.querySelectorAll(".likeUnlike").forEach(button => {
-  console.log(button)
-  console.log(button.getAttribute("username"))
-  console.log(button.getAttribute("likes"))
+document.querySelectorAll(".likeUnlike").forEach((button) => {
+  console.log(button);
+  console.log(button.getAttribute("username"));
+  console.log(button.getAttribute("likes"));
 
-
-  if(button.getAttribute("likes").includes(button.getAttribute("username"))){
-    button.value = 'Unlike';
+  if (button.getAttribute("likes").includes(button.getAttribute("username"))) {
+    button.value = "Unlike";
   } else {
-    button.value = "Like"
+    button.value = "Like";
   }
-  button.addEventListener('click', event => { 
-  console.log(button)
-  console.log(event.target)
-    if(button.value === "Like") {
-      
-    //makes the Button display unlike to show the post is liked
-    
-    button.value = "Unlike" 
+  button.addEventListener("click", (event) => {
+    console.log(button);
+    console.log(event.target);
+    if (button.value === "Like") {
+      //makes the Button display unlike to show the post is liked
 
-    const username = event.target.getAttribute("username")
-    const postId = event.target.getAttribute("postId")
-    //saves the username and postID so it can be sent in the fetch request
-    const data = {username, postId}
-    const options = {
-          method: 'POST',
-          headers: {
-                    'Content-Type': 'application/json',
-                   },
-                    body: JSON.stringify(data)
-                   }
-          
-          //makes the fetch request to the specified path with the options object
-          fetch('/like', options);
-          window.location.reload();
-  } else {
- 
-    //delete like in database
+      button.value = "Unlike";
 
-    //makes the Button display like to show the post has been unliked
+      const username = event.target.getAttribute("username");
+      const postId = event.target.getAttribute("postId");
+      //saves the username and postID so it can be sent in the fetch request
+      const data = { username, postId };
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
 
-    button.value = "Like"
+      //makes the fetch request to the specified path with the options object
+      fetch("/like", options);
+      window.location.reload();
+    } else {
+      //delete like in database
 
+      //makes the Button display like to show the post has been unliked
 
-    const username = event.target.getAttribute("username")
-    const postId = event.target.getAttribute("postId")
-    //saves the username and postID so it can be sent in the fetch request
-    const data = {username, postId}
+      button.value = "Like";
 
-    const options = {
-          method: 'POST',
-          headers: {
-                    'Content-Type': 'application/json',
-                   },
-                    body: JSON.stringify(data)
-                   }
-  //makes the fetch request to the specified path with the options object
-  fetch('/unlike', options);
-  window.location.reload();
-  }
-  })
-})
+      const username = event.target.getAttribute("username");
+      const postId = event.target.getAttribute("postId");
+      //saves the username and postID so it can be sent in the fetch request
+      const data = { username, postId };
+
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+      //makes the fetch request to the specified path with the options object
+      fetch("/unlike", options);
+      window.location.reload();
+    }
+  });
+});
