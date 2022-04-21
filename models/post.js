8a63aab2a17e9353f7c3fd200cdb4af1c177d775
Michalina Klupiec <michalina@Michalinas-MacBook-Air.local>
2022-04-21 11:46:02 +0100

    added name above posts

--- models/post.js
+++ models/post.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
   message: String,
-  likes: [mongoose.Schema.Types.ObjectId]
+  likes: [mongoose.Schema.Types.ObjectId],
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
 
 const Post = mongoose.model("Post", PostSchema);
