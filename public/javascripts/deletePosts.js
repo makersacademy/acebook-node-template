34221e1929edafe8700633e7dc73c41c81e6794c
GDRMVRM <gdrmvrm@gmail.com>
2022-04-21 11:36:19 +0100

    delete posts

--- public/javascripts/deletePosts.js
+++ public/javascripts/deletePosts.js
@@ -1,7 +1,7 @@
 document.querySelectorAll(".delete-post").forEach((div) => {
-  if (div.attributes.username.value === "diego") {
-    div.style.display = "block";
-  } else {
+  if (!div.attributes.username.value === div.attributes.postuser.value) {
     div.style.display = "none";
+  } else {
+    div.style.display = "block";
   }
 });
