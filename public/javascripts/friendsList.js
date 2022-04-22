3cc440d9c0b266ac82da6fa69620ee61fcee7d14
Christian Pinna <christian.a.pinna@gmail.com>
2022-04-22 11:56:38 +0100

    displaying fineds of the post index page
    Co-authored-by: Kenny Sweeney-Baird <KennySB1@users.noreply.github.com>

--- public/javascripts/friendsList.js
+++ public/javascripts/friendsList.js
@@ -1,5 +1,7 @@
 const divEl = document.querySelector('#friends');
 const friends = divEl.getAttribute("friends").split(',');
 friends.forEach(friend => {
-    
+  const friendDivEl = document.createElement('div');
+  friendDivEl.innerText = friend;
+  divEl.append(friendDivEl);
 })
