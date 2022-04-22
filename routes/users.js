532bfc83277c14cc35a3e0c54f8716a67dd6ca11
hamolicious <kickfliptv8@gmail.com>
2022-04-22 16:03:19 +0100

    fixed undefined username error

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,6 @@ const UsersController = require("../controllers/users");
 router.get("/new", UsersController.New);
 router.post("/", UsersController.Create);
 router.get("/:username/profilepicture", UsersController.ProfilePicture);
+router.get("/profilepicture", UsersController.ProfilePicture);
 
 module.exports = router;
