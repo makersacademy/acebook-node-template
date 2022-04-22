0ecba5e8d8df564f4c48ff49c99607b075493a33
hamolicious <kickfliptv8@gmail.com>
2022-04-22 10:04:32 +0100

    removed unused imports

--- models/post.js
+++ models/post.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const { required } = require("nodemon/lib/config");
 
 const PostSchema = new mongoose.Schema({
   message: {
--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const { required } = require("nodemon/lib/config");
 
 const UserSchema = new mongoose.Schema({
 
