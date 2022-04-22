ed6e8fa7f9e527baf3b02f5d21ebdfdae31a20c4
Lucy Poulton <lucy@lucypoulton.net>
2022-04-22 14:58:57 +0100

    add dataImage helper, add user image to post, made timestamp h6 for formatting

--- util/handlebarshelpers.js
+++ util/handlebarshelpers.js
@@ -1,20 +1,22 @@
 const hbs = require("hbs");
 const moment = require("moment");
-const { ObjectId } = require("mongodb");
+const {ObjectId} = require("mongodb");
 // handlebars helper to check for object presence in array
 hbs.registerHelper("contains", function (value, array, options) {
-    // fallback...
-    array = (array instanceof Array) ? array : [array];
-    return options[array.includes(value) ? "fn" : "inverse"](this);
-  });
-  
-  // handlebars helper to check for posted image
-  hbs.registerHelper("tob64", function (value) {
-    return value.toString("base64");
-  });
+  // fallback...
+  array = (array instanceof Array) ? array : [array];
+  return options[array.includes(value) ? "fn" : "inverse"](this);
+});
 
-  // handlebars helper to give post a timestamp
-  hbs.registerHelper("id-to-timestamp", function (value) {
-    const timeStamp = new ObjectId(value).getTimestamp()
-    return moment (timeStamp).fromNow()
-  });
+// handlebars helper to check for posted image
+hbs.registerHelper("tob64", function (value) {
+  return value.toString("base64");
+});
+
+// handlebars helper to give post a timestamp
+hbs.registerHelper("id-to-timestamp", function (value) {
+  const timeStamp = new ObjectId(value).getTimestamp()
+  return moment(timeStamp).fromNow()
+});
+
+hbs.registerHelper("dataImage", (img) => `data:${img?.contentType};base64,${img?.data?.toString('base64')}`);
