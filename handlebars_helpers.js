0b2fe66a19be38001f2b7f3c3c3b988d41d11b0d
Lauren Prendergast <lc6787@aol.com>
2022-04-22 16:32:22 +0100

    Cypress test date format bug fix

--- handlebars_helpers.js
+++ handlebars_helpers.js
@@ -4,6 +4,7 @@ const hbs = require('hbs');
 module.exports = function setup() {
     hbs.registerHelper("idtotime", id => {
         const timestamp = new ObjectId(id).getTimestamp()
-    return `${timestamp.getHours()}:${timestamp.getMinutes()}, ${timestamp.getDate()}-${("0" + (timestamp.getMonth()+1)).slice(-2)}-${timestamp.getFullYear()}`})
+    return `${timestamp.getHours()}:${timestamp.getMinutes()}, ${timestamp.getDate()}-${("0" + (timestamp.getMonth()+1)).slice(-2)}-${timestamp.getFullYear()}`
+    })
     
 }
