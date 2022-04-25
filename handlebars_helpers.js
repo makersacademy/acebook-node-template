2a84f9cfb0e5e0c80f5f7b6895faa6b0027099f9
Laura Cabantous <laura.cabantous@gmail.com>
2022-04-25 17:33:35 +0100

    Implement the Like button

--- handlebars_helpers.js
+++ handlebars_helpers.js
@@ -6,4 +6,12 @@ module.exports = function setup() {
         const timestamp = new ObjectId(id).getTimestamp()
     return `${timestamp.getHours()}:${timestamp.getMinutes()}, ${timestamp.getDate()}-${("0" + (timestamp.getMonth()+1)).slice(-2)}-${timestamp.getFullYear()}`})
     
+    hbs.registerHelper("contains", (a, b, options) => {
+        b = (b instanceof Array) ? b : [b];
+        return options[b.includes(a) ? "fn" : "inverse"](this);
+    })
+
+    hbs.registerHelper({
+        eq: (v1, v2) => v1 === v2,
+        gt: (v1, v2) => v1 > v2 });
 }
