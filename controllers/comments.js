b7a0a108753869b43f4dbec4b1392b4cd4444431
stephaniesomade <stephaniesomadee@gmail.com>
2022-04-25 15:41:55 +0100

    Set up

--- controllers/comments.js
+++ controllers/comments.js
@@ -5,18 +5,12 @@ const CommentsController = {
   New: async (req, res) => {
     console.log("In the post route")
     const id = req.params.id
-    // the id is passed into the url so it is part of the request's params
-    // if you console.log(req.params) it you'll see the object that is returned
     const comment = req.body.comments
-    // This is the user input
     await Comments.create({ 
       postId: id,
       content: comment
     })
-    // creating a new mongoose document within the Comments collection with the post's ID and the user input.
-    // content is the comment submitted. 
       res.status(201).redirect(`/posts/${id}`);
-      // This is so the route will just refresh on the current page. 
   }
 };
 
