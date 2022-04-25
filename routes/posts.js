176d17c4c9843d31c95cb8834ef3c99ab94fc831
jkgiwa <jkgiwa95@gmail.com>
2022-04-25 12:56:50 +0100

    implemented edit

--- routes/posts.js
+++ routes/posts.js
@@ -6,6 +6,8 @@ const PostsController = require("../controllers/posts");
 router.get("/", PostsController.Index);
 router.post("/", PostsController.Create);
 router.get("/new", PostsController.New);
+router.get("/edit/:id", PostsController.Edit);
+router.post("/edit/:id", PostsController.SaveEdit);
 
 
 module.exports = router;
