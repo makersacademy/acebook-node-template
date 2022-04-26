71d0d76c85a3bce0174d8951b3c6056c18b6ee01
laetitia <laetitia.studer@gmail.com>
2022-04-26 15:11:16 +0100

    profile page layout

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const homeRouter = require("./routes/home");
 const postsRouter = require("./routes/posts");
 const sessionsRouter = require("./routes/sessions");
 const usersRouter = require("./routes/users");
+const profileRouter = require("./routes/profile");
 
 const app = express();
 
@@ -63,6 +64,7 @@ app.use("/", homeRouter);
 app.use("/posts", sessionChecker, postsRouter);
 app.use("/sessions", sessionsRouter);
 app.use("/users", usersRouter);
+app.use("/profile", profileRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ controllers/profile.js
@@ -0,0 +1,7 @@
+const ProfileController = {
+  Index: (req, res) => {
+    res.render("users/profile");
+  },
+};
+
+module.exports = ProfileController;
