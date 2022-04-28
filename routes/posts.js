3613f5ccf8bf2c235b611efae53799d5598c9b4c
stephaniesomade <stephaniesomadee@gmail.com>
2022-04-28 16:30:38 +0100

    Message bar on the timeline

--- routes/posts.js
+++ routes/posts.js
@@ -6,7 +6,6 @@ const CommentsController = require("../controllers/comments");
 
 router.get("/", PostsController.Index);
 router.post("/", PostsController.Create);
-router.get("/new", PostsController.New);
 router.get("/:id", PostsController.Comments);
 router.post("/likes/:id", PostsController.Likes);
 router.post("/:id", CommentsController.New);
