e1922e405de47c2b71907a4543d146c3a23c7e8f
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-05-09 14:55:12 +0100

    set up changes'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,11 @@
-{
+module.exports = {
     "env": {
         "browser": true,
+        "commonjs": true,
         "es2021": true
     },
-    "extends": [
-        "google"
-    ],
     "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+        "ecmaVersion": "latest"
     },
     "rules": {
     }
