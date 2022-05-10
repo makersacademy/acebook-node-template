5a554283df2c2aebd3e9462a33b6e2e0f1e4b263
Jordan <jordanmanu@live.com.au>
2022-05-10 17:25:53 +0100

    deleted old code

--- app.js
+++ app.js
@@ -77,12 +77,4 @@ app.use((err, req, res) => {
   res.render("error");
 });
 
-// incorrect password error
-
-app.use((req, res, next) => {
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
-  next();
-});
-
 module.exports = app;
