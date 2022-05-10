3f90bb24a1119e2ee4be86ba89c62098fe5d69d5
Jordan <jordanmanu@live.com.au>
2022-05-10 11:32:20 +0100

    got the npm start working, but creating an account is causing a crash

--- models/user.js
+++ models/user.js
@@ -1,40 +1,40 @@
 var mongoose = require("mongoose"),
   Schema = mongoose.Schema,
-  bcrypt = require(bcrypt),
+  bcrypt = require("bcrypt"),
   SALT_WORK_FACTOR = 10;
 
 var UserSchema = new mongoose.Schema({
-  email: String,
-  password: String,
+  email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
-UserSchema.pre(save, function (next) {
+UserSchema.pre("save", function (next) {
   var user = this;
   // only hash the password if it has been modified (or is new)
   if (!user.isModified("password")) return next();
-});
 
-// generate a salt
-bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-  if (err) return next(err);
-
-  // hash the password along with our new salt
-  bcrypt.hash(user.password, salt, function (err, hash) {
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
     if (err) return next(err);
 
-    // override the cleartext password with the hashed one
-    user.password = hash;
-    next();
+    // hash the password along with our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
   });
 
   UserSchema.methods.comparePassword = function (canditatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    bcrypt.compare(canditatePassword, this.password, function (err, isMatch) {
       if (err) return cb(err);
       cb(null, isMatch);
     });
   };
 
   const User = mongoose.model("User", UserSchema);
-});
 
-module.exports = User;
+  module.exports = User;
+});
