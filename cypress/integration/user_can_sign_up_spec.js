5ae1ccc8b62e977ccc837ab8c9996d902ae3558b
Michael Su <mailmsuu@gmail.com>
2022-05-11 12:01:00 +0100

    added backend security on passwords, now stored as encrypted passwords in database - will need to change again in future to hide the passphrase inside a env file or so

--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -2,8 +2,8 @@ describe("Registration", () => {
   it("A user signs up and is redirected to sign in", () => {
     // sign up
     cy.visit("/users/new");
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
+    cy.get("#email").type("someone2@example.com");
+    cy.get("#password").type("password2");
     cy.get("#submit").click();
 
     cy.url().should("include", "/sessions/new");
