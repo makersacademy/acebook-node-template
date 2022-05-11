3472a87c11d1ecf0199e1ee4f59fd6982a199d73
Joe <j.newman@live.co.uk>
2022-05-11 15:46:31 +0100

    done

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,11 +2,10 @@ const Post = require("../models/post");
 
 const PostsController = {
   Index: (req, res) => {
-    Post.find((err, posts) => {
+    Post.find().populate({path: "user", select: "email"}).exec((err, posts) => {
       if (err) {
         throw err;
       }
-
       res.render("posts/index", { posts: posts });
     });
   },
@@ -14,7 +13,16 @@ const PostsController = {
     res.render("posts/new", {});
   },
   Create: (req, res) => {
-    const post = new Post(req.body);
+    const postInfo = req.body;
+    postInfo.user = req.session.user._id;
+    const post = new Post(postInfo);
+
+    console.log(req.body);
+    console.log(req.session.user);
+    console.log(req.session.user._id);
+    console.log(post);
+    console.log(postInfo);
+
     post.save((err) => {
       if (err) {
         throw err;
