3414f582f3c5392cb0bdffeb0bfbe71cef64d4e7
Joe McNally <joe.t.mcnally@hotmail.com>
2022-05-12 11:37:29 +0100

    Made a way of showing an error on the page when registration goes wrong. need to write tests.

--- controllers/users.js
+++ controllers/users.js
@@ -18,10 +18,15 @@ const UsersController = {
     const user = new User({email: req.body.email, password: encryptWithAES(req.body.password)});
     user.save((err) => {
       if (err) {
-        throw err;
+        if (err.name === "ValidationError") {
+          res.send("Email already in use"); // possible 400 status code
+        } else {
+          res.send("Oops, something went wrong!");
+        }
+      } else {
+        req.session.user = user
+        res.status(201).redirect("/posts");
       }
-      req.session.user = user
-      res.status(201).redirect("/posts");
     });
   },
 };
--- models/user.js
+++ models/user.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require('mongoose-unique-validator');
+
 
 const UserSchema = new mongoose.Schema({
-  email: String,
+  email: { type: String, unique: true },
   password: String,
 });
 
 const User = mongoose.model("User", UserSchema);
 
+UserSchema.plugin(uniqueValidator);
+
 module.exports = User;
+
