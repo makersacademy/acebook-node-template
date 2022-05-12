1f4c1bc8b3e316c3db695ca7719eb76bd750cbe8
Coral-Ann <coral.baker@outlook.com>
2022-05-12 14:38:16 +0100

    Added feature test for logout

---
+++ cypress/integration/user_can_logout_spec.js
@@ -0,0 +1,22 @@
+describe("Logging out", () => {
+  it("A user can log out to sessions/new page", () => {
+    // sign up
+    cy.visit("/");
+    cy.get("#Signup").click();
+    cy.url().should("include", "/users/new");
+    cy.get("#username").type("someone");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+    // log out
+    cy.visit("/posts");
+    cy.get("#logout").click();
+    cy.url().should("include", "/sessions/new");
+  });
+ });
+ 
