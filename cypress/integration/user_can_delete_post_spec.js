3cb90282a7cf37ddf6eb60c4ab252a07d2727a57
Michael Su <mailmsuu@gmail.com>
2022-05-13 16:25:04 +0100

    fixed tests

--- cypress/integration/user_can_delete_post_spec.js
+++ cypress/integration/user_can_delete_post_spec.js
@@ -6,14 +6,16 @@ describe("Delete button", () => {
     cy.get("#password").type("password4");
     cy.get("#submit").click();
 
+    cy.get('.post-message').find('.dltbtn').click();
+
     cy.get(".new-field").type("Delete test");
     cy.get(".post-button").click();
 
     cy.get(".posts").should("contain", "Delete test");
     cy.get(".posts").should("contain", "someone4@example.com");
 
-    cy.get('.post-message').contains('Delete test').gfind(".btn").click();
+    cy.get('.post-message').find('.dltbtn').click();
 
-    cy.get('.post-message').should("not.contain", "Delete test");
+    cy.get('.posts').should("not.contain", "Delete test");
   });
 });
