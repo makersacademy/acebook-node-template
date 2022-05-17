30c1c534f7c21e1d75ff7503b267b3505fa16cc6
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-05-17 15:14:43 +0100

    user comments are visible on the timeline , used .populate method

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 const PostsController = {
   Index: (req, res) => {
-    Post.find().populate({path: "user", select: "email"}).exec((err, posts) => {
+    Post.find().populate({path: "user", select: "email"}).populate({path : "comments", select: "message"}).exec((err, posts) => {
       if (err) {
         throw err;
       }
