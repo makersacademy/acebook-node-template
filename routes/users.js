5433b59a21cd19cd4b6633d4f6e3d94f64d19550
Michael Su <mailmsuu@gmail.com>
2022-05-17 17:38:02 +0100

    trying to upload pic

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const multer  = require('multer')
 const router = express.Router();
+
+const upload = multer({ dest: 'uploads/' })
 
 const UsersController = require("../controllers/users");
 
@@ -7,6 +10,6 @@ router.get("/new", UsersController.New);
 router.post("/", UsersController.Create);
 router.get("/:email", UsersController.Display);
 router.get("/:email/edit", UsersController.Update);
-router.post("/:email/edit", UsersController.UpdateDetails);
+router.post("/:email/edit", upload.single('image'), UsersController.UpdateDetails);
 
 module.exports = router;
