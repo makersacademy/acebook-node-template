a75452128f50845604df437953fab772b7efc62f
Anish <anishkakaiya@outlook.com>
2022-06-07 15:28:40 +0100

    add test to sign in from homepage with account pre-created

--- cypress/integration/user_can_sign_in_spec.js
+++ cypress/integration/user_can_sign_in_spec.js
@@ -1,5 +1,5 @@
 describe("Authentication", () => {
-  it("A user signs in and is redirected to /posts", () => {
+  it("A user signs in after signing up and is redirected to /posts", () => {
     // sign up
     cy.visit("/");
     cy.get("#signup").click();
@@ -15,4 +15,23 @@ describe("Authentication", () => {
     cy.url().should("include", "/posts");
     cy.contains("a", "New post");
   });
+
+  it("A user with an account signs in and is redirected to /posts", () => {
+    //sign up
+    cy.visit("/");
+    cy.get("#signup").click();
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click()
+
+    //sign in
+    cy.visit("/")
+    cy.get("#signin").click();
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    cy.url().should("include", "/posts");
+    cy.contains("a", "New post");
+  })
 });
