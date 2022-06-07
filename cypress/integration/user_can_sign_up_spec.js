1f214ee564ee0171d57746b1b81c19336a52d28c
Anish <anishkakaiya@outlook.com>
2022-06-07 14:25:38 +0100

    update test to test sign up page

--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -1,7 +1,8 @@
 describe("Registration", () => {
   it("A user signs up and is redirected to sign in", () => {
     // sign up
-    cy.visit("/users/new");
+    cy.visit("/");
+    cy.get("#signup")
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
     cy.get("#submit").click();
