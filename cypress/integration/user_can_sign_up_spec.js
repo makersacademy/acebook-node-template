6dc09874118d1187856c1d8754d7369e72ffb42a
Erlantz <erlantzram@gmail.com>
2022-06-07 11:21:24 +0100

    sign up tickect completed. Tests modify to reflect name added

--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -2,6 +2,7 @@ describe("Registration", () => {
   it("A user signs up and is redirected to sign in", () => {
     // sign up
     cy.visit("/users/new");
+    cy.get("#name").type("Test name 1");
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
     cy.get("#submit").click();
