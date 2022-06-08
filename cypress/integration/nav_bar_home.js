3d97584779def8f000067d6d6c78a37a2d8ea1c3
Archie Martin <98323890+archiemartini@users.noreply.github.com>
2022-06-08 12:17:48 +0100

    starting pull request

--- cypress/integration/nav_bar_home.js
+++
@@ -1,7 +0,0 @@
-describe("Navbar home button", () => {
-  it("navigates to the home page", () => {
-    cy.visit("/users/new");
-    cy.get("#home").click();
-    cy.url().should("include", "/?");
-  });
-});
