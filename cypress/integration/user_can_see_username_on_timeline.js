636ab7b2ba2753be6b5594176df202269dd57253
Tom O'Neill <tomoneill32@gmail.com>
2022-06-08 12:41:56 +0100

    Username is stored in db on sign up, on timeline

---
+++ cypress/integration/user_can_see_username_on_timeline.js
@@ -0,0 +1,19 @@
+describe("Authentication", () => {
+  it("A user signs in and is redirected to /posts", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@gmail.com");
+    cy.get("#password").type("password");
+    cy.get('#username').type("Jane Doe");
+    cy.get("#submit").click();
+
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("someone@gmail.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    cy.url().should("include", "/posts");
+    cy.contains("h1", "Jane Doe's Timeline");
+  });
+});
