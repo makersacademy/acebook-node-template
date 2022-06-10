04d6465cc6f1f5dfb2d82e64f4209d2a4562b247
Farzan-I <farzan_15@hotmail.co.uk>
2022-06-10 17:32:20 +0100

    bcrypt integrated for the users password on the database

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs")
 
 const UserSchema = new mongoose.Schema({
   name: String,
@@ -6,6 +7,40 @@ const UserSchema = new mongoose.Schema({
   password: String,
 });
 
-const User = mongoose.model("User", UserSchema);
+UserSchema.pre("save", function (next) {
+  const user = this
 
-module.exports = User;
+  if (this.isModified("password") || this.isNew) {
+    bcrypt.genSalt(10, function (saltError, salt) {
+      if (saltError) {
+        return next(saltError)
+      } else {
+        bcrypt.hash(user.password, salt, function(hashError, hash) {
+          if (hashError) {
+            return next(hashError)
+          }
+
+          user.password = hash
+          next()
+        })
+      }
+    })
+  } else {
+    return next()
+  }
+})
+
+UserSchema.methods.comparePassword = function(password, callback) {
+  bcrypt.compare(password, this.password, function(error, isMatch) {
+    if (error) {
+      return callback(error)
+    } else {
+      callback(null, isMatch)
+    }
+  })
+}
+
+
+
+module.exports = mongoose.model("User", UserSchema)
+
