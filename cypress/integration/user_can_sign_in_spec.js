e825984d3212c4aba4c1677f584e6a383f694618
George Dainton <gcdainton94@gmail.com>
2022-06-13 11:39:17 +0100

    User sign in and delete posts tests fixed

--- cypress/integration/user_can_sign_in_spec.js
+++ cypress/integration/user_can_sign_in_spec.js
@@ -29,7 +29,7 @@ describe("Authentication", () => {
     cy.visit("/sessions/new")
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
-    cy.get("#submit").click();
+    cy.get("#submit-signin").click();
 
     cy.url().should("include", "/posts");
     cy.contains("h1", "Timeline");
