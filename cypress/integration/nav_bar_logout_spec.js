e5871cb7d7e3e8a396919d26844a941d664326c6
Russell <russellmorbey@hotmail.co.uk>
2022-06-14 15:39:12 +0100

    Changed routes. Deleted sessions/new + users/new. Added log in and sign up to home page.

--- cypress/integration/nav_bar_logout_spec.js
+++ cypress/integration/nav_bar_logout_spec.js
@@ -3,15 +3,9 @@ const signIn = require('../support/signin_helper')
 
 describe("Navbar log-out button", () => {
   it("log-out logs out  a signed in user and navigates to new session page", () => {
-    cy.visit("/");
-    cy.get("#log-in").click();
-    cy.url().should("include", "/sessions/new");
-
     signUp("someone@example.com", "password", "username");
-
-     signIn("someone@example.com", "password");
-     cy.url().should("include", "/posts")
-    
+    signIn("someone@example.com", "password");
+    cy.url().should("include", "/posts")
     cy.get("#log-out").click();
     cy.url().should("eq", "http://localhost:3030/");
   });
--- cypress/integration/nav_bar_sign_up_button_spec.js
+++
@@ -1,7 +0,0 @@
-describe("Navbar sign-up button", () => {
-  it("navigates to the sign up page", () => {
-    cy.visit("/");
-    cy.get("#sign-up").click();
-    cy.url().should("include", "/users/new");
-  });
-});
