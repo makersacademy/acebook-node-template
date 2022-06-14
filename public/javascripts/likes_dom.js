00ba3cf86fe9a25f9b4666bb1331d234ac4dc3c2
Archie Martin <98323890+archiemartini@users.noreply.github.com>
2022-06-14 14:55:55 +0100

    No look with deleting likes, added dynamicity tho

---
+++ public/javascripts/likes_dom.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+
+elements = document.querySelectorAll('.like-form')
+elements.forEach((element) => {
+  element.addEventListener('submit', (eventObj) => {
+    console.log(element.id)
+    eventObj.preventDefault()
+    
+    options = {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        post_id: element.id
+      })
+    }
+    fetch('http://localhost:3000/posts/add_like', options).then((data) => console.log(data))
+    
+  })
+})
