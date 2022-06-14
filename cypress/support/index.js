9ecc390b56d1e4de00a87246ad0d8541c941cecb
Luke Storey <lukemstorey@gmail.com>
2022-06-14 12:24:07 +0100

    Remove unnesecarry files

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -19,23 +19,6 @@
 // Alternatively you can use CommonJS syntax:
 require("./commands");
 
-// var mongoose = require("mongoose");
-
-// require("../../spec/mongodb_helper");
-
-// beforeEach((done) => {
-//   mongoose.connection.collections.posts.drop(() => {
-//     done();
-//   });
-// });
-
-// beforeEach((done) => {
-//   mongoose.connection.collections.users.drop(() => {
-//     done();
-//   });
-// });
-require("./commands");
-
 beforeEach(() => {
   cy.exec("mongo acebook_test --eval 'db.users.remove({})'");
   cy.exec("mongo acebook_test --eval 'db.posts.remove({})'");
