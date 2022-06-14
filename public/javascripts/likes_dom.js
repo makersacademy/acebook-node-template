c62daefdd379bb50f98592b167b996b72e86bace
Archie Martin <98323890+archiemartini@users.noreply.github.com>
2022-06-14 17:46:18 +0100

    dynamic like counter changes working!

--- public/javascripts/likes_dom.js
+++ public/javascripts/likes_dom.js
@@ -1,24 +1,37 @@
 /* eslint-disable */
 
-elements = document.querySelectorAll('.like-form')
-elements.forEach((element) => {
-  element.addEventListener('submit', (eventObj) => {
-    console.log(element.id)
+posts = document.querySelectorAll('.post')
+posts.forEach((post) => {
+  like_form = post.querySelector('.like-form')
+  like_form.addEventListener('submit', (eventObj) => {
+    
     eventObj.preventDefault()
     
     options = {
       method: "POST",
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
-        post_id: element.id
+        post_id: like_form.id
       })
     }
+
     fetch('http://localhost:3000/posts/add_like', options).then((data) => console.log(data))
-    like_button = element.querySelector(".add-like-button")
+    
+    like_button = like_form.querySelector(".add-like-button")
+
     if(like_button.value === "Like") {
       like_button.value = "Unlike"
+      like_count_number = parseInt(post.querySelector('.likes-count').textContent)
+      like_count_number += 1
+      like_count_string = like_count_number.toString()
+      post.querySelector('.likes-count').textContent = like_count_string
     } else if (like_button.value === "Unlike") {
+      like_count_number = parseInt(post.querySelector('.likes-count').textContent)
       like_button.value = "Like"
+      like_count_number -= 1
+      like_count_string = like_count_number.toString()
+      post.querySelector('.likes-count').textContent = like_count_string
     }
+    
   })
 })
