2e1b0327dcbab1b325d2b10b79e0e929e7c0ea8f
Anish <anishkakaiya@outlook.com>
2022-06-15 17:37:35 +0100

    test passes

--- cypress/integration/user_can_delete_posts_spec.js
+++ cypress/integration/user_can_delete_posts_spec.js
@@ -10,18 +10,18 @@ describe("Timeline", () => {
     cy.get("#email").type("jerry@example.com");
     cy.get("#password").type("password");
     cy.get('input[type=file]').attachFile("../fixtures/earth.jpg")
-    cy.get("#submit-signup").click();
+    cy.get("#submit-signup").click({force:true});
 
     // submit a post
 
     cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
-    cy.get(".post-submit-button").click();
+    cy.get(".post-submit-button").click({force:true});
 
     cy.get(".posts").should("contain", "Hello, world!");
 
     // delete a post
 
-    cy.get('#delete-post').find('.post-delete-button').click();
+    cy.get('#delete-post').find('.post-delete-button').click({force:true});
 
     cy.get(".posts").should("not.have.value", "Hello, world!")
   });
