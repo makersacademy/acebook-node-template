24156f2fa350aaf863885f23a7c8e560dbe4fb61
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-06-16 17:00:15 +0100

    requester now print to page

--- controllers/friends.js
+++ controllers/friends.js
@@ -14,38 +14,46 @@ const FriendsController = {
     );
   },
   Request: (req, res) => {
-    User.findOne({_id: req.body.id},
-      (err, result) => {
-        if(result.requests.includes(req.session.userID)) {
+    const sessionUser = User.findById(req.session.userID);
+    const aquaintnce = User.findById(req.body.id);
+    
+    Promise.all([sessionUser, aquaintnce]).then( searchResults => {
+      console.log("line 21", searchResults)
+      User.findOne({_id: searchResults[1]._id},
+        (err, result) => {
+          if(result.requests.includes(searchResults[0]._id)) {
+            User.findOneAndUpdate(
+              {_id: searchResults[1]._id},
+              {$pull:
+                {requests: searchResults[0]}
+              }, 
+              {
+                new: true // <- setting the new option to true to return the document after update was applied
+              },
+              (err, result) => {
+                console.log(err);
+                console.log(result);
+                res.redirect('/users/profile')
+              })
+          } else {
           User.findOneAndUpdate(
-            {_id: req.body.id},
-            {$pull:
-              {requests: req.session.userID}
-            }, 
-            {
-              new: true // <- setting the new option to true to return the document after update was applied
+            {_id: searchResults[1]._id},
+            {$push:
+              {requests: searchResults[0]}
+            }, {
+              new: true
             },
-            (err, result) => {
+            (err, result) =>{ 
               console.log(err);
               console.log(result);
               res.redirect('/users/profile')
-            })
-        } else {
-        User.findOneAndUpdate(
-          {_id: req.body.id},
-          {$push:
-            {requests: req.session.userID}
-          }, {
-            new: true
-          },
-          (err, result) =>{ 
-            console.log(err);
-            console.log(result);
-            res.redirect('/users/profile')
-          }
-        )} 
-      }
-    )
+            }
+          )} 
+        }
+      )
+    })
+
+
   }
 }
 
