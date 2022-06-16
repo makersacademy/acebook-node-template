ef715fc26ee7a585a6fdc4aa1df001c1ee5f9f94
Simon Tinsley <45486136+sjtinsley@users.noreply.github.com>
2022-06-16 14:21:52 +0100

    Changes ready to merge main into here

--- public/javascripts/likes_dom.js
+++ public/javascripts/likes_dom.js
@@ -20,14 +20,19 @@ posts.forEach((post) => {
       fetch('http://localhost:3000/posts/add_like', options).then((data) => console.log(data)).catch((err) => console.log(err))
       
       like_button = like_form.querySelector(".add-like-button")
+      like_icon = like_form.querySelector('.fa-thumbs-up')
   
       if(like_button.value === "Like") {
         like_button.value = "Unlike"
+        like_icon.classList.remove("fa-regular")
+        like_icon.classList.add("fa-solid")
         like_count_number = parseInt(post.querySelector('.likes-count').textContent)
         like_count_number += 1
         like_count_string = like_count_number.toString()
         post.querySelector('.likes-count').textContent = like_count_string
       } else if (like_button.value === "Unlike") {
+        like_icon.classList.remove("fa-solid")
+        like_icon.classList.add("fa-regular")
         like_count_number = parseInt(post.querySelector('.likes-count').textContent)
         like_button.value = "Like"
         like_count_number -= 1
