4791393539f8aa0c93a8b12eb98d332c8dec4175
Anish <anishkakaiya@outlook.com>
2022-06-16 16:36:40 +0100

    add retrieval route

--- controllers/friends.js
+++ controllers/friends.js
@@ -46,7 +46,33 @@ const FriendsController = {
         )} 
       }
     )
+  },
+
+  Retrieve: (req, res) => {
+    User.find({_id: req.body.id})
+    .exec((async(err, posts) => {
+      if (err) {
+        throw err;
+      }
+      const user = await User.findOne({ _id: req.body.id }).exec();
+      const photo = {
+        contentType: user.photo.contentType,
+        data: user.photo.data.toString('base64'), // <- user photo added to profile page
+      };
+      const locations = ["Bumpass, Virginia", "Hell, Norway", "Titty Hill, England", "Sandy Balls, England"]
+      const statuses = ["A Mongoose never tells", "Already ordered 15 cats", "Depends on who's asking"]
+      res.render("friends/profile", { 
+        userName: user.userName, 
+        posts: posts, 
+        photo, 
+        age: Math.floor(Math.random() * 101),
+        location: locations[Math.floor(Math.random() * locations.length)],
+        relation_status: statuses[Math.floor(Math.random() * statuses.length)]
+      });
+    }))
   }
+  
 }
+
 
 module.exports = FriendsController;
