f58cd177c24f63cdde631b0cc37d05fd7773db01
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-07-13 15:06:57 +0100

    all unit tests for passing

--- spec/models/post.spec.js
+++ spec/models/post.spec.js
@@ -10,50 +10,55 @@ describe("Post model", () => {
     });
   });
 
-  // it("has a message", () => {
-  //   var post = new Post({
-  //     message: "another message",
-  //     firstname: "Mongo",
-  //   });
-  //   expect(post.message).toEqual("another message");
-  // });
+  it("has a message", () => {
+    var post = new Post({
+      message: "another message",
+      firstname: "Mongo",
+      likes: 0,
+    });
+    expect(post.message).toEqual("another message");
+  });
 
-  // it("can list all posts", (done) => {
-  //   Post.find((err, posts) => {
-  //     expect(err).toBeNull();
-  //     expect(posts).toEqual([]);
-  //     done();
-  //   });
-  // });
+  it("can list all posts", (done) => {
+    Post.find((err, posts) => {
+      expect(err).toBeNull();
+      expect(posts).toEqual([]);
+      done();
+    });
+  });
 
-  // it("can save a post", (done) => {
-  //   var post = new Post({
-  //     message: "another message",
-  //     firstname: "Mongo",
-  //   });
+  it("can save a post", (done) => {
+    var post = new Post({
+      message: "another message",
+      firstname: "Mongo",
+      likes: 0,
+    });
 
-  //   post.save((err) => {
-  //     expect(err).toBeNull();
+    post.save((err) => {
+      expect(err).toBeNull();
 
-  //     Post.find((err, posts) => {
-  //       expect(err).toBeNull();
+      Post.find((err, posts) => {
+        expect(err).toBeNull();
 
-  //       expect(posts[0]).toMatchObject({
-  //         message: "another message",
-  //         firstname: "Mongo",
-  //       });
-  //       done();
-  //     });
-  //   });
-  // });
+        expect(posts[0]).toMatchObject({
+          message: "another message",
+          firstname: "Mongo",
+          likes: 0,
+        });
+        done();
+      });
+    });
+  });
 
-  // it("has a User associated to it", () => {
-  //   var post = new Post({
-  //     message: "another message",
-  //     firstname: "Mongo",
-  //   });
-  //   expect(post.firstname).toEqual("Mongo");
-  // });
+  it("has a User associated to it", () => {
+    var post = new Post({
+      message: "another message",
+      firstname: "Mongo",
+      likes: 0,
+    });
+    expect(post.firstname).toEqual("Mongo");
+  });
+
   it("has a like count", () => {
     var post = new Post({
       message: "another message",
