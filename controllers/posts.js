8e021b5fdecea81b36cee807b9907355f69342fd
abacus <abhiram0207@gmail.com>
2022-07-13 18:14:51 +0100

    delete button works

--- controllers/posts.js
+++ controllers/posts.js
@@ -27,6 +27,17 @@ const PostsController = {
       res.status(201).redirect("/posts");
     });
   },
+  Delete: (req, res) => {
+    // some code to delete the post here
+    Post.findOneAndDelete({ _id: req.params.id }).exec(function (err) {
+      if (err) {
+        console.log(err);
+        res.redirect("back");
+      } else {
+        res.redirect("/posts");
+      }
+    });
+  },
 };
 
 module.exports = PostsController;
