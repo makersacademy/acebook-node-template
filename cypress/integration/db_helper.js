8448e7d85b7de15b8e0b768a6d6847525e93b666
Ephron Agyeman-opoku <ephronagyeman-opoku@Ephrons-MacBook-Air.local>
2022-07-13 15:56:31 +0100

    updates on truncate fucntion for mongodb

--- cypress/integration/db_helper.js
+++ cypress/integration/db_helper.js
@@ -1,30 +1,47 @@
 var mongoose = require("mongoose");
 
-
-beforeAll(function (done) {
+const dropCollection = () => {
   mongoose.connect("mongodb://0.0.0.0/acebook_test", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  });
-
-  var db = mongoose.connection;
-  db.collections.posts.drop(() => {
+})
+.then(mongoose.connection.collections.posts.drop(() => {
   console.log("posts database dropped");
-    });
+}))
+
+.then(mongoose.connection.close())
+}
+
+// beforeAll(function (done) {
+//   mongoose.connect("mongodb://0.0.0.0/acebook_test", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   });
+
+//   var db = mongoose.connection;
+//   db.collections.posts.drop(() => {
+//   console.log("posts database dropped");
+//     });
   
   
 
  
-  db.on("error", console.error.bind(console, "MongoDB connection error:"));
-  db.on("open", function () {
-    done();
-  });
-});
+//   db.on("error", console.error.bind(console, "MongoDB connection error:"));
+//   db.on("open", function () {
+//     done();
+//   });
 
-afterAll(function (done) {
-  mongoose.connection.close(true, function () {
-    done();
-  });
-});
+//   mongoose.connection.close()(true, function () {
+//     done();
+//   });
+// });
+
+// afterAll(function (done) {
+//   mongoose.connection.close(true, function () {
+//     done();
+//   });
+// });
+
+
 
 
