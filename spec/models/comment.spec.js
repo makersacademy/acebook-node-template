b91656a6e464a324ec05e7a4010b0c2d82072d1e
Jennie Evans <jennie1evans@hotmail.com>
2022-07-13 17:41:27 +0100

    unit tests passing

---
+++ spec/models/comment.spec.js
@@ -0,0 +1,46 @@
+var mongoose = require("mongoose");
+
+require("../mongodb_helper");
+var Comment = require("../../models/comment");
+
+describe("Comment model", () => {
+  beforeEach((done) => {
+    mongoose.connection.collections.comments.drop(() => {
+      done();
+    });
+  });
+
+  it("has a comment", () => {
+    var comment = new Comment({
+      message: "another message"
+    });
+    expect(comment.message).toEqual("another message");
+  });
+
+  it("can list all comments", (done) => {
+    Comment.find((err, comments) => {
+      expect(err).toBeNull();
+      expect(comments).toEqual([]);
+      done();
+    });
+  });
+
+  it("can save a comment", (done) => {
+    var comment = new Comment({
+      message: "another message",
+    });
+
+    comment.save((err) => {
+      expect(err).toBeNull();
+
+      Comment.find((err, comments) => {
+        expect(err).toBeNull();
+
+        expect(comments[0]).toMatchObject({
+          message: "another message",
+        });
+        done();
+      });
+    });
+  });
+});
