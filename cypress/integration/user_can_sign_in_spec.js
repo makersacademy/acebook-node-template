3684d2225871eada5876e37a363fded5daf59473
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-07-14 11:37:26 +0100

    updated id for name input

--- cypress/integration/user_can_sign_in_spec.js
+++ cypress/integration/user_can_sign_in_spec.js
@@ -2,7 +2,7 @@ describe("Authentication", () => {
   it("A user signs in and is redirected to /posts", () => {
     // sign up
     cy.visit("/users/new");
-    cy.get("#firstName").type("TestUser");
+    cy.get("#name").type("TestUser");
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
     cy.get("#submit").click();
--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -2,7 +2,7 @@ describe("Registration", () => {
   it("A user signs up and is redirected to sign in", () => {
     // sign up
     cy.visit("/users/new");
-    cy.get("#firstName").type("TestUser");
+    cy.get("#name").type("TestUser");
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
     cy.get("#submit").click();
