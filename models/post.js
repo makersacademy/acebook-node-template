0a84d5e6618827c2659c8348d68c8bf4ea3fa934
Joe McGarry <joewmcgarry@gmail.com>
2022-07-14 09:25:06 +0100

    Adds passing test for giving Post model a date

--- models/post.js
+++ models/post.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
   message: String,
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: () => Date.now(),
+  },
 });
 
 const Post = mongoose.model("Post", PostSchema);
