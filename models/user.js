9269ee6afdf0b664d3d9239d0539b6570eaffd53
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-07-14 15:25:04 +0100

    Add hashing function to password

--- models/user.js
+++ models/user.js
@@ -1,9 +1,31 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
+});
+
+UserSchema.pre("save", function (next) {
+  const user = this;
+  if (this.isModified("password") || this.isNew) {
+    bcrypt.genSalt(10, function (saltError, salt) {
+      if (saltError) {
+        return next(saltError);
+      } else {
+        bcrypt.hash(user.password, salt, function (hashError, hash) {
+          if (hashError) {
+            return next(hashError);
+          }
+          user.password = hash;
+          next();
+        });
+      }
+    });
+  } else {
+    return next();
+  }
 });
 
 const User = mongoose.model("User", UserSchema);
