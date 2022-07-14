29344f3f1a2711869a4789e7d1cd2bc7ab0eb131
Michael Chan <michaelcychan@gmail.com>
2022-07-14 16:25:02 +0100

    test fixed

--- cypress/integration/user_can_delete_posts_spec.js
+++ cypress/integration/user_can_delete_posts_spec.js
@@ -19,7 +19,7 @@ describe("Timeline", () => {
       // submit a post
       cy.visit("/posts");
       cy.contains("New post").click();
-      cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
+      cy.get("#new-post-form").find('#message').type("Hello, world!");
       cy.get("#new-post-form").submit();
       
       // delete a post
