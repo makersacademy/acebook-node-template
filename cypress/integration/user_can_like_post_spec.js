251db6e5bd401cc3c025204f953aa4f59772cee0
Hafiz <hafizalimi77@gmail.com>
2022-07-14 20:25:01 +0100

    added like button and passes test that contains 1 like

---
+++ cypress/integration/user_can_like_post_spec.js
@@ -0,0 +1,24 @@
+describe("like button", function(){
+  it("can like a post", function(){
+    cy.visit("/users/new");
+      cy.get("#email").type("someone@example.com");
+      cy.get("#password").type("password");
+      cy.get("#submit").click();
+  
+      // sign in
+      cy.visit("/sessions/new");
+      cy.get("#email").type("someone@example.com");
+      cy.get("#password").type("password");
+      cy.get("#submit").click();
+  
+      // submit a post
+      cy.visit("/posts");
+      cy.contains("New post").click();
+      cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
+      cy.get("#new-post-form").submit();
+      
+
+    cy.get('#like-button:last').click();
+    cy.get('#like-counter:last').should('contain', 1)
+ });
+});
