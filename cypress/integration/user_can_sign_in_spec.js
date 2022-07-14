620b11388d5ecbb622059444d831f5104f08cba6
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-07-14 08:59:12 +0100

    updated feature tests to reflect new signup method

--- cypress/integration/user_can_sign_in_spec.js
+++ cypress/integration/user_can_sign_in_spec.js
@@ -2,6 +2,7 @@ describe("Authentication", () => {
   it("A user signs in and is redirected to /posts", () => {
     // sign up
     cy.visit("/users/new");
+    cy.get("#firstName").type("TestUser");
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
     cy.get("#submit").click();
