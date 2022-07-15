c0049482d761a460bcbdfbb5091d37a68cbea97b
Saif-ul Hussain <saif-ulhussain@saif-uls-air.home>
2022-07-15 12:51:47 +0100

    cypress integration test added - user can submit comments spec

--- cypress/integration/user_can_submit_comments_spec.js
+++ cypress/integration/user_can_submit_comments_spec.js
@@ -1,3 +1,31 @@
 describe("Timeline", () => {
-  
+  it("can submit comments, when signed in, and post had been submitted", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // submit a post
+    cy.visit("/posts");
+    cy.contains("New post").click();
+
+    cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
+    cy.get("#new-post-form").submit();
+
+
+    // submit a comment
+    cy.visit("/posts");
+
+    cy.get("#new-comment-form").find('[type="text"]').type("Hi!");
+    cy.get("#new-comment-form").submit();
+
+    cy.get(".posts").should("contain", "Hi!");
+  });
 });
