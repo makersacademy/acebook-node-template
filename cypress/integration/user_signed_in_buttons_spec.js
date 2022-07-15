24e6ebfa54bb7f5846c2a959e914b996afdce6e3
Osman <osman_saeed@hotmail.co.uk>
2022-07-15 13:49:07 +0100

    two button spec files

--- cypress/integration/user_signed_in_buttons_spec.js
+++ cypress/integration/user_signed_in_buttons_spec.js
@@ -1,10 +1,4 @@
-describe("Home page", () => {
-    it("has a sign in and a sign up button when the user is not logged in", () => {
-        cy.visit("/");
-        cy.get("#login-button").should("contain", "Log In");
-        cy.get("#sign-up-button").should("contain", "Sign Up");
-    })
-
+describe("User signed in page", () => {
     it("has a sign out button when the user is logged in", () => {
         cy.visit("/");
         cy.get("#login-button").should("contain", "Log In");
