871eaee52e75300aaab533cdcafdb1eeb78801c0
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-07-17 20:58:05 +0100

    Add after each hook to submit post test

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -1,6 +1,11 @@
-const signUpAndSignIn = require('./webhelper')
+const signUpAndSignIn = require("./webhelper");
 
 describe("Timeline", () => {
+  afterEach(() => {
+    cy.task("dropPosts");
+    cy.task("dropUsers");
+  });
+
   it("can submit posts, when signed in, and view them", () => {
     // run webhelper to sign up and sign in to acebook
     signUpAndSignIn();
@@ -9,11 +14,16 @@ describe("Timeline", () => {
     cy.visit("/posts");
     cy.contains("New post").click();
 
-    cy.get("#new-post-form").find('[type="text"]').type("Test message: cypress submit post test!");
+    cy.get("#new-post-form")
+      .find('[type="text"]')
+      .type("Test message: cypress submit post test!");
     cy.get("#new-post-form").submit();
 
-    cy.get(".posts").should("contain", "Test message: cypress submit post test!");
+    cy.get(".posts").should(
+      "contain",
+      "Test message: cypress submit post test!"
+    );
     cy.get(".posts").should("contain", "CypressTestUser");
-    cy.get(".posts").should("contain", "2022")
+    cy.get(".posts").should("contain", "2022");
   });
 });
