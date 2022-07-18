97e230336a4c4097b1cacaeee6d26136dd9566c6
Daniel <mbatadaniel@gmail.com>
2022-07-18 16:19:50 +0100

    creating comment function

---
+++ cypress/integration/user_can_make_a_comment_under_a_post.spec.js
@@ -0,0 +1,29 @@
+const signUpAndSignIn = require("./webhelper");
+
+
+
+describe("Timeline", () => {
+  afterEach(() => {
+    cy.task("dropPosts");
+    cy.task("dropUsers)");
+  });
+
+  it("displays comment text box and comment button ", () => {
+    // run webhelper to sign up and sign in to acebook
+    signUpAndSignIn();
+
+    // submit a post
+    cy.visit("/posts");
+    cy.contains("New post").click();
+
+    cy.get("#new-post-form")
+      .find('[type="text"]')
+      .type("to try if comments work.");
+    cy.get("#new-post-form").submit();
+
+    cy.visit('/posts')
+    cy.get('#comment').type("test comment").submit()
+
+    cy.get(".comments").should("contain", "test comment" )
+  });
+});
