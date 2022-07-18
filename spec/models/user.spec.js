f2372008b10ea5da7fee6896f6d6246d746891da
Michael Chan <michaelcychan@gmail.com>
2022-07-18 15:56:16 +0100

    unit test working

--- spec/models/user.spec.js
+++ spec/models/user.spec.js
@@ -5,7 +5,7 @@ const User = require("../../models/user");
 
 describe("User model", () => {
   beforeEach((done) => {
-    mongoose.connection.collections.collection('users').deleteMany({}, () => {
+    mongoose.connection.collection('users').deleteMany({}, () => {
       done();
     });
   });
@@ -54,39 +54,40 @@ describe("User model", () => {
       });
     });
   });
-});
-
-fit("it can't save a user, if the same user already exists", (done) => {
+  it("it can't save a user, if the same user already exists", (done) => {
   
-  // To create one user
-  const user1 = new User({
-    email: "test@duplicate.com",
-    password: "password",
-  });
-  user1.save((err) => {
-    expect(err).toBeNull();
-    User.find((err, users) => {
+    // To create one user
+    const user1 = new User({
+      email: "test@duplicate.com",
+      password: "password",
+    });
+    user1.save((err) => {
       expect(err).toBeNull();
-      expect(users[0]).toMatchObject({
-        email: "test@duplicate.com",
-        password: "password",
-      });
-      // Creating another user with same email
-      const user2 = new User({
-        email: "test@duplicate.com",
-        password: "password",
-      });
-      
-      // Expecting Mongo Error E11000 -> duplicate entry in a unique key
-      // expect(user2.save()).rejects.toEqual({MongoError: 'E11000 duplicate key error collection: acebook_test.users index: email_1 dup key: { email: "test@duplicate.com" }'});
-      user2.save((err) () => {
-        console.log(err)
-      } );
-
       User.find((err, users) => {
         expect(err).toBeNull();
-        expect(users.length).toEqual(1);
-        done();
+        expect(users[0]).toMatchObject({
+          email: "test@duplicate.com",
+          password: "password",
+        });
+        // Creating another user with same email
+        const user2 = new User({
+          email: "test@duplicate.com",
+          password: "password",
+        });
+  
+        // Expecting Mongo Error E11000 -> duplicate entry in a unique key
+        // MongoError: 'E11000 duplicate key error collection: acebook_test.users index: email_1 dup key: { email: "test@duplicate.com" }'
+        const errorE11000 = async () => {
+          await user2.save();
+        }
+        expect(errorE11000()).rejects.toThrow();
+  
+        // expect there is only one user
+        User.find((err, users) => {
+          expect(err).toBeNull();
+          expect(users.length).toEqual(1);
+          done();
+        });
       });
     });
   });
