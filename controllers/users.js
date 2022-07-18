9e9cb0823dc9ecc69b535f3e0c84da5236a64a4c
Edward <103263530+edra-dev@users.noreply.github.com>
2022-07-18 14:03:02 +0100

    salt&hash

--- controllers/users.js
+++ controllers/users.js
@@ -15,9 +15,9 @@ const UsersController = {
 
     // creating a new mongoose doc from user data
     const user = new User(body);
-    // generate salt to hash password
+    // salt password 10 times
     const salt = await bcrypt.genSalt(10);
-    // now we set user password to hashed password
+    // set user password to hashed password
     user.password = await bcrypt.hash(user.password, salt);
     // user.save().then((doc) => res.status(201).send(doc));
     user.save().then(res.status(201).redirect("/posts"));
