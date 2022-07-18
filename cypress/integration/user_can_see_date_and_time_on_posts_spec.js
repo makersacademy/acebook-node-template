622fb69a6d6e73a3890f2b20ec0febad1fea1820
Robi <robi.maliqi@hotmail.co.uk>
2022-07-18 14:36:21 +0100

    removed comments from test

--- cypress/integration/user_can_see_date_and_time_on_posts_spec.js
+++ cypress/integration/user_can_see_date_and_time_on_posts_spec.js
@@ -2,18 +2,12 @@ import { signUp, signIn, submitPost } from "./web_helpers";
 
 describe("Timeline", () => {
   it("can submit a post and see the date and time of it", () => {
-    // sign up
     signUp();
-
-    // sign in
     signIn();
 
-
     const timeBeforePost = new Date();
-    // submit a post
+
     submitPost();
-    
-    // const date = today.getFullYear();
 
     cy.get(".datestamp").first().invoke('text').then(dateText => {
       const date = new Date(dateText);
@@ -22,9 +16,6 @@ describe("Timeline", () => {
 
       expect(date).to.be.lte(timeAfterPost);
       expect(date).to.be.gte(timeBeforePost);
-  });
-
-    //cy.get(".datestamp").first() //.should("have.text", `${range}`);
-    // checked test, passed
+    });
   });
 });
