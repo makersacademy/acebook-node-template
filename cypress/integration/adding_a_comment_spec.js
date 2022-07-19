338fcdb5544b4c953a1f3785b8be4de633a39ca5
Jennie Evans <jennie1evans@hotmail.com>
2022-07-19 19:45:06 +0100

    sign in test working

--- cypress/integration/adding_a_comment_spec.js
+++ cypress/integration/adding_a_comment_spec.js
@@ -12,10 +12,10 @@ describe("Timeline", () => {
     submitPost();
 
     //add the comment to the newest post
-    cy.get("#new-comment-form").first()
+    cy.get("#new-comment-form")
       .find('[type="text"]')
       .type("Example comment from Cypress Testing");
-    cy.get("#new-comment-form").submit();
+      cy.get("#new-comment-form").submit();
     cy.get(".posts").first()
     .get(".comment-message-text").last().should("have.text", "Example comment from Cypress Testing");
   });
