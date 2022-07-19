3a33edc213db876805e5a8f24e6dcb69070db549
Hafiz <hafizalimi77@gmail.com>
2022-07-19 16:35:47 +0100

    added integration test

--- cypress/integration/user_own_profile_page_spec.js
+++ cypress/integration/user_own_profile_page_spec.js
@@ -18,4 +18,35 @@ describe("User's Own Profile Page", function() {
     cy.get(".title").should("contain", "Profile Page");
     cy.get(".greeting").should('contain', "someone@example.com");
   });
+  it('can own post in profile page', () => {
+    cy.task('emptyUsers').then(() => {
+      cy.visit("/users/new");
+      cy.get("#email").type("someone@example.com");
+      cy.get("#password").type("password");
+      cy.get("#submit").click();
+      
+      // sign in
+      cy.visit("/sessions/new");
+      cy.get("#email").type("someone@example.com");
+      cy.get("#password").type("password");
+      cy.get("#submit").click();
+
+      // submit a post
+      cy.visit("/posts");
+      cy.contains("New post").click();
+
+      cy.get("#new-post-form").find('#message').type("Hello, world!");
+      cy.get("#new-post-form").submit();
+
+      cy.get(".posts").should("contain", "Hello, world!");
+      cy.get(".posts").should("contain", "someone@example.com");
+      
+      //check content at profile page
+      cy.get('#view-self-profile').click();
+      cy.get(".posts").should("contain", "Hello, world!");
+      cy.get(".posts").should("contain", "someone@example.com");
+  })
 });
+
+
+
