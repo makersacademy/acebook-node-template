0a87f2d6ffc4fd0c9d6dca236ea82a2592137b47
Osman <osman_saeed@hotmail.co.uk>
2022-07-20 14:46:12 +0100

    profile page spec passing

--- cypress/integration/user_can_view_other_profile_pages_spec.js
+++ cypress/integration/user_can_view_other_profile_pages_spec.js
@@ -41,4 +41,47 @@ describe("User can view others' Profile Page", function() {
     cy.get(".profile-link").first().submit();
     cy.get('.greeting').should("contain", "This profile page is about someone@example.com");
   });
+
+  it('checks for another users posts', () => {
+    // sign up & log out
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // log in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // submit a post
+    cy.visit("/posts");
+    cy.contains("New post").click();
+
+    cy.get("#new-post-form").find('#message').type("Hello, world!");
+    cy.get("#new-post-form").submit();
+
+    cy.get(".posts").should("contain", "Hello, world!");
+    cy.get(".posts").should("contain", "someone@example.com");
+
+    // sign up as second user
+    cy.visit("/users/new");
+    cy.get("#email").type("newperson@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // log in as second user
+    cy.visit("/sessions/new");
+    cy.get("#email").type("newperson@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // click to go through to the other person's page
+    cy.get(".profile-link").first().submit();
+    cy.get('.greeting').should("contain", "This profile page is about someone@example.com");
+
+    // expect post
+    cy.get(".post-container").should("contain", "Hello, world!");
+  });
 });
