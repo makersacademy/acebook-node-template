374be7f0f8800773861d6914a3c2333cae1e7369
Edward <103263530+edra-dev@users.noreply.github.com>
2022-07-21 15:49:45 +0100

    Delete

--- cypress/integration/user_can_delete_a_post_spec.js
+++ cypress/integration/user_can_delete_a_post_spec.js
@@ -8,7 +8,7 @@ describe("Timeline", () => {
 
     // delete a post
     cy.visit("/posts");
-    cy.get(".delete-button").first().click();
+    cy.get(".delete-button-container").first().click();
     cy.get(".posts").not("contain", "Example Post from Cypress Testing");
   });
 });
