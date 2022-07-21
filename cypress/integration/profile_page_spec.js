2971a7837d4c37abfd2c6af6d9c224c90cbdcfa8
Daniel <mbatadaniel@gmail.com>
2022-07-21 12:53:22 +0100

    prepopulated fields with user data, removed birthday from edit info page. fixed broken test and all tests passing

--- cypress/integration/profile_page_spec.js
+++ cypress/integration/profile_page_spec.js
@@ -13,8 +13,8 @@ describe("Profile Page", () => {
     cy.contains("Profile Page").click();
 
     // page contains the username
-    cy.contains("Test User")
-  })
+    cy.contains("Test User");
+  });
 
   it("displays detailed information (dob, location, full name) about user", () => {
     // run webhelper to sign up and sign in to acebook
@@ -24,20 +24,18 @@ describe("Profile Page", () => {
     cy.contains("Profile Page").click();
 
     // test for information on profile page
-    cy.contains("Test User")
-    cy.contains("London")
-    cy.contains("11 June 1999")
-  })
+    cy.contains("Test User");
+    cy.contains("London");
+    cy.contains("11 June 1999");
+  });
 
   it("Displays posts made by user", () => {
     // use webhelper to sign up and sign in as a different user
-    signUpAndSignIn("Test", "User2")
+    signUpAndSignIn("Test", "User2");
 
     cy.contains("New post").click();
 
-    cy.get("#new-post-form")
-      .find('[type="text"]')
-      .type("Do not display");
+    cy.get("#new-post-form").find('[type="text"]').type("Do not display");
     cy.get("#new-post-form").submit();
 
     cy.contains("Sign Out").click();
@@ -49,24 +47,22 @@ describe("Profile Page", () => {
     // make another post as new user
     cy.contains("New post").click();
 
-    cy.get("#new-post-form")
-      .find('[type="text"]')
-      .type("Show this message");
+    cy.get("#new-post-form").find('[type="text"]').type("Show this message");
     cy.get("#new-post-form").submit();
 
     // visit profile page and only see post made by current user
     cy.contains("Profile Page").click();
     cy.url().should("include", "/profile/user");
     cy.get("ul").should(($post) => {
-      expect($post).to.contain("Show this message")
-      expect($post).not.to.include.text("Do not display")
-    })
-    
+      expect($post).to.contain("Show this message");
+      expect($post).not.to.include.text("Do not display");
+    });
+
     // use webhelper to drop users and posts collections
     cy.task("dropPosts");
-  })
+  });
 
-  it.only("allows user to update their personal information", () => {
+  it("allows user to update their personal information", () => {
     // run webhelper to sign up and sign in to acebook
     signUpAndSignIn("Test", "User");
 
@@ -74,33 +70,34 @@ describe("Profile Page", () => {
     cy.contains("Profile Page").click();
 
     // test for information on profile page
-    cy.contains("Test User")
-    cy.contains("London")
-    cy.contains("11 June 1999")
+    cy.contains("Test User");
+    cy.contains("London");
+    cy.contains("11 June 1999");
 
     // user clicks on 'Edit Info' button and is taken to edit page
     cy.get(".user-information-container").contains("Edit").click();
-    cy.url().should("include", "/profile/user/TestUser/editInfo")
+    cy.url().should("include", "/profile/user/TestUser/editInfo");
 
     // existing user info is pre-populated into form
-    cy.contains("TestUser")
-    cy.get("#edit-info-form").should("contain", "Test")
-      .and("contain", "User")
-      .and("contain", "London")
-      .and("contain", "11 June 1999")
-    
+    cy.get("#firstName").should("have.value", "Test");
+    cy.get("#lastName").should("have.value", "User");
+    cy.get("#location").should("have.value", "London");
+
+    // clear fields of all content
+    cy.get("#firstName").clear();
+    cy.get("#lastName").clear();
+    cy.get("#location").clear();
+
     // user changes personal information and is redirected to their profile page
     cy.get("#firstName").type("Updated");
-    cy.get("#lastName").type("Name")
-    cy.get("#birthday").type("1987-25-06");
+    cy.get("#lastName").type("Name");
     cy.get("#location").type("Birmingham");
-    cy.get("#submit").click()
-    cy.url().should("include", "/profile/user/TestUser")
+    cy.get("#submit").click();
+    cy.url().should("include", "/profile/user/TestUser");
 
     // new personal information is displayed on profile page
-    cy.contains("TestUser")
-    cy.contains("Updated Name")
-    cy.contains("25 June 1987")
-    cy.contains("Birmingham")
-  })
-})
+    cy.contains("TestUser");
+    cy.contains("Updated Name");
+    cy.contains("Birmingham");
+  });
+});
