a4b124b620d1eba070dc864505d741fd28eb0280
Daniel <mbatadaniel@gmail.com>
2022-07-21 18:01:36 +0100

    added eventlistener to the button

--- public/javascripts/show_comments.js
+++ public/javascripts/show_comments.js
@@ -1,4 +1,7 @@
-const showComments () => {
-
-
-}
+// eslint-disable-next-line;
+const showComments = () => {
+  const buttonEl = document.querySelector("#show-comments");
+  buttonEl.addEventListener("click", () => {
+    console.log("show-comments clicked");
+  });
+};
