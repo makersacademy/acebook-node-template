a932e1cbb2854acc96a4df53e374776f51dfb50e
Michael Chan <michaelcychan@gmail.com>
2022-07-21 16:25:12 +0100

    added files to eslintignore and adjusting jest coverage

--- jest.config.js
+++ jest.config.js
@@ -22,10 +22,9 @@ module.exports = {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
-    './routes/**',
    './models/**',
-   './controllers/**',
-    './app.js'],
+   './controllers/**'
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
