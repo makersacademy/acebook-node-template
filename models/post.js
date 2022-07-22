d1e6fc7b61855bdc281447b39b85fd082eb3dd03
Dave Kempsell <dave_kempsell@hotmail.com>
2022-07-22 09:16:09 +0100

    title added to posts schema, and to new post form. not displayed anywhere. unit test passes, no feature test, as no feature to test

--- models/post.js
+++ models/post.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema({
   userId: ObjectId,
   username: String,
+  title: String,
   message: String,
   likes: Array,
   timestamp: String,
