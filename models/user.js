b5f2c19f71d12a11e507d42f388e3034b9239497
Marlon-Buckley <m4rlz@live.co.uk>
2022-08-03 14:06:44 +0100

    Adding time stamps to users.js

--- models/user.js
+++ models/user.js
@@ -3,8 +3,12 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: String,
   password: String,
-});
+}, 
+
+{ timestamps: true });
 
 const User = mongoose.model("User", UserSchema);
+console.log(User.createdAt)
+console.log(User)
 
 module.exports = User;
