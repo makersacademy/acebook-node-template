1a5ecea33604ab419585f4946f098e68e2afaf72
Marlon-Buckley <m4rlz@live.co.uk>
2022-08-05 17:01:53 +0100

    Ending the day with a bang

---
+++ public/click.js
@@ -0,0 +1,16 @@
+const button = document.getElementById('myButton');
+button.addEventListener('click', function(e) {
+  console.log('button was clicked');
+
+  fetch('/clicked', {method: 'POST'})
+    .then(function(response) {
+      if(response.ok) {
+        console.log('Click was recorded');
+        return;
+      }
+      throw new Error('Request failed.');
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+})
--- public/client.js
+++ public/client.js
@@ -1,29 +1,5 @@
 console.log('Client-side code running');
 
-const button = document.getElementById('myButton');
-button.addEventListener('click', function(e) {
-  console.log('button was clicked');
-
-  fetch('/clicked', {method: 'POST'})
-    .then(function(response) {
-      if(response.ok) {
-        console.log('Click was recorded');
-        return;
-      }
-      throw new Error('Request failed.');
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-});
-
-// let likeBtn = document.querySelector(".like__btn");
-// let likeIcon = document.querySelector("#icon"),
-//   count = document.querySelector("#count");
-
-// let clicked = false;
-
-
 const likehandler = (event) =>  {
     
   
@@ -31,14 +7,5 @@ const likehandler = (event) =>  {
   event.target.children[1].textContent++;
 
   this_.likes
-//     if (!clicked) {
-//     clicked = true;
-//     likeIcon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
-//     count.textContent++;
-//   } else {
-//     clicked = false;
-//     likeIcon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
-//     count.textContent--;
-//   }
-};
 
+};
