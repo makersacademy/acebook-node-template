2092a206a45340d9448e4caa6f55eebe939ef98e
Eghosa Edobor <eghosaedobor11@gmail.com>
2022-08-10 15:44:58 +0100

    Integratio_test_homepage_to_signup

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -3,4 +3,9 @@ describe("Home page", () => {
     cy.visit("/");
     cy.get(".title").should("contain", "Acebook");
   });
+
+  it("Navigates to Sign up page from Home", () => {
+    cy.visit("/");
+    cy.url().should("include", "/sessions/new");
+  });
 });
