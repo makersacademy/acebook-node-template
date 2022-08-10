0b9d8c83fee53e943ab02fc83fde5da780e1dacc
Eghosa Edobor <eghosaedobor11@gmail.com>
2022-08-10 17:08:00 +0100

    routes

--- controllers/home.js
+++ controllers/home.js
@@ -2,6 +2,12 @@ const HomeController = {
   Index: (req, res) => {
     res.render("home/index", { title: "Acebook" });
   },
+  Login: (req, res) => {
+    res.render("sessions/new", {});
+    // when I call login, I want a return the sessions/new page
+  } 
 };
+
+// method to go to homepage
 
 module.exports = HomeController;
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,17 @@
 const express = require("express");
+// express is just a library we use, web framework 
+// which allows you to run functions
 const router = express.Router();
 
 const PostsController = require("../controllers/posts");
 
 router.get("/", PostsController.Index);
+// calling method on the post controls, getting info
 router.post("/", PostsController.Create);
+// calling method to give info to the server
 router.get("/new", PostsController.New);
-
+//  displaying posts which is in the view folder
 module.exports = router;
+
+
+// so essentially passing in whatever the user types ionto the logic in controller
