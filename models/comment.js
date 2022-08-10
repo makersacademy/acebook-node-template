4a0768712d53c37285f039bfc02e7716c7ab1685
Karolina Joksaite <k.joksaite00@gmail.com>
2022-08-10 16:17:03 +0100

    added timeCreated property

--- models/comment.js
+++ models/comment.js
@@ -3,6 +3,14 @@ const { Schema } = mongoose;
 
 const CommentSchema = new Schema({
   comment: { type: String, required: true },
+  timeCreated: {
+    type: String,
+    default: function () {
+      const date = new Date();
+
+      return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+    },
+  },
 });
 
 const Comment = mongoose.model('Comment', CommentSchema);
