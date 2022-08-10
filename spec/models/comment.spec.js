1f13102bcf9d3617db160c94c480cf960ea68cc9
Karolina Joksaite <k.joksaite00@gmail.com>
2022-08-10 16:16:05 +0100

    added test for default time

--- spec/models/comment.spec.js
+++ spec/models/comment.spec.js
@@ -1,17 +1,25 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-require("../mongodb_helper");
-const Comment = require("../../models/comment");
+require('../mongodb_helper');
+const Comment = require('../../models/comment');
 
-describe("Comment model", () => {
+describe('Comment model', () => {
   beforeEach((done) => {
     mongoose.connection.collections.comments.drop(() => {
       done();
     });
   });
 
-  it("has a comment", () => {
-    const comment = new Comment({comment: "test comment"});
-    expect(comment.comment).toEqual("test comment");
-  })
+  it('has a comment', () => {
+    const comment = new Comment({ comment: 'test comment' });
+    expect(comment.comment).toEqual('test comment');
+  });
+
+  it('has a default time', () => {
+    const comment = new Comment({ comment: 'new comment' });
+    const date = new Date().toLocaleDateString();
+
+    expect(comment.timeCreated).toBeTruthy();
+    expect(comment.timeCreated).toEqual(expect.stringContaining(date));
+  });
 });
