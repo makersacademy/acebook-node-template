16632fbab9e7a4e351c037e57a7528a591265e4c
Shaun <shaunhohoho@gmail.com>
2022-08-11 12:01:50 +0100

    ahmed making a branch

--- spec/models/friends.test.spec.js
+++ spec/models/friends.test.spec.js
@@ -2,8 +2,8 @@ const { ObjectId } = require("mongodb");
 const mongoose = require("mongoose");
 
 require("../mongodb_helper");
-// const User = require("../../models/user");
-const Friends = require("../../models/friends");
+const User = require("../../models/user");
+const Friends = require("../../models/friend");
 
 describe("Friends model", () => {
   beforeEach((done) => {
@@ -12,20 +12,57 @@ describe("Friends model", () => {
     });
   });
 
-  it("Checks that two users are freinds", () => {
+  it("Checks that two users are freinds", (done) => {
+    const user1 = new User({
+      firstName: "Someone1",
+      lastName: "Surname1",
+      username: "SomeoneSurname1",
+      email: "someone1@example.com",
+      password: "password1",
+      phoneNumber: "12345678",
+    });
 
-    const friend = new Friends({ requester: '62f3c3f5b6bbf9712b23f341', recipient: '62f3c3f5b6bbf9712b23f34', status: 1 });
-    friend.save().then(() => console.log(friend))
-
-    // friends.save((err, friends) => {
-    //   expect(err).toBeNull();
-    //   expect(
-    // freinds.length).toBe(1);
-    //   done();
+    // const user2 = new User({
+    //   firstName: "Someone2",
+    //   lastName: "Surname2",
+    //   username: "SomeoneSurname2",
+    //   email: "someone2@example.com",
+    //   password: "password2",
+    //   phoneNumber: "12345678",
     // });
 
-    //expect(friends.requester).toEqual('1');
+    const user1_id = 
+      user1.save((err, user) => {
+        if (err) {
+          throw err;
+        }
+        console.log("hello");
+        return user._id;
+      });
+
+      // const user2_id = user2.save((err, user) => {
+      //   if (err) {
+      //     console.log("error", err);
+      //     throw err;
+      //   }
+      //   return user.id;
+      // });
+
+      // const friend = new Friends({
+      //   requester: "62f3c3f5b6bbf9712b23f341",
+      //   recipient: "62f3c3f5b6bbf9712b23f341",
+      //   status: 1,
+      // });
+      // friend.save().then(() => console.log(friend));
+
+      // friends.save((err, friends) => {
+      //   expect(err).toBeNull();
+      //   expect(
+      // freinds.length).toBe(1);
+      //   done();
+      // });
+
+      //expect(friends.requester).toEqual('1');
+    };
   });
 
-
-});
