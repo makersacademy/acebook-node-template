37cc7da365e03292fc1b7c7a080c36df6d28fe26
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-08-11 16:11:36 +0100

    Added missing comma in post.js schema

--- models/post.js
+++ models/post.js
@@ -5,7 +5,7 @@ const PostSchema = new Schema({
   message: {type: String, required: true},
   user: {type: Schema.Types.ObjectId, ref: 'User', required: false},
   likes: {type: Number, required: false},
-  comments: { type: Array, default: [] }
+  comments: { type: Array, default: [] },
   timePosted: {type: String, default: function() {
     const date = new Date();
     return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
