47b7e2ddea5bfba099b5c0653c6a099ed5260cee
Karolina Joksaite <k.joksaite00@gmail.com>
2022-08-11 12:40:20 +0100

    added get and post routes for comments

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,13 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const PostsController = require("../controllers/posts");
+const PostsController = require('../controllers/posts');
 
-router.get("/", PostsController.Index);
-router.post("/", PostsController.Create);
-router.get("/new", PostsController.New);
+router.get('/', PostsController.Index);
+router.post('/', PostsController.Create);
+router.get('/new', PostsController.New);
+router.get('/comment/new', PostsController.NewComment);
+router.post('/', PostsController.CreateComment);
+router.get('/', PostsController.Comment);
 
 module.exports = router;
