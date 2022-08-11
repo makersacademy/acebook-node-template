e6826f587a1f81d74bb6f99a9a48480324ee5b28
delphine <delphine.mubiligi@gmail.com>
2022-08-11 14:58:21 +0100

    added time to posts

--- models/post.js
+++ models/post.js
@@ -5,7 +5,10 @@ const PostSchema = new Schema({
   message: {type: String, required: true},
   user: {type: Schema.Types.ObjectId, ref: 'User', required: false},
   likes: {type: Number, required: false},
-  timePosted: {type: Date, required: false}
+  timePosted: {type: String, default: function() {
+    const date = new Date();
+    return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+  }}
   //comments: [{ type: String, required: false, ref: 'Comment' }]//change to be array of commentSchemas CHECK WITH COACH
 });
 
