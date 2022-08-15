367ec24e2a930b6075bbf18d7f918f8b5c8721e9
Eghosa Edobor <eghosaedobor11@gmail.com>
2022-08-15 16:45:49 +0100

    search_index_to_search route

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,5 @@
+const res = require("express/lib/response");
+
 const HomeController = {
   Index: (req, res) => {
     res.render("home/index", { title: "Acebook" });
@@ -5,7 +7,10 @@ const HomeController = {
   Login: (req, res) => {
     res.render("sessions/new", {});
     // when I call login, I want a return the sessions/new page
-  } 
+  },
+  SearchIndex: (req, res) => {
+    res.render('search/index');
+  }
 };
 
 // method to go to homepage
