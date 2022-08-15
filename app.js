775abfc2fe436c4f53d08c8b04420372a4d4b969
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-08-15 15:58:07 +0100

    app.listen added

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ const sessionsRouter = require("./routes/sessions");
 const usersRouter = require("./routes/users");
 
 const mongoose = require('mongoose');
+const app = express();
 
 const dbURI= 'mongodb+srv://SharedUser:Milton@acebook.g2d2kr5.mongodb.net/Acebook?retryWrites=true&w=majority';
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then((result) => console.log('Connected to db'))
-  .catch((err)=> consolee.log(err))
+  .then((result) => app.listen(3000))
+  .catch((err)=> consolee.log(err));
 
-const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
