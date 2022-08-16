d80e47933038a54b237d7db071daa90b57b80b38
Eghosa Edobor <eghosaedobor11@gmail.com>
2022-08-16 16:27:57 +0100

    routed search pathway from profile page

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const UsersController = require("../controllers/users");
+const HomeController = require("../controllers/home");
 
 router.get("/new", UsersController.New);
 router.post("/", UsersController.Create);
 router.get("/profile", UsersController.Profile);
+router.get('/search', HomeController.SearchIndex);
 module.exports = router;
