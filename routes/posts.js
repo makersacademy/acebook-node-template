7dc420a536c90a330e1bab9c85ab561c97f9df18
Karolina Joksaite <k.joksaite00@gmail.com>
2022-08-16 12:47:10 +0100

    users can like and unlike

--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const PostsController = require("../controllers/posts");
+const PostsController = require('../controllers/posts');
 
-router.get("/", PostsController.Index);
-router.post("/", PostsController.Create);
-router.get("/new", PostsController.New);
+router.get('/', PostsController.Index);
+router.post('/', PostsController.Create);
+router.get('/new', PostsController.New);
 
-router.post("/:_id/comment", PostsController.CreateComment);
+router.post('/:_id/comment', PostsController.CreateComment);
 
-router.post("/likes/:id", PostsController.Like);
-// router.post("/:_id/unlike", PostsController.Unlike);
+router.post('/like/:_id', PostsController.ToggleLike);
 
 module.exports = router;
