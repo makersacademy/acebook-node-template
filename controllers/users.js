1146a9c805143fc68810b47af77c7c736e1dcf89
Eghosa Edobor <eghosaedobor11@gmail.com>
2022-08-17 11:54:08 +0100

    Other User Function in Controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const Post = require('../models/post');
 const UsersController = {
   New: (req, res) => {
     res.render("users/new", {});
@@ -6,6 +7,7 @@ const UsersController = {
 
   Create: (req, res) => {
     const user = new User(req.body);
+    
     user.save((err) => {
       if (err) {
         throw err;
@@ -24,6 +26,28 @@ const UsersController = {
      });
     }
    },
+
+  //  OtherUser: (req, res) => {
+  //   // find the user's data in the database, return one object that matches
+  //   User.findOne({email: email}, (err, user) => {
+  //     if (err) {
+  //       throw err;
+  //     }
+      
+  //     //search for posts made by this user
+  //     Post.find({email: email}, (err, user) => {
+  //       if (err) {
+  //         throw err;
+  //       }
+        
+  //       // pass user data object into view file
+  //       // res.render(xxxxxxxxxxxxxxxxxx", {
+  //         user: userData,
+  //         posts: userPosts,
+      
+  //       });
+  //     });
+  //   });
 };
 
 module.exports = UsersController;
