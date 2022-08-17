a9871221bdecb4396896ce759e39b7ed0fa5f43b
IrinaVi <irinavik1805@gmail.com>
2022-08-17 15:52:31 +0100

    comment and like work

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,11 +2,11 @@ const Post = require("../models/post");
 
 const PostsController = {
   Index: (req, res) => {
+    //finds all posts: 
     Post.find((err, posts) => {
       if (err) {
         throw err;
       }
-      console.log(req.session);
       res.render("posts/index", {
         posts: posts,
         session: req.session,
@@ -19,7 +19,8 @@ const PostsController = {
     });
   },
   Create: (req, res) => {
-    const post = new Post(req.body);
+    //const post = new Post(req.body);
+    const post = new Post({ content: req.body.content, userId: req.session.user._id, username: req.session.user.username });
     post.save((err) => {
       if (err) {
         throw err;
@@ -32,9 +33,10 @@ const PostsController = {
     const post_id = req.body.post_id;
     const comment_content = req.body.content;
     const user = req.session.user._id;
+    const username = req.session.user.username;
     // const user = req.session.user.username;
     const update = {
-      $push: { comment: [{ author: user, content: comment_content }] },
+      $push: { comment: [{ author: user, content: comment_content, username: username }] },
     };
 
     Post.findOneAndUpdate(
@@ -51,6 +53,32 @@ const PostsController = {
     );
     res.status(201).redirect("/posts");
   },
+
+  AddLike: (req, res) => {
+    console.log("Add like controller works!")
+    const post_id = req.body.post_id;
+    const user = req.session.user._id;
+    // const user = req.session.user.username;
+    console.log("POST ID:", post_id, "user:", user);
+    const update = {
+      $push: { like: [{ likeAuthor: user }] },
+    };
+
+    Post.findOneAndUpdate(
+      { _id: post_id },
+      update,
+      { new: true, useFindAndModify: false },
+      (error, data) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(data);
+        }
+      }
+    );
+    res.status(201).redirect("/posts");
+  },
+
 };
 
 module.exports = PostsController;
