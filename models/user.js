e7b677443c67c88562c4c36ea232dde5df949f70
Eghosa Edobor <eghosaedobor11@gmail.com>
2022-08-18 09:44:54 +0100

    ran and installled bcryptjs crypto module for node.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+
 const UserSchema = new Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
