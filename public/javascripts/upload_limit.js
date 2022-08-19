b8385ce18f8397c869478c7a3257ac48613576a3
Paris Monson <parismonson@yahoo.com>
2022-08-19 11:40:57 +0100

    Changed upload limit to 1MB

--- public/javascripts/upload_limit.js
+++ public/javascripts/upload_limit.js
@@ -1,8 +1,8 @@
 var uploadField = document.getElementById("image");
 
 uploadField.onchange = function () {
-  if (this.files[0].size > 2097152) {
-    alert("File is too big! Must be less than 2MB!");
+  if (this.files[0].size > 1048576) {
+    alert("File is too big! Must be less than 1MB!");
     this.value = "";
   }
 };
