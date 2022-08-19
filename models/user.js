7d074938f5cc776d3642406f3b705ca296a11fcb
Joseph Edwards Rean <joseph.edwardsrean@outlook.com>
2022-08-19 14:06:16 +0100

    Commented out hashing of passwords

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 
 
 const UserSchema = new Schema({
@@ -13,38 +13,43 @@ const UserSchema = new Schema({
   post: { type: Schema.Types.ObjectId, ref: "Post", required: false },
 });
 
+
 // created a function that hashes the password
-UserSchema.pre("save", function (next) {
-  const user = this;
-  if (this.isModified("password") || this.isNew) {
-    bcrypt.genSalt(10, function (saltError, salt) {
-      if (saltError) {
-        return next(saltError);
-      } else {
-        bcrypt.hash(user.password, salt, function (hashError, hash) {
-          if (hashError) {
-            return next(hashError);
-          }
-          user.password = hash;
-          next();
-        });
-      }
-    });
-  } else {
-    return next();
-  }
-});
+// UserSchema.pre("save", function (next) {
+//   const user = this;
+//   if (this.isModified("password") || this.isNew) {
+//     bcrypt.genSalt(10, function (saltError, salt) {
+//       if (saltError) {
+//         return next(saltError);
+//       } else {
+//         bcrypt.hash(user.password, salt, function (hashError, hash) {
+//           if (hashError) {
+//             return next(hashError);
+//           }
+//           user.password = hash;
+//           next();
+//         });
+//       }
+//     });
+//   } else {
+//     return next();
+//   }
+// });
+
 
 // created a method that compares the password in the db and the password in the req.body
-UserSchema.methods.comparePassword = function (password, callback) {
-  bcrypt.compare(password, this.password, function (error, isMatch) {
-    if (error) {
-      return callback(error);
-    } else {
-      callback(null, isMatch);
-    }
-  });
-};
+// UserSchema.methods.comparePassword = function (password, callback) {
+//   console.log('compare passwords');
+//   bcrypt.compare(password, this.password, function (error, isMatch) {
+//     if (error) {
+//       console.log('test 1');
+//       return callback(error);
+//     } else {
+//       console.log('test2');
+//       callback(null, isMatch);
+//     }
+//   });
+// };
 
 
 const User = mongoose.model("User", UserSchema);
