9c801492c421e5aec8b98c39fe144774d34d0319
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-07 09:15:44 +0100

    working on redirecting existing user

--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -12,13 +12,13 @@ describe("Registration", () => {
   it("'users/new/' redirects to '/posts' if user is already logged in", () => {
     // sign up
     cy.visit("/users/new");
-    cy.get("#email").type("someone@example.com");
+    cy.get("#email").type("anotherone@example.com");
     cy.get("#password").type("password");
     cy.get("#submit").click();
 
     // sign in
     cy.visit("/sessions/new");
-    cy.get("#email").type("someone@example.com");
+    cy.get("#email").type("anotherone@example.com");
     cy.get("#password").type("password");
     cy.get("#submit").click();
 
@@ -26,4 +26,22 @@ describe("Registration", () => {
     cy.visit("/users/new");
     cy.url().should('include', '/posts')
   })
+
+  // it("redirects to '/' if user already exists", () => {
+  //   // sign up
+  //   cy.visit("/users/new");
+  //   cy.get("#email").type("person@example.com");
+  //   cy.get("#password").type("password");
+  //   cy.get("#submit").click();
+
+  //   // log out
+  //   cy.get("#logout").click();
+
+  //   // sign up again
+  //   cy.visit("/users/new");
+  //   cy.get("#email").type("person@example.com");
+  //   // cy.get("#password").type("password");
+  //   cy.get("#submit").click();
+  //   cy.url().should("match", /.+\/$/)
+  // })
 });
