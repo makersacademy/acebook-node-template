207b33ca64e3e1e4e8de8bb70847e057aed24d37
Zac <zac@zacmoss.co.uk>
2022-09-07 11:39:51 +0100

    created routes/profiles.js

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,6 @@ const UsersController = require("../controllers/users");
 
 router.get("/new", UsersController.New);
 router.post("/", UsersController.Create);
-router.get("/profile/:userId", UsersController.Profile);
+// router.get("/profile/:userId", UsersController.Profile);
 
 module.exports = router;
