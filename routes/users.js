0a98120226b7c73401b33f0fd1267d08bcd55431
naomischlosser <naomi_schlosser@hotmail.com>
2022-09-07 15:50:35 +0100

    Remove comments that are outdated

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,5 @@ const UsersController = require("../controllers/users");
 
 router.get("/new", UsersController.New);
 router.post("/", UsersController.Create);
-// router.get("/profile/:userId", UsersController.Profile);
 
 module.exports = router;
