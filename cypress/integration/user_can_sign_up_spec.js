95e9a6f15dab15daac3c53739598be93b7fca762
naomischlosser <naomi_schlosser@hotmail.com>
2022-09-07 15:02:49 +0100

    Wrote test for user signup and old session path from app

--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -1,11 +1,20 @@
 describe("Registration", () => {
   it("A user signs up and is redirected to sign in", () => {
+    // Go to sign up page
+    cy.visit("/");
+    cy.get("#new-user").click();
+    cy.url().should("include", "/users/new");
+    cy.contains("Sign up").click();
+
     // sign up
     cy.visit("/users/new");
+    cy.get("#first-name").type("some");
+    cy.get("#last-name").type("one");
+    cy.get("#username").type("someone123");
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
-    cy.get("#submit").click();
+    cy.get("#signup").click();
 
-    cy.url().should("include", "/sessions/new");
+    cy.url().should("include", "/");
   });
 });
