b78a99eb341cfad190c8988a65fab7b51ff4e61e
Zac <zac@zacmoss.co.uk>
2022-09-07 15:55:00 +0100

    passed adding to friends list unit tests

--- spec/models/user.spec.js
+++ spec/models/user.spec.js
@@ -116,9 +116,9 @@ describe("User model", () => {
     expect(user.last_name).toEqual("one");
   });
 
-  it("Should add a user to the friends list.", () => {
+  it("Should add a user to the friends list.", (done) => {
     const user = new User({
-      username: "someone",
+      username: "someoneelse",
       first_name: "some",
       last_name: "one",
       email: "someone@example.com",
@@ -128,10 +128,20 @@ describe("User model", () => {
 
     user.save((err) => {
       expect(err).toBeNull();
-      User.find((err, users) => {
+      User.find({ username: "someoneelse" }, (err, user) => {
         expect(err).toBeNull();
-        const newFriendList = users[0].push("friend3");
-        User.updateOne({ username: "someone" }, {friends = newFriendList})
+        const newFriendList = user[0].friends.concat("friend3");
+        User.updateOne(
+          { username: "someoneelse" },
+          { friends: newFriendList },
+          () => {
+            User.find({ username: "someoneelse" }, (err, user) => {
+              expect(err).toBeNull();
+              expect(user[0].friends[2]).toBe("friend3");
+              done();
+            });
+          }
+        );
       });
     });
   });
