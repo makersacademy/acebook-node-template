a331e94d5f9d2360992fa1c826c888eedb1d9008
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-07 20:44:38 +0100

    improved user_cannot_sign_in_spec.js

--- cypress/integration/user_cannot_sign_in_spec.js
+++ cypress/integration/user_cannot_sign_in_spec.js
@@ -1,10 +1,16 @@
 describe("Authentication", () => {
+  beforeEach(() => {
+    cy.task('dropUsers');
+    cy.task('dropPosts');
+  })
+
   it("A user cannot sign in due to incorrect credentials", () => {
-    // sign up
+    // sign up & log out
     cy.visit("/users/new");
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
     cy.get("#submit").click();
+    cy.get("#logout").click();
 
     // sign in
     cy.visit("/");
