ab772e5f21cfb1f39f3e97ed532cba1815478c71
Zac <zac@zacmoss.co.uk>
2022-09-08 16:03:47 +0100

    merging main into this branch

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const AdminController = require("../controllers/admin");
 
+router.get("/", AdminController.Index);
 router.delete("/reset", AdminController.Destroy);
 
 module.exports = router;
