e45464d35b48b8250062fd6b8becd07cc0123969
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-09-12 17:36:40 +0100

    users cannot sign in/integration test complete

--- cypress/integration/user_cannot_sign_in_spec.js
+++ cypress/integration/user_cannot_sign_in_spec.js
@@ -15,9 +15,9 @@ describe("Authentication", () => {
 
     // sign in
     cy.visit("/");
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("123");
-    cy.get("#submit").click();
+    cy.get("#user_email").type("someone@example.com");
+    cy.get("#user_password").type("123");
+    cy.get("#login_submit_btn").click();
 
     cy.url().should("include", "/");
     cy.contains("p", "Incorrect credentials");
