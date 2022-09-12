b4b383346a38a16e7d5b66b165e806843da9eeca
Zac <zac@zacmoss.co.uk>
2022-09-12 17:07:00 +0100

    added delete request form to html in posts/index

--- cypress/integration/posts_delete_button_works_spec.js
+++ cypress/integration/posts_delete_button_works_spec.js
@@ -26,34 +26,27 @@ describe("Posts feed", () => {
     // make a new post
     cy.visit("/posts/new");
     cy.get("#message").type("this is a post");
+    const today = new Date();
+    const time =
+      today.getFullYear() +
+      "/" +
+      (today.getMonth() + 1) +
+      "/" +
+      today.getDate() +
+      " " +
+      today.getHours() +
+      ":" +
+      today.getMinutes();
     cy.get("#submit-post").click();
 
-    //sign out
-    cy.get(".logout-button-input").click();
+    // post appears in feed with info
+    cy.visit("/posts");
+    cy.get(".posts").contains("this is a post");
+    cy.get(".posts").contains("someone");
+    cy.get(".posts").contains(time);
 
-    //sign in as someone else
-    // sign up
-    cy.visit("/users/new");
-    cy.get("#username").type("billy");
-    cy.get("#email").type("billy@example.com");
-    cy.get("#password").type("password");
-    cy.get("#signup").click();
-
-    // sign in
-    cy.visit("/");
-    cy.get("#email").type("billy@example.com");
-    cy.get("#password").type("password");
-    cy.get("#login").click();
-
-    // make request to delete post
-    cy.request("GET", "http://localhost:3030/admin/posts").then((response) => {
-      // Object.keys(response).forEach((key) => cy.log(key))
-      cy.request(
-        "DELETE",
-        `http://localhost:3030/posts/delete/${response.body[0]._id}`
-      ).then((response) => {
-        expect(response.body).to.eq("User IDs do not match");
-      });
-    });
+    // click delete button on post
+    cy.get(".post-delete-button").click();
+    cy.get();
   });
 });
