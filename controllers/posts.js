5d410c5a129e90bc70b3bd37129e6990dbb93564
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-13 16:37:59 +0100

    file upload code added

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,9 +1,18 @@
 const Post = require("../models/post");
-
+const Resize = require("../image/resize");
+const path = require('path');
 
 const PostsController = {
   Upload: async (req, res) => {
     await console.log('post');
+
+    const imagePath = path.join(__dirname, '/public/images');
+    const fileUpload = new Resize(imagePath);
+    if (!req.file) {
+      res.status(401).json({ error: 'Please provide an image' });
+    }
+    const filename = await fileUpload.save(req.file.buffer);
+    return res.status(200).json({ name: filename });
   },
 
   Like: async (req, res) => {
