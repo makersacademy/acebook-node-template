6cb7d689bf7739c3a6b6d9459ab4ed2bc92e37cf
alexandrashelley <alexbshelley@gmail.com>
2022-09-13 12:32:41 +0100

    progress up until lunch

--- controllers/likes.js
+++ controllers/likes.js
@@ -2,9 +2,25 @@ const Post = require("../models/post");
 
 const LikesController = {
   Create: (req, res) => {
-    
-    res.send();
+    userId = req.session.user._id;
+    postId = req.params.postId
 
+    Post.findOneAndUpdate(
+      { _id: postId },
+      { $addToSet: { likes: userId } },
+      { new: true },
+      (err, post) => {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log(`A like from ${userId} is added to ${postId}`)
+          const numberOfLikes = post.likes.length;
+
+          res.send("posts/index", {
+            counter: numberOfLikes
+          });
+        };
+      });
   },
 };
 
