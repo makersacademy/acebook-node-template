3522a477930127eaabe9cfa86f8eff601b144d83
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-13 14:53:13 +0100

    add body-parser capability to app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require("http-errors");
 const express = require("express");
+const bodyParser = require('body-parser');
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
@@ -24,6 +25,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride("_method"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(
   session({
