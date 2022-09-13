8deaa2d36db238a3831fc34e03509346660eb0be
David Meade Jr <davidmeadejnr@gmail.com>
2022-09-13 16:21:55 +0100

    Initial logo redirect started

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,12 @@
 const HomeController = {
   Index: (req, res) => {
-    res.render("home/index", { title: "Acebook" });
+    let username = null;
+    if (req.session.user) {
+      username = req.session.user.username;
+      console.log(username);
+    }
+    console.log(username);
+    res.render("home/index", { title: "Acebook", user: username });
   },
 };
 
