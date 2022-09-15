0f0368d49700ed2824841484e687f2374c49de41
Zac <zac@zacmoss.co.uk>
2022-09-15 12:57:17 +0100

    Passed all tests and refactored likes controller
    Co-authored-by: David Meade Jr. <davidmeadejr@users.noreply.github.com>
    Co-authored-by: Naomi Schlösser <naomischlosser@users.noreply.github.com>
    Co-authored-by: Harry Fox <hfox1@users.noreply.github.com>
    Co-authored-by: Syed <SNaqvi21@users.noreply.github.com>

--- controllers/likes.js
+++ controllers/likes.js
@@ -1,28 +1,39 @@
 const Post = require("../models/post");
 
 const LikesController = {
-  Create: (req, res) => {
+  Update: (req, res) => {
     const userId = req.session.user._id;
     const postId = req.params.postId;
 
     console.log("this post is being liked");
     console.log(postId);
 
-    Post.findOneAndUpdate(
-      { _id: postId },
-      { $addToSet: { likes: userId } },
-      { new: true },
-      (err, post) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(`A like from ${userId} is added to ${postId}`);
-          const numberOfLikes = post.likes.length;
-
-          res.send(JSON.stringify({ counter: numberOfLikes }));
-        }
+    Post.findOne({ _id: postId, likes: userId }, (err, post) => {
+      // checks to see if the user has liked the post
+      if (err) {
+        // do something if error
+        console.log(err);
+      } else {
+        // if user hasn't liked the post, add the userID to the likes array in the post entry
+        // if they have, remove the userId from the likes array
+        const updateObject = {};
+        const updateOperator = !post ? "$addToSet" : "$pull";
+        updateObject[updateOperator] = { likes: userId };
+        Post.findOneAndUpdate(
+          { _id: postId },
+          updateObject,
+          { new: true },
+          (err, post) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(`${userId} ${updateOperator} ${postId} likes`);
+              res.send(JSON.stringify({ counter: post.likes.length }));
+            }
+          }
+        );
       }
-    );
+    });
   },
 };
 
