39e86bf0d3053125066d8ebd92287c74dafd8f0e
Adrian Fee <adrian.fee@yapily.com>
2022-09-22 21:39:27 +0700

    begun working on adding user photos

--- app.js
+++ app.js
@@ -11,18 +11,64 @@ const homeRouter = require("./routes/home");
 const postsRouter = require("./routes/posts");
 const sessionsRouter = require("./routes/sessions");
 const usersRouter = require("./routes/users");
-const photoRouter = require("./routes/photos");
-const accountRouter = require("./routes/account");
+
 const app = express();
 var bodyParser = require('body-parser');
-
+var fs = require('fs');
 require('dotenv/config');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
-// Set EJS as templating engine
+  
+// Set EJS as templating engine 
 app.set("view engine", "ejs");
+
+var multer = require('multer');
+  
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+  
+var upload = multer({ storage: storage });
+var imgModel = require('./models/photo.js');
+
+app.get('/images', (req, res) => {
+  imgModel.find({}, (err, items) => {
+      if (err) {
+          console.log(err);
+          res.status(500).send('An error occurred', err);
+      }
+      else {
+          res.render('imagesPage', { items: items });
+      }
+  });
+});
+
+app.post('/images', upload.single('image'), (req, res, next) => {
+  
+  var obj = {
+      name: req.body.name,
+      desc: req.body.desc,
+      img: {
+          data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+          contentType: 'image/png'
+      }
+  }
+  imgModel.create(obj, (err, item) => {
+      if (err) {
+          console.log(err);
+      }
+      else {
+          // item.save();
+          res.redirect('/');
+      }
+  });
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
