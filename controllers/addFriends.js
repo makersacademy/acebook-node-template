8f8aa39388954ae721073ebf8560c0c1e2afbde9
MarcoDaz <80964100+MarcoDaz@users.noreply.github.com>
2022-09-27 15:56:19 +0100

    trying to get addFriends results to work

--- controllers/addFriends.js
+++ controllers/addFriends.js
@@ -1,12 +1,34 @@
+const User = require("../models/user");
+
 const addFriendsController = {
   Search: (req, res) => {
     if (!req.session.user) {
-      res.redirect("/sessions/new")
+      res.redirect("/sessions/new");
     } else {
+      const searchTerm = req.body.searchTerm;
+      let results = [];
 
-      res.render("addFriends/index")
+      if (!searchTerm) {
+        results = []
+        res.render("addFriends/index", { results: results });
+      } else {
+        // find Users
+        User.find({
+          name: { $regex: searchTerm },
+        }).then((users) => {
+          // get their images
+          users.forEach((user, i) => {
+            user.image = user.image.data.toString("base64");
+
+            // pass to results
+            results.push(user);
+          });
+
+          res.render("addFriends/index", { results: results });
+        });
+      }
     }
-  }
+  },
 };
 
 module.exports = addFriendsController;
