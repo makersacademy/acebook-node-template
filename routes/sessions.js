e1846042712b1641b42fc3091dd87dcf5e0f1fe0
Vishal Saigal <vishalsaigal@MASTUDENT-Vishal-Saigal-C17G44KFQ6L4.local>
2022-09-28 10:28:27 +0100

    deleted excess log statements

--- routes/sessions.js
+++ routes/sessions.js
@@ -2,20 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const SessionsController = require("../controllers/sessions");
-const { route } = require("./home");
-const logRouterDeclaration = (res, req, next) => {
-    console.log(res.body.loggingMessage)
-    res.body.loggingMessage = "logrouterdeclaration";
-    next();
-}
 
-const logNewRouterDeclaration = (res, req, next) => {
-    console.log(res.body.loggingMessage)
-    console.log("AND NOW WE SHOULD BE AT THE END")
-    next();
-}
-router.use(logRouterDeclaration)
-router.get("/new", logNewRouterDeclaration, SessionsController.New);
+router.get("/new", SessionsController.New);
 router.post("/", SessionsController.Create);
 router.delete("/", SessionsController.Destroy);
 
