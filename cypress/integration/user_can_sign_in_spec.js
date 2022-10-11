b453b0c672c7bd214c9314634d39767a13413a3e
Jonas Diete <jonas.diete@gmail.com>
2022-10-11 17:04:19 +0100

    extended signup page and tested for redirecting and contents correct

--- cypress/integration/user_can_sign_in_spec.js
+++ cypress/integration/user_can_sign_in_spec.js
@@ -1,16 +1,25 @@
+// const User = require('../../models/user');
+// const Post = require('../../models/post');
+
 describe("Authentication", () => {
+  // afterEach(() => {
+  //   db.users.collection.drop();
+  // });
+  
   it("A user signs in and is redirected to /posts", () => {
     // sign up
     cy.visit("/users/new");
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
+    cy.get("#name").type("name");
+    cy.get("#email").type("someone1@example.com");
+    cy.get("#password1").type("password");
+    cy.get("#password2").type("password");
     cy.get("#submit").click();
 
-    // sign in
-    cy.visit("/sessions/new");
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
+    // // sign in
+    // cy.visit("/sessions/new");
+    // cy.get("#email").type("someone1@example.com");
+    // cy.get("#password").type("password");
+    // cy.get("#submit").click();
 
     cy.url().should("include", "/posts");
     cy.contains("a", "New post");
--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -1,11 +1,55 @@
+// const User = require('../../models/user');
+// const Post = require('../../models/post');
+
 describe("Registration", () => {
-  it("A user signs up and is redirected to sign in", () => {
-    // sign up
+  // beforeEach(() => {
+  //   User.collection.drop();
+  //   Post.collection.drop();
+  // });
+
+  it("A user signs up and is redirected to home page", () => {
     cy.visit("/users/new");
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
+    cy.get("#name").type("name");
+    cy.get("#email").type("someone2@example.com");
+    cy.get("#password1").type("password");
+    cy.get("#password2").type("password");
     cy.get("#submit").click();
 
-    cy.url().should("include", "/sessions/new");
+    cy.url().should("include", "/posts");
   });
+
+  it("A user types two different passwords and is redirected to signing up page", () => {
+    cy.visit("/users/new");
+    cy.get("#name").type("name");
+    cy.get("#email").type("someone3@example.com");
+    cy.get("#password1").type("pass");
+    cy.get("#password2").type("password");
+    cy.get("#submit").click();
+
+    cy.url().should("include", "users/new");
+    cy.contains("div", "Passwords do not match");
+  });
+  // should test if email is already in the database
+  it ("A user types an email that is already in the database and is redirected to signing up page", () => {
+    cy.visit("/users/new");
+    cy.get("#name").type("name");
+    cy.get("#email").type("someone4@example.com");
+    cy.get("#password1").type("password");
+    cy.get("#password2").type("password");
+    cy.get("#submit").click();
+    // needs to click logout
+    cy.get("#logout").click();
+    // needs to sign up again
+    cy.visit("/users/new");
+    cy.get("#name").type("name");
+    cy.get("#email").type("someone4@example.com");
+    cy.get("#password1").type("password");
+    cy.get("#password2").type("password");
+    cy.get("#submit").click();
+    // should be redirected to sign up page
+    cy.url().should("include", "users/new");
+    // should see error message
+    cy.contains("div", "Email already exists.");
+  });
+
 });
