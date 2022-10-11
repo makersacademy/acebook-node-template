177a41b29e660657ceab5b9644980803d3420d47
Yichao <yq2550@gmail.com>
2022-10-11 16:33:01 +0100

    feat(unique email) - make email unique when creating in models/user.js. Handle duplicate email error in controllers/users.js
    Co-authored-by: 44jovi <kwong.jovi@gmail.com>

--- controllers/users.js
+++ controllers/users.js
@@ -9,7 +9,15 @@ const UsersController = {
     const user = new User(req.body);
     user.save((err) => {
       if (err) {
-        throw err;
+        if (err.name === "MongoError" && err.code === 11000) {
+          // Duplicate email
+          return res
+            .status(422)
+            .send({ success: false, message: "Email already exists!" })
+            .redirect("/users/new");
+        }
+        // Handle some other error
+        return res.status(422).send(err);
       }
       res.status(201).redirect("/posts");
     });
