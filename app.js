de6576f4e41215ee04ac7760de78adc45a935a0e
MillieKS <mkseverino95@gmail.com>
2022-10-11 07:26:09 -0700

    login authentiation and error messgae with incorrect credentials. Added Creat Acc button.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const session = require("express-session");
 const methodOverride = require("method-override");
+const flash = require('connect-flash'); // ADDED
 
 const homeRouter = require("./routes/home");
 const postsRouter = require("./routes/posts");
@@ -73,6 +74,13 @@ app.use((err, req, res) => {
   // render the error page
   res.status(err.status || 500);
   res.render("error");
+});
+
+app.use(flash());
+
+app.get('/sessions/new', (req, res) => {
+  req.flash('message', 'Invalid Details');
+  res.redirect('/sessions/new');
 });
 
 module.exports = app;
