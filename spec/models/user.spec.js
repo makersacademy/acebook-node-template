f8f153c21ad44db847118d939d106f2131e5d0cc
Yichao <yq2550@gmail.com>
2022-10-11 12:00:04 +0100

    feat(user.js) - add username to User Model. Pass unit tests.
    Co-authored-by: 44jovi <kwong.jovi@gmail.com>

--- spec/models/user.spec.js
+++ spec/models/user.spec.js
@@ -1,32 +1,43 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-require("../mongodb_helper");
-const User = require("../../models/user");
+require('../mongodb_helper');
+const User = require('../../models/user');
 
-describe("User model", () => {
+describe('User model', () => {
   beforeEach((done) => {
     mongoose.connection.collections.users.drop(() => {
       done();
     });
   });
 
-  it("has an email address", () => {
+  it('has an email address', () => {
     const user = new User({
-      email: "someone@example.com",
-      password: "password",
+      username: 'someone',
+      email: 'someone@example.com',
+      password: 'password',
     });
-    expect(user.email).toEqual("someone@example.com");
+    expect(user.email).toEqual('someone@example.com');
   });
 
-  it("has a password", () => {
+  it('has a password', () => {
     const user = new User({
-      email: "someone@example.com",
-      password: "password",
+      username: 'someone',
+      email: 'someone@example.com',
+      password: 'password',
     });
-    expect(user.password).toEqual("password");
+    expect(user.password).toEqual('password');
   });
 
-  it("can list all users", (done) => {
+  it('has an username', () => {
+    const user = new User({
+      username: 'someone',
+      email: 'someone@example.com',
+      password: 'password',
+    });
+    expect(user.username).toEqual('someone');
+  });
+
+  it('can list all users', (done) => {
     User.find((err, users) => {
       expect(err).toBeNull();
       expect(users).toEqual([]);
@@ -34,10 +45,11 @@ describe("User model", () => {
     });
   });
 
-  it("can save a user", (done) => {
+  it('can save a user', (done) => {
     const user = new User({
-      email: "someone@example.com",
-      password: "password",
+      username: 'someone',
+      email: 'someone@example.com',
+      password: 'password',
     });
 
     user.save((err) => {
@@ -47,8 +59,9 @@ describe("User model", () => {
         expect(err).toBeNull();
 
         expect(users[0]).toMatchObject({
-          email: "someone@example.com",
-          password: "password",
+          username: 'someone',
+          email: 'someone@example.com',
+          password: 'password',
         });
         done();
       });
