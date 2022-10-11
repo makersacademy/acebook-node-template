1ca7410e7e1d05901fd30a8d333290f84a4e1eea
Yichao <yq2550@gmail.com>
2022-10-11 16:33:01 +0100

    feat(unique email) - make email unique when creating in models/user.js. Handle duplicate email error in controllers/users.js
    Co-authored-by: 44jovi <kwong.jovi@gmail.com>

--- models/user.js
+++ models/user.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = mongoose.model("User", UserSchema);
