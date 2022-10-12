ff4eb8443cdd23fa060a3361251eb26e016fa446
Jonas Diete <jonas.diete@gmail.com>
2022-10-12 12:23:03 +0100

    fixed tests for login and signup

--- cypress/integration/user_can_sign_in_spec.js
+++ cypress/integration/user_can_sign_in_spec.js
@@ -1,10 +1,4 @@
-// const User = require('../../models/user');
-// const Post = require('../../models/post');
-
 describe("Authentication", () => {
-  // afterEach(() => {
-  //   db.users.collection.drop();
-  // });
   
   it("A user signs in and is redirected to /posts", () => {
     // sign up
@@ -15,24 +9,31 @@ describe("Authentication", () => {
     cy.get("#password2").type("password");
     cy.get("#submit").click();
 
-    // // sign in
-    // cy.visit("/sessions/new");
-    // cy.get("#email").type("someone1@example.com");
-    // cy.get("#password").type("password");
-    // cy.get("#submit").click();
+    //logout
+    cy.get("#logout").click();
+
+    // sign in
+    cy.get("#email").type("someone1@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
 
     cy.url().should("include", "/posts");
     cy.contains("a", "New post");
   });
+
   it("show error message when incorrect credentials have been inputted", () => {
     // sign up
     cy.visit("/users/new");
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
+    cy.get("#name").type("name");
+    cy.get("#email").type("someone1@example.com");
+    cy.get("#password1").type("password");
+    cy.get("#password2").type("password");
     cy.get("#submit").click();
 
+    //logout
+    cy.get("#logout").click();
+
     // sign in
-    cy.visit("/sessions/new");
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("pass");
     cy.get("#submit").click();
