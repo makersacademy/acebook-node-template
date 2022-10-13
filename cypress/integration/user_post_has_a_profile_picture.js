049531cddaedc32a5946733cffd46660aaa9b759
Liovirgilda Mendonca <liovirgilda_developer@gmail.com>
2022-10-13 15:51:48 +0100

    integration test for profile_picture

---
+++ cypress/integration/user_post_has_a_profile_picture.js
@@ -0,0 +1,30 @@
+describe("Timeline", () => {
+    it("can submit posts, when signed in, and view them", () => {
+      // sign up
+      cy.visit("/users/new");
+      cy.get("#name").type("name");
+      cy.get("#email").type("someone@example.com");
+      cy.get("#password1").type("password");
+      cy.get("#password2").type("password");
+      cy.get("#submit").click();
+  
+      // sign in
+      cy.visit("/sessions/new");
+      cy.get("#email").type("someone@example.com");
+      cy.get("#password").type("password");
+      cy.get("#submit").click();
+  
+      // submit a post
+  
+      cy.visit("/posts");
+      cy.contains("New post").click();
+  
+      cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
+      cy.get("#new-post-form").submit();
+  
+      // checks if img element created
+      
+      cy.get('.profile_picture').parent('p');
+    });
+  });
+  
