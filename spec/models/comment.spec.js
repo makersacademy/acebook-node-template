7d837a674bab3a1b2c726468f12911d973646186
postie89 <t.mannion89@gmail.com>
2022-10-13 16:03:43 +0100

    added unit test for comments

---
+++ spec/models/comment.spec.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+
+require("../mongodb_helper");
+var Comment = require("../../models/comment");
+
+describe("Comment model", () => {
+  it("has a message", () => {
+    var comment = new Comment({ message: "some message" });
+    expect(comment.message).toEqual("some message");
+  });
+  it("has a name", () => {
+    var comment = new Comment({ name: "some name" });
+    expect(comment.name).toEqual("some name");
+  });
+  it("has a createdAt", () => {
+    var comment = new Comment({ createdAt: "some date" });
+    expect(comment.createdAt).toEqual("some date");
+  });
+});
