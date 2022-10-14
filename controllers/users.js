22787e006ed166aacc645abfb1d61cf512e52446
Guillermina Lorenzo Martinez <guillerminalorenzomartinez@MASTUDE5H47Q6L4.localdomain>
2022-10-14 11:21:15 +0100

    Password validation

--- controllers/users.js
+++ controllers/users.js
@@ -10,8 +10,19 @@ const UsersController = {
     let errorMessage = "";
     user.name = req.body.name;
     user.email = req.body.email;
+
     if (req.body.password1 === req.body.password2) {
-      user.password = req.body.password1;    
+      user.password = req.body.password1;   
+      
+      let passwordExpression = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+
+      if (user.password.match(passwordExpression) ) {
+        req.session.user = user;
+        res.status(201).redirect("/posts");
+      } else {
+        errorMessage = "Password must be 8-15 characters and contain an uppercase, lowercase, numeric and special character"
+        res.render("users/new", {layout: false, error: errorMessage});
+      }
       user.save((err) => {
         if (err) {
           errorMessage = "Email already exists."
