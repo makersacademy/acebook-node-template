150c4a638f36b344e677a7caf4d74ad75737d457
ritaaktay <ritaaktay@gmail.com>
2022-10-17 11:46:18 +0100

    testing posts model with embedded comments

--- models/post.js
+++ models/post.js
@@ -7,11 +7,29 @@ const PostSchema = new mongoose.Schema({
     default: () => Date.now(),
     immutable: true,
   },
-  author: { 
+  author: {
     type: mongoose.SchemaTypes.ObjectId,
     ref: "User",
     required: true,
   },
+  comments: [
+    {
+      message: {
+        type: String,
+        required: true,
+      },
+      author: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: () => Date.now(),
+        immutable: true,
+      },
+    },
+  ],
 });
 
 const Post = mongoose.model("Post", PostSchema);
--- spec/models/post.spec.js
+++ spec/models/post.spec.js
@@ -37,7 +37,7 @@ describe("Post model", () => {
   it("can save a post", (done) => {
     var post = new Post({
       message: "some message",
-      author: "123456789012345678901234",
+      author: "123456789012345678901234", //has to be exact length as a Mongo ObjectID!
     });
     post.save((err) => {
       expect(err).toBeNull();
@@ -47,6 +47,92 @@ describe("Post model", () => {
         expect(posts[0]).toMatchObject({ message: "some message" });
         expect(posts[0].author).toMatchObject(
           new mongoose.mongo.ObjectId("123456789012345678901234")
+        );
+        done();
+      });
+    });
+  });
+
+  it("can add a comment to a post", (done) => {
+    var post = new Post({
+      message: "some message",
+      author: "123456789012345678901234",
+      createdAt: 1665497979886,
+      comments: [
+        {
+          message: "a comment",
+          author: "123456789012345678901234",
+          createdAt: 1665497979886,
+        },
+      ],
+    });
+    post.save((err) => {
+      expect(err).toBeNull();
+
+      Post.find((err, posts) => {
+        expect(err).toBeNull();
+        expect(posts[0]).toMatchObject({ message: "some message" });
+        expect(posts[0].createdAt.toString()).toEqual(
+          "Tue Oct 11 2022 15:19:39 GMT+0100 (British Summer Time)"
+        );
+        expect(posts[0].author).toMatchObject(
+          new mongoose.mongo.ObjectId("123456789012345678901234")
+        );
+        expect(posts[0].comments[0]).toMatchObject({
+          message: "a comment",
+          author: new mongoose.mongo.ObjectId("123456789012345678901234"),
+        });
+        expect(posts[0].comments[0].createdAt.toString()).toEqual(
+          "Tue Oct 11 2022 15:19:39 GMT+0100 (British Summer Time)"
+        );
+        done();
+      });
+    });
+  });
+
+  it("can add multiple comments to a post", (done) => {
+    var post = new Post({
+      message: "some message",
+      author: "123456789012345678901234",
+      createdAt: 1665497979886,
+      comments: [
+        {
+          message: "a comment",
+          author: "123456789012345678901234",
+          createdAt: 1665497979886,
+        },
+        {
+          message: "another comment",
+          author: "123456789012345678901235",
+          createdAt: 1665497979886,
+        },
+      ],
+    });
+    post.save((err) => {
+      expect(err).toBeNull();
+
+      Post.find((err, posts) => {
+        expect(err).toBeNull();
+        expect(posts[0]).toMatchObject({ message: "some message" });
+        expect(posts[0].createdAt.toString()).toEqual(
+          "Tue Oct 11 2022 15:19:39 GMT+0100 (British Summer Time)"
+        );
+        expect(posts[0].author).toMatchObject(
+          new mongoose.mongo.ObjectId("123456789012345678901234")
+        );
+        expect(posts[0].comments[0]).toMatchObject({
+          message: "a comment",
+          author: new mongoose.mongo.ObjectId("123456789012345678901234"),
+        });
+        expect(posts[0].comments[0].createdAt.toString()).toEqual(
+          "Tue Oct 11 2022 15:19:39 GMT+0100 (British Summer Time)"
+        );
+        expect(posts[0].comments[1]).toMatchObject({
+          message: "another comment",
+          author: new mongoose.mongo.ObjectId("123456789012345678901235"),
+        });
+        expect(posts[0].comments[1].createdAt.toString()).toEqual(
+          "Tue Oct 11 2022 15:19:39 GMT+0100 (British Summer Time)"
         );
         done();
       });
