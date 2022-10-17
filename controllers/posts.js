e24f60ec7163196157bdb08a001b7b23636ddfb0
ritaaktay <ritaaktay@gmail.com>
2022-10-17 14:10:56 +0100

    users can add and see comments

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,16 +1,20 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 
 const PostsController = {
   Index: (req, res) => {
     Post.find({})
       .sort([["createdAt", -1]])
       .populate("author")
+      .populate({
+        path: "comments",
+        populate: { path: "author" },
+      })
       .exec((err, posts) => {
         if (err) {
           throw err;
         }
-
-        // console.log(posts);
+        console.log(posts[0].comments[0].message);
         res.render("posts/index", { posts: posts });
       });
   },
@@ -37,8 +41,23 @@ const PostsController = {
       if (err) {
         throw err;
       }
-      // console.log(post.like);  // need to add post to .exec to use
+      // need to add post to .exec to use
       res.status(201).redirect("/posts");
+    });
+  },
+  Comment: (req, res) => {
+    const comment = new Comment({
+      message: req.body.content,
+      author: req.session.user._id,
+    });
+    comment.save((err, comment) => {
+      Post.updateOne(
+        { _id: req.body.postid },
+        { $addToSet: { comments: comment._id } }
+      ).exec((err, post) => {
+        if (err) throw err;
+        res.redirect("/posts");
+      });
     });
   },
 };
