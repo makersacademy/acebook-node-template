8e14039d79ec94760b3754b28b7808f8842865a0
Vanboylan <108574954+Vanboylan@users.noreply.github.com>
2022-10-17 15:54:10 +0100

    test fixed

--- cypress/integration/user_can_sign_in_spec.js
+++ cypress/integration/user_can_sign_in_spec.js
@@ -16,7 +16,7 @@ describe("Authentication", () => {
     cy.get("#submit").click();
 
     cy.url().should("include", "/posts");
-    cy.visit("/posts").should("contain", "Make a new post");
+    cy.visit("/posts").get('.title').should("contain", "Timeline");
   });
   it("Sign in page has a new title", () => {
     // sign up
