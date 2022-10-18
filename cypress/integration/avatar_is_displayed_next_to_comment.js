97f79eaac446148cb7661256d4f70e1892e5fa52
ritaaktay <ritaaktay@gmail.com>
2022-10-18 11:43:46 +0100

    comment author image tested

--- cypress/integration/avatar_is_displayed_next_to_comment.js
+++ cypress/integration/avatar_is_displayed_next_to_comment.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 describe("Timeline", () => {
-  xit("can submit posts, when signed in, and view them", () => {
+  it("can submit posts, when signed in, and view them", () => {
     // sign up
     cy.visit("/users/new");
     cy.get("#name").type("someone");
@@ -15,15 +15,6 @@ describe("Timeline", () => {
     cy.get("#password").type("password");
     cy.get("#submit").click();
 
-    // get /settings
-    cy.get("#settings-navbar").click();
-
-    // skip checking for default avatar (see other test(s))
-    cy.get("#choose-file").selectFile(
-      path.join(__dirname, "..", "..", "public", "images", "cat1.png")
-    );
-    cy.get("#upload-file").click();
-
     // submit a post
     cy.visit("/posts");
     cy.contains("Make a post").click();
@@ -32,18 +23,21 @@ describe("Timeline", () => {
     cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
     cy.get("#submit").click();
 
-    // new post is displayed with the author information and its avatar
-    cy.get(".post")
-      .first() // or .eq(0)
-      .should("contain", "Hello, world!")
-      .should("contain", "someone");
+    //submit a comment
+    cy.get(".comment-input:first").type("A comment");
+    cy.get(".submit-comment:first").click();
 
+    // new comment is displayed with the author information and its avatar
     cy.readFile(
-      path.join(__dirname, "..", "..", "public", "images", "cat1.png"),
+      path.join(__dirname, "..", "..", "public", "images", "testImage.png"),
       "base64"
     ).then((image) => {
       const src = `data:image/png;base64,${image}`;
-      cy.get(".author-post-image").first().should("have.attr", "src", src);
+      cy.get(".post:first")
+        .find(".comment:first")
+        .within(() => {
+          cy.get(".author-comment-image").should("have.attr", "src", src);
+        });
     });
   });
 });
