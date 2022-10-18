76fb572ea5788a8acde133fd6d94840b08500e90
James Ruffini <34510364+iniffur@users.noreply.github.com>
2022-10-18 10:42:38 +0100

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -6,8 +6,6 @@ const PostsController = require("../controllers/posts");
 router.get("/", PostsController.Index);
 router.post("/", PostsController.Create);
 router.post("/like", PostsController.Like);
-// router.get("/new", PostsController.New);
-// router.get("/files", PostsController.files);
 router.post("/:id", PostsController.CreateComment);
 
 module.exports = router;
