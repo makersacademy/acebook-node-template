76c1ef627ea3eb1a063161ff154ac8cf1bbb9a3b
Abdullah <adaoub0@gmail.com>
2022-10-19 10:29:21 +0100

    test driven delete functionality for posts

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -18,7 +18,9 @@ describe("Timeline", () => {
 
     // cy.get(".new-post-btn").click();
 
-    cy.get("#new-post-form").find('[type="text"]', {force: true}).type("Hello, world!", {force: true});
+    cy.get("#new-post-form")
+      .find('[type="text"]', { force: true })
+      .type("Hello, world!", { force: true });
     cy.get("#new-post-form").submit();
 
     cy.get(".posts").should("contain", "Hello, world!");
@@ -42,26 +44,33 @@ describe("Timeline", () => {
 
     // cy.contains("New post").click();
 
-    cy.get("#new-post-form").find('[type="text"]', {force: true}).type("Hello, world!", {force: true});
+    cy.get("#new-post-form")
+      .find('[type="text"]', { force: true })
+      .type("Hello, world!", { force: true });
     cy.get("#new-post-form").submit();
     // cy.contains("New post").click({force:true});
-    cy.get("#new-post-form").find('[type="text"]', {force: true}).type("AAAAA", {force: true});
+    cy.get("#new-post-form")
+      .find('[type="text"]', { force: true })
+      .type("AAAAA", { force: true });
     cy.get("#new-post-form").submit();
     // cy.contains("New post").click({force:true})
-    cy.get("#new-post-form").find('[type="text"]', {force: true}).type("BBB", {force: true});
+    cy.get("#new-post-form")
+      .find('[type="text"]', { force: true })
+      .type("BBB", { force: true });
     cy.get("#new-post-form").submit();
     // cy.contains("New post").click({force:true});
-    cy.get("#new-post-form").find('[type="text"]', {force: true}).type("a", {force: true});
+    cy.get("#new-post-form")
+      .find('[type="text"]', { force: true })
+      .type("a", { force: true });
     cy.get("#new-post-form").submit();
 
     cy.get(".posts").first().should("contain", "a");
     cy.get(".posts").last().should("contain", "Hello, world!");
   });
-  
 
   it("the timeline has the logged user first name", () => {
     // sign up
-    cy.contains('Log Out').click({force:true})
+    cy.contains("Log Out").click({ force: true });
     cy.visit("/users/new");
     cy.get("#firstName").type("Bob");
     cy.get("#lastName").type("John");
@@ -74,7 +83,7 @@ describe("Timeline", () => {
     cy.get("#password").type("password");
     cy.get("#submit").click();
 
-  //   //check if first name of logged in user appears
+    //   //check if first name of logged in user appears
 
     cy.visit("/posts");
     cy.get(".title").contains("Hi Bob! Welcome to the TimeLine");
@@ -96,7 +105,9 @@ describe("Timeline", () => {
     //submit a post
     cy.visit("/posts");
     // cy.contains("New post").click({force:true});
-    cy.get("#new-post-form").find('[type="text"]', {force: true}).type("Hello, world!", {force: true});
+    cy.get("#new-post-form")
+      .find('[type="text"]', { force: true })
+      .type("Hello, world!", { force: true });
     cy.get("#new-post-form").submit();
     //check if first name of logged in user appears
     cy.visit("/posts");
@@ -119,7 +130,9 @@ describe("Timeline", () => {
     //submit a post
     cy.visit("/posts");
     // cy.contains("New post").click({force:true});
-    cy.get("#new-post-form").find('[type="text"]', {force: true}).type("Hello, world!", {force: true});
+    cy.get("#new-post-form")
+      .find('[type="text"]', { force: true })
+      .type("Hello, world!", { force: true });
     cy.get("#new-post-form").submit();
     // like post
     // cy.contains("likes").click();
@@ -145,7 +158,9 @@ describe("Timeline", () => {
     //submit a post
     cy.visit("/posts");
     // cy.contains("New post").click({force:true});
-    cy.get("#new-post-form").find('[type="text"]', {force: true}).type("Hello, world!", {force: true});
+    cy.get("#new-post-form")
+      .find('[type="text"]', { force: true })
+      .type("Hello, world!", { force: true });
     cy.get("#new-post-form").submit();
 
     // comment on a post
@@ -153,9 +168,9 @@ describe("Timeline", () => {
     // cy.visit("/posts");
 
     cy.get("#new-comment-form")
-      .find('[type="text"]', {force: true})
+      .find('[type="text"]', { force: true })
       .first()
-      .type("my first comment", {force:true});
+      .type("my first comment", { force: true });
     cy.get("#new-comment-form").submit();
 
     // get posts with comments
@@ -170,19 +185,76 @@ describe("Timeline", () => {
     cy.get("#lastName").type("John");
     cy.get("#email").type("a2@a.com");
     cy.get("#password").type("password");
-    cy.get("#submit").click({force:true});
+    cy.get("#submit").click({ force: true });
     // sign in
     cy.visit("/sessions/new");
     cy.get("#email").type("a2@a.com");
     cy.get("#password").type("password");
-    cy.get("#submit").click({force:true});
+    cy.get("#submit").click({ force: true });
     //submit a post
     cy.visit("/posts");
     // cy.contains("New post").click({force:true});
-    cy.get("#new-post-form").find('[type="text"]', {force: true}).type("Hello, world!", {force: true});
+    cy.get("#new-post-form")
+      .find('[type="text"]', { force: true })
+      .type("Hello, world!", { force: true });
     cy.get("#new-post-form").submit();
     // check user photo in the post
     cy.visit("/posts");
-    cy.get(".posts").find("img").should('have.attr', 'src', '/images/default-pic.png')
+    cy.get(".posts")
+      .find("img")
+      .should("have.attr", "src", "/images/default-pic.png");
+  });
+
+  it("post has a delete button", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#firstName").type("Bob");
+    cy.get("#lastName").type("John");
+    cy.get("#email").type("new@new.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click({ force: true });
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("new@new.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click({ force: true });
+    //submit a post
+    cy.visit("/posts");
+
+    cy.get("#new-post-form")
+      .find('[type="text"]', { force: true })
+      .type("Hello, world!", { force: true });
+    cy.get("#new-post-form").submit();
+    // check if delete button is present
+    cy.visit("/posts");
+    cy.get("#delete_button").contains("Delete");
+  });
+
+  //click on delete button and check if post is deleted
+  it("delete a post", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#firstName").type("Bob");
+    cy.get("#lastName").type("John");
+    cy.get("#email").type("new1@new.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click({ force: true });
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("new1@new.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click({ force: true });
+    //submit a post
+    cy.visit("/posts");
+
+    cy.get("#new-post-form")
+      .find('[type="text"]', { force: true })
+      .type("Hello, world Again!", { force: true });
+    cy.get("#new-post-form").submit();
+    // find delete button and click it
+    cy.visit("/posts");
+    cy.get("#delete_button").click({ force: true });
+    // check if post is deleted
+    cy.get(".posts").should("not.contain", "Hello, world Again!");
   });
 });
