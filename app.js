316f58338564232d6d7f09d609431d6373893d29
Leroy Jallow <leroyj@MASTUDE5H2RQ6L4.broadband>
2022-10-19 17:25:03 +0100

    css acebook

--- app.js
+++ app.js
@@ -58,12 +58,12 @@ const sessionChecker = (req, res, next) => {
 };
 
 // route setup
+app.use("/users", usersRouter);
 app.use("/users/index", sessionChecker, usersRouter);
 app.use("/users/:id", sessionChecker, usersRouter);
 app.use("/", homeRouter);
 app.use("/posts", sessionChecker, postsRouter);
 app.use("/sessions", sessionsRouter);
-app.use("/users", usersRouter);
 
 
 // catch 404 and forward to error handler
