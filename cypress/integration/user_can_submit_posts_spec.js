930d3c3736a6ac2138c853efffc8187624385b5c
RobMRowe <>
2022-11-16 15:04:56 +0000

    separate tests

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -12,20 +12,6 @@ describe("Timeline", () => {
     cy.get("#password").type("password");
     cy.get("#submit").click();
 
-    // user cannot submit empty post
-    cy.visit("/posts");
-
-    const stub = cy.stub()
-    cy.on('window:alert', stub)
-
-    cy.contains("New post").click();
-
-    cy.get("#new-post-form").submit();
-
-    cy.then(() => {
-      expect(stub.getCall(0)).to.be.calledWith('Post cannot be empty!')
-    })
-
     // submit a post
     cy.visit("/posts");
     cy.contains("New post").click();
@@ -34,11 +20,56 @@ describe("Timeline", () => {
     cy.get("#new-post-form").submit();
 
     cy.get(".posts").should("contain", "Hello, world!");
+  });
+
+  it("doesn't accept empty posts", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // user cannot submit empty post
+    cy.visit("/posts");
+    cy.contains("New post").click();
+
+    const stub = cy.stub()
+    cy.on('window:alert', stub)
+
+    cy.get("#new-post-form").submit();
+
+    cy.then(() => {
+      expect(stub.getCall(0)).to.be.calledWith('Post cannot be empty!')
+    })
+  });
+
+  it("shows the most recent posts first", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
 
     // receive posts sorted by most recent first
     cy.visit("/posts");
     cy.contains("New post").click();
+    cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
+    cy.get("#new-post-form").submit();
 
+    cy.visit("/posts");
+    cy.contains("New post").click();
     cy.get("#new-post-form").find('[type="text"]').type("Goodbye, world!");
     cy.get("#new-post-form").submit();
 
