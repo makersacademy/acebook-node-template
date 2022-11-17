5864680e15b5a21116ca924d6acb65d1ea6b3717
Manaswini Salgerker <manaswini.salgarkar93@gmail.com>
2022-11-17 16:02:55 +0000

    added  comments

--- models/post.js
+++ models/post.js
@@ -4,6 +4,7 @@ const PostSchema = new mongoose.Schema({
     message: String,
     likes: { type: Number, default: 0 },
     userID: {type: String},
+    // tags post to its user:author holds objectID of that User.
     author: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
