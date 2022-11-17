81d75835939b36c7f1195d723f9215aec1e05159
annaobarzanowska <anna.obarzanowska@gmail.com>
2022-11-17 16:55:57 +0000

    adjusted signing up to force user to input all values and adjusted message form to force user to have to input something before submitting

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -37,4 +37,15 @@ describe('Timeline', () => {
     cy.get('#new-post-form').submit()
     cy.get('.posts').eq(0).should('contain.text', 'Goodbye, world!')
   })
+  it('cannot submit an empty post', () => {
+    cy.visit('/sessions/new')
+    cy.get('#email').type('someone@example.com')
+    cy.get('#password').type('password')
+    cy.get('#submit').click()
+
+    cy.visit('/posts')
+    cy.contains('New post').click()
+    cy.get('#new-post-form').submit()
+    cy.url().should('include', '/posts/new')
+  })
 })
