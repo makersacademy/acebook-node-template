9f3a568a729862d2eed07ce5ad2d1877276faa37
akettlet <akettlet@gmail.com>
2022-11-18 11:50:46 +0000

    added tests to see if submit button is disabled

--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -20,4 +20,56 @@ describe("Registration", () => {
 
     cy.url().should("include", "/users/new");
   });
+
+  it("User tries to submit without an email address", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#password").type("Password1$");
+    cy.get("#name").type("Testing User");
+    cy.get('#submit').should('be.disabled')
+  });
+
+  it("User tries to submit without a user name", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("Password1$");
+    cy.get('#submit').should('be.disabled')
+  });
+
+  it("User tries to submit with a short password", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#name").type("Testing User");
+    cy.get("#password").type("Prd1$");
+    cy.get('#submit').should('be.disabled')
+  });
+
+  it("User tries to submit with a password without number", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#name").type("Testing User");
+    cy.get("#password").type("Password$");
+    cy.get('#submit').should('be.disabled')
+  });
+
+  it("User tries to submit with a password without special charecter", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#name").type("Testing User");
+    cy.get("#password").type("Password1");
+    cy.get('#submit').should('be.disabled')
+  });
+
+  it("User tries to submit with a password without capital letter", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#name").type("Testing User");
+    cy.get("#password").type("password1");
+    cy.get('#submit').should('be.disabled')
+  });
 });
