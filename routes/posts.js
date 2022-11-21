1562167b0ea1cb7fe92d4255366e6159ec1defb0
Benjamin Cambell <113980042+BnjCam25@users.noreply.github.com>
2022-11-21 12:29:52 +0000

    added functionality

--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,7 @@ const PostsController = require('../controllers/posts')
 
 router.get('/', PostsController.Index)
 router.post('/', PostsController.Create)
+router.post('/', PostsController.Update)
 router.get('/new', PostsController.New)
 router.get('/profile', PostsController.Profile)
 
