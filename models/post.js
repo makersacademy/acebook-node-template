4422158f3f9d67d0e9850965ac71dc745c52e1c7
Benjamin Cambell <113980042+BnjCam25@users.noreply.github.com>
2022-11-22 11:18:30 +0000

    somwhat working

--- models/post.js
+++ models/post.js
@@ -3,7 +3,7 @@ const User = require('../models/user')
 
 const PostSchema = new mongoose.Schema({
   message: String,
-  comments: Array,
+  comments: {type: Array, default: []},
   likes: { type: Number, default: 0 },
   // connects to id from User schema
   user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true}
--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,7 @@ const PostsController = require('../controllers/posts')
 
 router.get('/', PostsController.Index)
 router.post('/', PostsController.Create)
-router.post('/', PostsController.Update)
+router.post('/comments', PostsController.Comments)
 router.get('/new', PostsController.New)
 router.get('/profile', PostsController.Profile)
 
