3b1d5501408d00738cc572b0d9c46af0dbecce62
annaobarzanowska <anna.obarzanowska@gmail.com>
2022-11-22 11:59:27 +0000

    edits to commenting function

--- models/post.js
+++ models/post.js
@@ -4,6 +4,7 @@ const User = require('../models/user')
 const PostSchema = new mongoose.Schema({
   message: String,
   comments: {type: Array, default: []},
+  // commenters: {type: Array, default: []},
   likes: { type: Number, default: 0 },
   // connects to id from User schema
   user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true}
