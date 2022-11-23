0aa93435b69d7d2daf8453e695b25140deeea318
Benjamin Cambell <113980042+BnjCam25@users.noreply.github.com>
2022-11-23 11:36:18 +0000

    Password encrypts and logs in

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+var bcrypt = require('bcrypt')
+const SALT_WORK_FACTOR = 10
 
 const UserSchema = new mongoose.Schema({
   email: String,
@@ -8,6 +10,34 @@ const UserSchema = new mongoose.Schema({
   DOB: String,
   posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
 })
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+      if (err) return next(err);
+
+      // hash the password using our new salt
+      bcrypt.hash(user.password, salt, function(err, hash) {
+          if (err) return next(err);
+          // override the cleartext password with the hashed one
+          user.password = hash;
+          next();
+      });
+  });
+});
+   
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+      if (err) return cb(err);
+      cb(null, isMatch);
+  });
+};
+   
 
 const User = mongoose.model('User', UserSchema)
 
