0a2b2a771e9ac3e82b9f3040aaedce25349fec19
Benjamin Cambell <113980042+BnjCam25@users.noreply.github.com>
2022-11-23 10:21:20 +0000

    Update app.js

--- app.js
+++ app.js
@@ -17,26 +17,6 @@ const app = express()
 var hbs = require('hbs');
 hbs.registerHelper('dateFormat', require('handlebars-dateformat'));
 
-hbs.registerHelper(`ifEquals`, function (a, b, opts) {
-  if (a === b) {
-      return opts.fn(this);
-  }
-  return opts.inverse(this);
-});
-hbs.registerHelper( "when",function(operand_1, operator, operand_2, options) {
-  var operators = {
-   'eq': function(l,r) { return l == r; },
-   'noteq': function(l,r) { return l != r; },
-   'gt': function(l,r) { return Number(l) > Number(r); },
-   'or': function(l,r) { return l || r; },
-   'and': function(l,r) { return l && r; },
-   '%': function(l,r) { return (l % r) === 0; }
-  }
-  , result = operators[operator](operand_1,operand_2);
-  if (result) return options.fn(this);
-  else  return options.inverse(this);
-});
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
