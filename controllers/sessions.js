9b625b67610f76a7a5258fc1a0551948afc752fa
annaobarzanowska <anna.obarzanowska@gmail.com>
2022-11-23 10:33:48 +0000

    added password changes to Cypress

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -12,7 +12,7 @@ const SessionsController = {
   Create: (req, res) => {
     console.log('trying to log in')
     const email = req.body.email
-    const  = req.body.password
+    const password = req.body.password
 
     User.findOne({ email }).then((user) => {
       if (!user) {
