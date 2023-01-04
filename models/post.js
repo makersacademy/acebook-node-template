63fc9b48692b397cf48a9c32e738205c4d4ea0d5
Tanya Sahin <tanyasahin123@gmail.com>
2023-01-04 17:20:07 +0000

    homepage

--- models/post.js
+++ models/post.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
   message: String,
-  likes: { type: Number, default: 0 }
+  likes: { type: Number, default: 0 },
+  date: { type: Date, default:Date.now }
 });
 
 const Post = mongoose.model("Post", PostSchema);
