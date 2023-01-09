c277e95afd5e1f1870d6200df4d80361f169b2cb
Caroline <carolinegibson92@outlook.com>
2023-01-09 14:50:53 +0000

    Added usernames/ follow/ redirects for sign ins?

--- controllers/users.js
+++ controllers/users.js
@@ -8,23 +8,39 @@ const UsersController = {
   Create: (req, res) => {
     const newuser = new User(req.body);
     const email = req.body.email
+    const username = req.body.username
     User.findOne({ email: email }).then((user) => {
       //if user doesn't exist in the db, redirects to /new which creates a new account
       if (!user)       {
+        User.findOne({ username: username }).then((name) => {
+            if (!name) {                 
         newuser.save((err) => {
       if (err) {
         throw err;
       }
-      res.status(201).redirect("/posts");
+      //res.status(201).redirect("/posts");
+      
+        var accountCreated = true;
+        res.render("home/index", {accountCreated})
     })}
-    //if user doesn't exist, save it
-      else
+
+    else
       {
         console.log('User already exists!')
-        var emailExists = true;
+        var usernameExists = true;
+        res.render("home/index", {usernameExists})
+      } 
+
+  })}
+    //if user doesn't exist, save it
+      
+    else
+      {
+        console.log('User already exists!')
+        var emailExists = true;        
         res.render("home/index", {emailExists})
       } 
-    })
+  })
   }
 }
 
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
+  username: {type: String, unique: true},
   email: {type: String, unique: true},
   password: String,
 });
