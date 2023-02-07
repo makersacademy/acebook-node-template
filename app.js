e27d2958f2add1d51ec4f566c304d6136f8096b1
alex-jianu <alextj160398@gmail.com>
2023-02-07 14:20:15 +0000

    home page

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "/public/images")));
 app.use(methodOverride("_method"));
 
 app.use(
