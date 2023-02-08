ab05b03d0186746a504355db441a7626f6ac4168
Avi Patel <avipatel98@gmail.com>
2023-02-08 16:10:20 +0000

    fix sign up test for validation

--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -2,9 +2,9 @@ describe("Registration", () => {
   it("A user signs up and is redirected to sign in", () => {
     // sign up
     cy.visit("/users/new");
-    cy.get("#username").type("username");
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
+    cy.get("#username").type("TestUserNames");
+    cy.get("#email").type("testusers@example.com");
+    cy.get("#password").type("Makers12345.");
     cy.get("#submit").click();
 
     cy.url().should("include", "/sessions/new");
