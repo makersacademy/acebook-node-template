e3f145fbd6749f9b3178060f7028d68649ea72a7
alex-jianu <alextj160398@gmail.com>
2023-02-08 14:28:32 +0000

    cypress fixed

--- cypress/integration/user_can_see_posts_count_on_post.js
+++ cypress/integration/user_can_see_posts_count_on_post.js
@@ -1,28 +1,29 @@
+/* eslint-disable quotes */
 describe("Timeline", () => {
-    it("can see likes count on a new post", () => {
-      // sign up
-      cy.visit("/users/new");
-      cy.get("#email").type("someone@example.com");
-      cy.get("#password").type("password");
-      cy.get("#submit").click();
-  
-      // sign in
-      cy.visit("/sessions/new");
-      cy.get("#email").type("someone@example.com");
-      cy.get("#password").type("password");
-      cy.get("#submit").click();
-  
-      // submit a post
-      cy.visit("/posts");
-      cy.contains("New post").click();
-  
-      cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
-      cy.get("#new-post-form").submit();
-  
-      cy.get(".posts").should("contain", "Hello, world!");
+  it("can see likes count on a new post", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#username").type("someusername");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
 
-      // Assert that we can see the likes count
-      cy.get(".posts").should("contain", "0 likes");
-    });
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // submit a post
+    cy.visit("/posts");
+    cy.contains("New post").click();
+
+    cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
+    cy.get("#new-post-form").submit();
+
+    cy.get(".posts").should("contain", "Hello, world!");
+
+    // Assert that we can see the likes count
+    cy.get(".posts").should("contain", "0 Likes");
   });
-  
+});
