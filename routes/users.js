d2be77a4a6f0d3a32cf83f361d134b35c2b64e08
ScAziz <stephen.aziz@hotmail.co.uk>
2023-02-09 15:19:29 +0000

    clean up before push

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const UsersController = require("../controllers/users");
-const User = require("../models/user");
+// const User = require("../models/user");
 
 router.get("/new", UsersController.New);
 router.get("/messages", UsersController.Messages);
