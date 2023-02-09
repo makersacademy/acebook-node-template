4124ad8a1177f4f32aa0409b934a1bcaa1f77ad5
PDyson1400 <pdyson1400@gmail.com>
2023-02-09 12:31:13 +0000

    confirmation nearly working

--- controllers/users.js
+++ controllers/users.js
@@ -34,7 +34,6 @@ const UsersController = {
       if (err) {
         throw err;
       }
-      
       res.render("users/details", {user: user});
     });
   },
@@ -57,6 +56,54 @@ const UsersController = {
         });
       }
       res.status(201).redirect(`/users/${targetId}`);
+    });
+  },
+
+  Confirm: (req, res) => {
+    const theirId = req.params.id;
+    const hostId = req.session.user._id;
+
+    User.findById(hostId, (err, user) => {
+      if (err) {
+        throw err;
+      }
+      for(let i = 0; i < user.friends.length; i++) {
+        console.log(user.friends[i]);
+        if(user.friends[i]["user_id"] === theirId) {
+          console.log("confirmation")
+          user.friends[i]["status"] = "confirmed";
+          console.log(user.friends[i]);
+        }
+      }
+      user.save((err) => {
+        if (err) {
+          throw err;
+        }
+        res.status(201).redirect(`/users/${theirId}`);
+      });
+    });
+  },
+
+  Deny: (req, res) => {
+    const otherId = req.params.id;
+    const hostId = req.session.user._id;
+
+    User.findById(hostId, (err, user) => {
+      if (err) {
+        throw err;
+      }
+      for(let i = 0; i < user.friends.length; i++) {
+        if(user.friends[i]["user_id"] === otherId) {
+          user.friends[i]["status"] = "denied";
+          user.save((err) => {
+            if (err) {
+              throw err;
+            }
+          });
+          console.log(user);
+        }
+      }
+      res.status(201).redirect(`/users/${otherId}`);
     });
   },
 };
--- routes/users.js
+++ routes/users.js
@@ -7,5 +7,7 @@ router.get("/new", UsersController.New);
 router.post("/", UsersController.Create);
 router.get("/:id", UsersController.Details);
 router.post("/:id/request", UsersController.Request);
+router.post("/:id/confirm", UsersController.Confirm);
+router.post("/:id/deny", UsersController.Deny);
 
 module.exports = router;
