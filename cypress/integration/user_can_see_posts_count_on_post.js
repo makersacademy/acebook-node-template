3523545f740218bfe38b8b43a8e8c725f19b310f
sanamrehman <sanam.rehman@outlook.com>
2023-02-13 15:30:24 +0000

    fixed posts count test

--- cypress/integration/user_can_see_posts_count_on_post.js
+++ cypress/integration/user_can_see_posts_count_on_post.js
@@ -4,23 +4,25 @@ describe("Timeline", () => {
     cy.visit("/users/new");
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
+    cy.get("#confirm_password").type("password");
+    cy.get("#username").type("username");
     cy.get("#submit").click();
 
     // sign in
-    cy.visit("/sessions/new");
+    cy.url().should("include", "/sessions/new");
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
     cy.get("#submit").click();
 
-// submit a post
-  // cy.visit("/posts");
-  cy.url().should("include", "/posts");
-  cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
-  cy.get("#new-post-form").submit();
+    // submit a post
+    // cy.visit("/posts");
+    cy.url().should("include", "/posts");
+    cy.get("input#message").type("Hello, world!");
+    cy.get("#submit_post").click();
 
-  cy.get(".posts").should("contain", "Hello, world!");
+    cy.get(".posts").should("contain", "Hello, world!");
 
-  // Assert that we can see the likes count
-  cy.get(".posts").should("contain", "0 likes");
+    // Assert that we can see the likes count
+    cy.get(".posts").should("contain", "0 likes");
   });
 });
