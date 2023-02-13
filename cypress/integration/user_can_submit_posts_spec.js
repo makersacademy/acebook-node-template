71b0b297fecdf0962f568a8dd9915f6c60fecf44
Adam <110791742+TheMany172@users.noreply.github.com>
2023-02-13 11:16:30 +0000

    Update user_can_submit_posts_spec.js
    removed duplicate test

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -92,32 +92,4 @@ describe("Timeline Posts", () => {
     cy.get('#new-post-form > [type="submit"]').click()
     cy.get('ul').should('contain.text',"Hello, world!");
   });
-
-  it("cannot submit a post as just 'spaces'", () => {
-    // sign up
-    cy.task("wipe_database");
-    cy.visit("/users/new");
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
-
-    // sign in
-    cy.visit("/sessions/new");
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
-
-    // DON'T submit a post, but press submit anyway
-    cy.visit("/posts");
-    cy.contains("New post").click();
-    cy.get("#new-post-form").submit();
-    cy.url().should("include", "/posts/new");
-
-    // Submit a post with just spaces - this should not post
-    cy.visit("/posts");
-    cy.contains("New post").click();
-    cy.get("#new-post-form").find('[type="text"]').type("      ");
-    cy.get('#new-post-form > [type="submit"]').click()
-    cy.url().should("include", "/posts/new");
-  });
 });
