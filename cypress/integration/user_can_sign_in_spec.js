60eba7c2fa1c714c95b87f0ac2a964d1d4632562
Daniel Macleod <codermacleod@gmail.com>
2023-02-13 12:06:40 +0000

    Implementing Password and Email validation

--- cypress/integration/user_can_sign_in_spec.js
+++ cypress/integration/user_can_sign_in_spec.js
@@ -3,6 +3,11 @@ describe("Authentication", () => {
     // sign up
     cy.visit("/users/new");
     cy.get("#email").type("someone@example.com");
+
+    
+    
+
+
     cy.get("#password").type("password");
     cy.get("#confirm_password").type("password");
     cy.get("#username").type("username");
@@ -10,12 +15,25 @@ describe("Authentication", () => {
 
     // sign in
     cy.url().should("include", "/sessions/new");
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
+    const email = cy.get("#email").type("someone@example.com");
+    const password = cy.get("#password").type("password");
     cy.get("#submit").click();
+
+    cy.get(email.text).should('match', /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+    // cy.get("#email").contains(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
 
     cy.url().should("include", "/posts");
     cy.get("#log_out").click();
     
   });
 });
+
+
+// describe('Email Validator', () => {
+//   it('checks if a string matches a regular expression', () => {
+//     const stringToTest = cy.get("#email");
+//     const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+//     stringToTest.should('match', regex);
+//   });
+// });
