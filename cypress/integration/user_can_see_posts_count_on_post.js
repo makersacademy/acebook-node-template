9c9502cdab63b5fee29959b899a56197f7884b8a
sanamrehman <sanam.rehman@outlook.com>
2023-02-13 14:56:47 +0000

    applied fix to posts count test submit button error

--- cypress/integration/user_can_see_posts_count_on_post.js
+++ cypress/integration/user_can_see_posts_count_on_post.js
@@ -1,30 +1,27 @@
 describe("Timeline", () => {
-    it("can see likes count on a new post", () => {
-      // sign up
-      cy.visit("/users/new");
-      cy.get("#email").type("someone@example.com");
-      cy.get("#password").type("password");
-      cy.get("#confirm_password").type("password");
-      cy.get("#username").type("username");
-      cy.get("#submit").click();
-  
-      // sign in
-      cy.url().should("include", "/sessions/new");
-      cy.get("#email").type("someone@example.com");
-      cy.get("#password").type("password");
-      cy.get("#submit").click();
-  
-      // submit a post
-      // cy.visit("/posts");
-      cy.url().should("include", "/posts");
-      cy.get("input#message").type("Hello, world!");
-      cy.get("#submit").click();
+  it("can see likes count on a new post", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
 
-  
-      cy.get(".posts").should("contain", "Hello, world!");
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
 
-      // Assert that we can see the likes count
-      cy.get(".posts").should("contain", "0 likes");
-    });
+    // submit a post
+    cy.visit("/posts");
+    cy.contains("Submit").click();
+
+    cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
+    cy.get("#new-post-form").submit();
+
+    cy.get(".posts").should("contain", "Hello, world!");
+
+    // Assert that we can see the likes count
+    cy.get(".posts").should("contain", "0 likes");
   });
-  
+});
