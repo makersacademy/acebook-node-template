74c06679aa22d3dc9c69c26b3c8ade089a1c8039
Joao <joaomdf@live.com.pt>
2023-02-13 11:07:33 +0000

    fixed test after views change

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -42,17 +42,8 @@ describe("Timeline Posts", () => {
     cy.contains("New post").click();
     cy.get("#new-post-form").submit();
     cy.url().should("include", "/posts/new");
-
-    // CAN INSERT BACK IN FOR FORMATTING _ CURRENTLY RELYING ON HTML
-
-    // submit a flawed post, spaces at front, spaces in middle, spaces at end
-    // cy.get("#new-post-form").find('[type="text"]').type("            Hello,           world!         ");
-    // cy.get('#new-post-form > [type="submit"]').click()
-    //
-    // // assert that the message exists, but with the spaces stripped
-    // // eslint-disable-next-line no-invalid-regexp
-    // cy.get('li > :nth-child(1)').should('have.text',"Hello, world!");
   });
+
   it("cannot submit a post as just 'spaces'", () => {
     // sign up
     cy.task("wipe_database");
@@ -79,6 +70,54 @@ describe("Timeline Posts", () => {
     cy.get("#new-post-form").find('[type="text"]').type("      ");
     cy.get('#new-post-form > [type="submit"]').click()
     cy.url().should("include", "/posts/new");
+  });
 
+  it("a post with extra spaces will be trimmed", () => {
+    // sign up
+    cy.task("wipe_database");
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    cy.visit("/posts");
+    cy.contains("New post").click();
+    cy.get("#new-post-form").find('[type="text"]').type("            Hello, world!         ");
+    cy.get('#new-post-form > [type="submit"]').click()
+    cy.get('ul').should('contain.text',"Hello, world!");
+  });
+
+  it("cannot submit a post as just 'spaces'", () => {
+    // sign up
+    cy.task("wipe_database");
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // sign in
+    cy.visit("/sessions/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    // DON'T submit a post, but press submit anyway
+    cy.visit("/posts");
+    cy.contains("New post").click();
+    cy.get("#new-post-form").submit();
+    cy.url().should("include", "/posts/new");
+
+    // Submit a post with just spaces - this should not post
+    cy.visit("/posts");
+    cy.contains("New post").click();
+    cy.get("#new-post-form").find('[type="text"]').type("      ");
+    cy.get('#new-post-form > [type="submit"]').click()
+    cy.url().should("include", "/posts/new");
   });
 });
