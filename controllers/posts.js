a647b68e2ee133145b0ddd3600cca61acbb91310
emmadavids <davidsek88@gmail.com>
2023-06-13 09:32:01 +0100

    added message to inform user of login

--- controllers/posts.js
+++ controllers/posts.js
@@ -26,3 +26,4 @@ const PostsController = {
 };
 
 module.exports = PostsController;
+
--- controllers/sessions.js
+++ controllers/sessions.js
@@ -12,16 +12,15 @@ const SessionsController = {
 
     User.findOne({ email: email }).then((user) => {
       if (!user) {
-        res.redirect("/sessions/new");
+        res.render("sessions/new", { error: 'User not found' });
       } else if (user.password != password) {
-        res.redirect("/sessions/new");
+        res.render("sessions/new", { error: 'Incorrect password' });
       } else {
         req.session.user = user;
         res.redirect("/posts");
       }
     });
   },
-
   Destroy: (req, res) => {
     console.log("logging out");
     if (req.session.user && req.cookies.user_sid) {
