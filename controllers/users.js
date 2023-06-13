cffa4cc7677b3d6137b9372dbef4df11f3edd246
Jasmine Harper <106932663+jasmine-asra@users.noreply.github.com>
2023-06-13 14:13:29 +0100

    Sign up (#4)
    * added first and last name to user
    
    * changed database schema
    
    * fixed database issues
    
    * displays error messages

--- controllers/users.js
+++ controllers/users.js
@@ -5,15 +5,53 @@ const UsersController = {
     res.render("users/new", {});
   },
 
+  //Hash password
+
   Create: (req, res) => {
     const user = new User(req.body);
     user.save((err) => {
       if (err) {
-        throw err;
+        res.status(500).render('users/new', {error: err.message}); //fix this 
+      } else {
+        res.status(201).redirect("/posts");
       }
-      res.status(201).redirect("/posts");
     });
   },
-};
+}
 
 module.exports = UsersController;
+
+/*
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/signup', (req, res) => {
+  // Render the signup handlebars template
+  res.render('signup', { errorMessage: null });
+});
+
+app.post('/signup', (req, res) => {
+  // Validate the form inputs and check for errors
+  const errors = [];
+  // ... validate form inputs and populate the `errors` array if any
+
+  if (errors.length > 0) {
+    // Render the signup handlebars template with the error message
+    res.render('signup', { errorMessage: 'Please correct the following errors: ' + errors.join(', ') });
+  } else {
+    // Process the successful form submission
+    res.send('Sign up successful!');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+
+
+*/
