3dd170e11e44c1588fb1466276b225af6196aa13
Jasmine <j.asra.harper@gmail.com>
2023-06-13 11:27:52 +0100

    fixed database issues

--- controllers/users.js
+++ controllers/users.js
@@ -5,15 +5,59 @@ const UsersController = {
     res.render("users/new", {});
   },
 
-  Create: (req, res) => {
-    const user = new User(req.body);
-    user.save((err) => {
-      if (err) {
-        throw err;
-      }
-      res.status(201).redirect("/posts");
-    });
-  },
-};
+  //Email is unique
 
+
+  // name = request.form["name"]
+  //       username = request.form["username"]
+  //       email = request.form["email"]
+  //       phone = request.form["phone"]
+  //       password = request.form["password"]
+  //       confirm_password = request.form["confirm_password"]
+
+  //       if not user_repository.email_is_unique(email) or not user_repository.username_is_unique(
+  //           username
+  //       ):
+  //           error_message = "Email address or username is already registered"
+  //           return render_template("users/sign_up.html", error_message=error_message)
+
+
+
+  //Password is valid (8 chars, including number and special char)
+  // def password_is_valid(password):
+  //   nums = "1234567890"
+  //   special_char = "!@#$%^&*()-_=+[]{};:,<.>/?`~"
+  //   return (
+  //       len(password) >= 8
+  //       and any(char in nums for char in password)
+  //       and any(char in special_char for char in password)
+  //   )
+
+
+
+  //Password and confirm password match
+  // if not password_is_valid(password):
+  //             error_message = "Password must be at least 8 characters long and contain a number and special character"
+  //             return render_template("users/sign_up.html", error_message=error_message)
+
+  //         if not password == confirm_password:
+  //             error_message = "Passwords do not match"
+  //             return render_template("users/sign_up.html", error_message=error_message)
+
+  //Hash password
+
+  try {
+    Create: (req, res) => {
+      const user = new User(req.body);
+      user.save((err) => {
+        if (err) {
+          throw err;
+        }
+        res.status(201).redirect("/posts");
+      });
+    },
+  } catch (error) {
+    console.error("Error saving user", error.message)
+  }
+}
 module.exports = UsersController;
--- models/user.js
+++ models/user.js
@@ -3,9 +3,17 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
-  email: String,
-  password: String,
-  confirmPassword: String,
+  email: { 
+    type: String,
+    required: true,
+    match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email'],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    match: [/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/, 'Please enter a valid password'],
+  },
 });
 
 const User = mongoose.model("User", UserSchema);
