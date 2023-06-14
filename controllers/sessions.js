a991cefbe749c90d79422937d1766db3c0e0383b
kate.dollete <cfdollete@up.edu.ph>
2023-06-14 13:34:22 +0100

    Add password hashing feature

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,34 +1,44 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 const SessionsController = {
-  New: (req, res) => {
-    res.render("sessions/new", {});
-  },
+	New: (req, res) => {
+		res.render("sessions/new", {});
+	},
 
-  Create: (req, res) => {
-    console.log("trying to log in");
-    const email = req.body.email;
-    const password = req.body.password;
+	Create: (req, res) => {
+		console.log("trying to log in");
+		const email = req.body.email;
+		const password = req.body.password;
 
-    User.findOne({ email: email }).then((user) => {
-      if (!user) {
-        res.redirect("/sessions/new");
-      } else if (user.password != password) {
-        res.redirect("/sessions/new");
-      } else {
-        req.session.user = user;
-        res.redirect("/posts");
-      }
-    });
-  },
+		User.findOne({ email: email }).then((user) => {
+			if (!user) {
+				console.log("User doesn't exist!");
+				res.redirect("/sessions/new");
+			} else {
+				bcrypt.compare(password, user.password, function (error, isMatch) {
+					if (error) {
+						throw error;
+					} else if (!isMatch) {
+						console.log("Password doesn't match!");
+						res.redirect("/sessions/new");
+					} else {
+						console.log("Password matches!");
+						req.session.user = user;
+						res.redirect("/posts");
+					}
+				});
+			}
+		});
+	},
 
-  Destroy: (req, res) => {
-    console.log("logging out");
-    if (req.session.user && req.cookies.user_sid) {
-      res.clearCookie("user_sid");
-    }
-    res.redirect("/sessions/new");
-  },
+	Destroy: (req, res) => {
+		console.log("logging out");
+		if (req.session.user && req.cookies.user_sid) {
+			res.clearCookie("user_sid");
+		}
+		res.redirect("/sessions/new");
+	},
 };
 
 module.exports = SessionsController;
--- models/user.js
+++ models/user.js
@@ -1,10 +1,33 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const UserSchema = new mongoose.Schema({
 	firstName: String,
 	lastName: String,
 	email: String,
 	password: String,
+});
+
+UserSchema.pre("save", function (next) {
+	const user = this;
+
+	if (this.isModified("password") || this.isNew) {
+		bcrypt
+			.genSalt(saltRounds)
+			.then((salt) => {
+				console.log("Salt: ", salt);
+				return bcrypt.hash(user.password, salt);
+			})
+			.then((hash) => {
+				console.log("Hash: ", hash);
+				user.password = hash;
+				next();
+			})
+			.catch((err) => console.error(err.message));
+	} else {
+		return next();
+	}
 });
 
 const User = mongoose.model("User", UserSchema);
