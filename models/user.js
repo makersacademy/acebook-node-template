9a21f63df0a95adf593e9768c647b1337d5183ca
kate.dollete <cfdollete@up.edu.ph>
2023-06-14 16:32:14 +0100

    Update seed data

--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
-  friends: {
-    type: [String],
-    default: [],
-  },
+	firstName: String,
+	lastName: String,
+	email: String,
+	password: String,
+	friends: {
+		type: [String],
+		default: [],
+	},
 });
 
 const User = mongoose.model("User", UserSchema);
@@ -18,56 +18,56 @@ User.collection.drop();
 //fake Users
 
 const usersData = [
-  {
-    firstName: "Alex",
-    lastName: "Buzea",
-    email: "alex@alex.com",
-    password: "123",
-    friends: ["joe@joe.com", "chris@chris.com", "sue@sue.com"],
-  },
-  {
-    firstName: "Joe",
-    lastName: "Brown",
-    email: "joe@joe.com",
-    password: "123",
-    friends: ["johny@johny.com", "chris@chris.com", "stan@stan.com"],
-  },
-  {
-    firstName: "Chris",
-    lastName: "Robinson",
-    email: "chris@chris.com",
-    password: "123",
-    friends: ["johny@johny.com", "chris@chris.com", "stan@stan.com"],
-  },
-  {
-    firstName: "Sue",
-    lastName: "Mason",
-    email: "sue@sue.com",
-    password: "123",
-    friends: ["johny@johny.com", "chris@chris.com", "stan@stan.com"],
-  },
-  {
-    firstName: "Susie",
-    lastName: "Smith",
-    email: "susie@susie.com",
-    password: "123",
-    friends: [],
-  },
-  {
-    firstName: "Peter",
-    lastName: "Smith",
-    email: "peter@peter.com",
-    password: "123",
-    friends: [],
-  },
+	{
+		firstName: "Alex",
+		lastName: "Buzea",
+		email: "alex@alex.com",
+		password: "123",
+		friends: ["joe@joe.com", "chris@chris.com", "sue@sue.com"],
+	},
+	{
+		firstName: "Joe",
+		lastName: "Brown",
+		email: "joe@joe.com",
+		password: "123",
+		friends: ["alex@alex.com", "chris@chris.com", "peter@peter.com"],
+	},
+	{
+		firstName: "Chris",
+		lastName: "Robinson",
+		email: "chris@chris.com",
+		password: "123",
+		friends: ["susie@susie.com", "joe@joe.com"],
+	},
+	{
+		firstName: "Sue",
+		lastName: "Mason",
+		email: "sue@sue.com",
+		password: "123",
+		friends: ["alex@alex.com", "chris@chris.com", "joe@joe.com"],
+	},
+	{
+		firstName: "Susie",
+		lastName: "Smith",
+		email: "susie@susie.com",
+		password: "123",
+		friends: [],
+	},
+	{
+		firstName: "Peter",
+		lastName: "Smith",
+		email: "peter@peter.com",
+		password: "123",
+		friends: [],
+	},
 ];
 
 User.insertMany(usersData, (error, users) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Users added successfully!");
-  }
+	if (error) {
+		console.log(error);
+	} else {
+		console.log("Users added successfully!");
+	}
 });
 
 module.exports = User;
