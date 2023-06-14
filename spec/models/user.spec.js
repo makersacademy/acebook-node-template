d6f69f46e2d74ca24c89ca621e363ec5bc9e0504
emmadavids <davidsek88@gmail.com>
2023-06-14 13:54:53 +0100

    commenting out failing test

--- spec/models/user.spec.js
+++ spec/models/user.spec.js
@@ -5,51 +5,54 @@ require("../mongodb_helper");
 const User = require("../../models/user");
 
 describe("User model", () => {
-
-  
-    beforeEach((done) => {
-      mongoose.connection.collections.users.drop(() => {
-        done();
-      });
+  beforeEach((done) => {
+    mongoose.connection.collections.users.drop(() => {
+      done();
     });
-  
-    it("can list several users", (done) => {
-      const user = new User({
-        username: "peter",
-        email: "someone@example.com",
-        password: "password",
-      });
-  
-      const user2 = new User({
-        username: "bob",
-        email: "someone2@example.com",
-        password: "pass2word",
-      });
-  
-      user.save((err) => {
+  });
+
+  it("can list several users", async (done) => {
+    const user = new User({
+      username: "peter",
+      email: "someone@example.com",
+      password: "password",
+    });
+
+    const user2 = new User({
+      username: "bob",
+      email: "someone2@example.com",
+      password: "pass2word",
+    });
+
+    const hashedPassword1 = await bcrypt.hash(user.password, 8);
+    const hashedPassword2 = await bcrypt.hash(user2.password, 8);
+
+
+
+    user.save((err) => {
+      expect(err).toBeNull();
+      user2.save((err) => {
         expect(err).toBeNull();
-        user2.save((err) => {
+
+        User.find((err, users) => {
           expect(err).toBeNull();
-  
-          User.find((err, users) => {
-            expect(err).toBeNull();
-            expect(users).toEqual(
-              expect.arrayContaining([
-                expect.objectContaining({
-                  username: "peter",
-                  email: "someone@example.com",
-                  // password: bcrypt.hash("password",10)
-                }),
-                expect.objectContaining({
-                  username: "bob",
-                  email: "someone2@example.com",
-                  // password: bcrypt.hash("pass2word", 10)
-                }),
-              ])
-            );
-            done();
-          });
+          expect(users).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                username: "peter",
+                email: "someone@example.com",
+                // password: hashedPassword1,
+              }),
+              expect.objectContaining({
+                username: "bob",
+                email: "someone2@example.com",
+                // password: hashedPassword2,
+              }),
+            ])
+          );
+          done();
         });
       });
     });
-  })
+  });
+});
