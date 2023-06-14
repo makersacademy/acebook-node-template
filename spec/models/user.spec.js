ffe7201e5f9e75e0b622cb53d1bce4c95fd994a2
Jasmine <j.asra.harper@gmail.com>
2023-06-14 15:04:16 +0100

    merged sign-up versions

--- spec/models/user.spec.js
+++ spec/models/user.spec.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 require("../mongodb_helper");
 const User = require("../../models/user");
+const UserController = require("../../controllers/users")
+const sinon = require('sinon');
 
 describe("User model", () => {
   beforeEach((done) => {
@@ -185,5 +187,32 @@ describe("User model", () => {
     const lastName = "Doe"; // Example first name
     // Assert that the full name does not exceed the maximum character length
     expect(lastName.length).toBeLessThanOrEqual(maxLength);
+  });
+
+  it('should return an error if user already exists', (done) => {
+    const req = {
+    body: {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@example.com',
+        password: 'password!123'
+    }
+    };
+    const res = {
+    status: (code) => {
+        expect(code).toEqual(400);
+        return {
+        render: (view, options) => {
+            expect(view).toEqual('users/new');
+            expect(options.error).toEqual('Email address already taken');
+            done();
+        }
+        };
+    }
+    };
+    // Simulate finding an existing user with the same email address
+    User.findOne = sinon.stub().yields(null, { email: 'johndoe@example.com' });
+
+    UserController.Create(req, res);
   });
 });
