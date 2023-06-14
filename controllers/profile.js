ed399e393defe59bca747d8cd3e33589f842cdd7
Alex Buzea <alex.buzea@outlook.com>
2023-06-14 16:02:42 +0100

    ability to add and remove friends from database and show on html page

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,26 +1,84 @@
 const User = require("../models/user");
 
-
 const ProfileController = {
   Index: (req, res) => {
-    const currentUser = req.session.user; // Get the currently signed-in user from the session
+    const currentUser = req.session.user;
+
     User.find({}, (err, allUsers) => {
       if (err) {
         throw err;
       }
 
       const friends = currentUser.friends;
-      const friends_names =  allUsers.filter(
-        (user) => friends.includes(user.email)
-      );
-      console.log(friends)
-      console.log(currentUser)
+      const friends_names = allUsers.filter((user) => friends.includes(user.email));
       const nonFriends = allUsers.filter(
-        (user) => !friends.includes(user.email) && (user.email !=currentUser.email)
+        (user) => !friends.includes(user.email) && user.email !== currentUser.email
       );
 
       res.render("profile/index", { friends_names: friends_names, nonFriends: nonFriends });
     });
+  },
+
+  RemoveFriend: (req, res) => {
+    const currentUser = req.session.user;
+    const friendEmail = req.body.friendEmail;
+
+    User.findOneAndUpdate(
+      { email: currentUser.email },
+      { $pull: { friends: friendEmail } },
+      { new: true },
+      (err, updatedUser) => {
+        if (err) {
+          throw err;
+        }
+
+        // Re-query the user data with updated friends list
+        User.find({}, (err, allUsers) => {
+          if (err) {
+            throw err;
+          }
+
+          const friends = updatedUser.friends;
+          const friends_names = allUsers.filter((user) => friends.includes(user.email));
+          const nonFriends = allUsers.filter(
+            (user) => !friends.includes(user.email) && user.email !== updatedUser.email
+          );
+
+          res.render("profile/index", { friends_names: friends_names, nonFriends: nonFriends });
+        });
+      }
+    );
+  },
+
+  AddFriend: (req, res) => {
+    const currentUser = req.session.user;
+    const friendEmail = req.body.friendEmail;
+
+    User.findOneAndUpdate(
+      { email: currentUser.email },
+      { $push: { friends: friendEmail } },
+      { new: true },
+      (err, updatedUser) => {
+        if (err) {
+          throw err;
+        }
+
+        // Re-query the user data with updated friends list
+        User.find({}, (err, allUsers) => {
+          if (err) {
+            throw err;
+          }
+
+          const friends = updatedUser.friends;
+          const friends_names = allUsers.filter((user) => friends.includes(user.email));
+          const nonFriends = allUsers.filter(
+            (user) => !friends.includes(user.email) && user.email !== updatedUser.email
+          );
+
+          res.render("profile/index", { friends_names: friends_names, nonFriends: nonFriends });
+        });
+      }
+    );
   },
 };
 
--- routes/profile.js
+++ routes/profile.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const ProfileController = require("../controllers/profile");
 
 router.get("/", ProfileController.Index);
+router.post("/remove-friend", ProfileController.RemoveFriend);
+router.post("/add-friend", ProfileController.AddFriend);
 
 module.exports = router;
