7e92548859f10829680bb64ac734588449225ae4
MMal87 <munibamalik@gmail.com>
2023-06-15 13:31:25 +0100

    changes to likes

--- controllers/posts.js
+++ controllers/posts.js
@@ -40,50 +40,37 @@ const PostsController = {
 
   likePost: (req, res) => {
     const postId = req.params.postId;
-    //const userId = req.user.id; // Assuming you have user authentication and can access the user ID
-    //const likesCount = post.likes.length;
-
+    
+  
     // Find the post by ID in the database
     Post.findById(postId)
       .then((post) => {
-        const a = 'a'//need to get rid of this and add the userid
-        // Check if the user has already liked the post
-        if (post.likes.includes(a)) {
+        // Add the like to the post
+        if (post.likes.some((like) => like === req.session.user._id)) {
           return res.status(400).json({ error: "User has already liked the post." });
-        }else{
+      }
+      post.likes.push(req.session.user._id);
 
-        // Add the user ID to the likes array
-        post.likes.push(a);
-        //post.likesCount += 1;
-        }
-
-        // Save the updated post
-        return post.save();
+      // Add the user to the likes array
+      // Save the updated post
+      return post.save();
       })
       .then((updatedPost) => {
-        res.json({ likesCount: updatedPost.likes.length });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({ error: "An error occurred while processing your request." });
+      const likesCount = updatedPost.likes && Array.isArray(updatedPost.likes)
+        ? updatedPost.likes.length
+        : 0;
+
+      res.json({ likesCount });
+
+    })
+      .catch(() => {
+        // console.log(err);
+        // res.status(500).json({ error: "An error occurred while processing your request." });
       });
   },
+
 };
 
 
-
-//   Likes: (req, res) => {
-//     Post.findByIdAndUpdate(req.params.postId, { $inc: { likesCount: 1 } }, { new: true })
-//   .populate('likes')
-//   .then((post) => {
-//     res.render('posts/show', { post });
-//   })
-//   .catch((err) => {
-//     console.log(err.message);
-//   });
-    
-//   }
-  
-// };
 
 module.exports = PostsController;
