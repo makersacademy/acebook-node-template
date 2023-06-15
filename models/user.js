924c854de306f8034d673c954dc5077167ef7720
Barlowi2023 <ian.m.barlow1@googlemail.com>
2023-06-15 14:53:26 +0100

    added username to user.js in models and index.hbs

--- models/user.js
+++ models/user.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: String,
-  password: String,
+  username: {type: String, required: true},
+  email: { type: String, required: true },
+  password: { type: String, required: true },
   posts: [
     {
       content: String,
