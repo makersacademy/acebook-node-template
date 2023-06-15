65e0f4adeeccd6fc72565dc127dcf47d51f6f367
kate.dollete <cfdollete@up.edu.ph>
2023-06-15 16:11:39 +0100

    Add form validation with regex

--- controllers/users.js
+++ controllers/users.js
@@ -8,8 +8,20 @@ const UsersController = {
 	Create: async (req, res) => {
 		try {
 			const user = new User(req.body);
-			await user.save();
-			res.status(201).redirect("/sessions/new");
+			const errorMessages = validator(
+				user.firstName,
+				user.lastName,
+				user.email,
+				user.password
+			);
+
+			if (errorMessages.length > 0) {
+				res.status(400);
+				res.render("users/new", { errorMessages: errorMessages });
+			} else {
+				await user.save();
+				res.status(201).redirect("/sessions/new");
+			}
 		} catch (err) {
 			const errorMessages = [];
 
@@ -22,6 +34,36 @@ const UsersController = {
 			res.render("users/new", { errorMessages: errorMessages });
 		}
 	},
+};
+
+const validator = (firstName, lastName, email, password) => {
+	const errorMessages = [];
+
+	if (!(firstName.length >= 1 && firstName[0].match(/[A-Z]/))) {
+		errorMessages.push(
+			"First name is invalid: Must contain at least one letter, and the first letter must be capitalised."
+		);
+	}
+
+	if (!(lastName.length >= 1 && lastName[0].match(/[A-Z]/))) {
+		errorMessages.push(
+			"Last name is invalid: Must contain at least one letter, and the first letter must be capitalised."
+		);
+	}
+
+	if (!email.match(/[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}/)) {
+		errorMessages.push("Email is invalid.");
+	}
+
+	if (
+		!(password.length >= 8 && password.match(/^(?=.*[a-zA-Z])(?=.*\d).{8,}$/))
+	) {
+		errorMessages.push(
+			"Password is invalid: Must contain at least 8 characters, a letter and a number."
+		);
+	}
+
+	return errorMessages;
 };
 
 module.exports = UsersController;
