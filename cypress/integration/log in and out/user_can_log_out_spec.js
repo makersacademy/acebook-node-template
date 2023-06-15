64903a9788c5368fb2b0b00b97720f931c048244
Louis <louismartin806@gmail.com>
2023-06-15 12:14:28 +0100

    add all log in and out tests to dedicated folder

---
+++ cypress/integration/log in and out/user_can_log_out_spec.js
@@ -0,0 +1,25 @@
+describe("Session Handling", () => {
+    it("A logged in user can log out and is redirected to landing page", () => {
+        //clearDB drops the DB for a fresh test environment
+        cy.task('clearDb');
+        
+        // sign up
+        cy.visit("/users/signup");
+        cy.get("#email").type("someone@example.com");
+        cy.get("#password").type("mPgaN5s51g!");
+        cy.get("#submit").click();
+
+        // sign in
+        cy.visit("/sessions/login");
+        cy.get("#email").type("someone@example.com");
+        cy.get("#password").type("mPgaN5s51g!");
+        cy.get("#submit").click();
+
+        // log out
+        cy.get('nav > ul > li').contains('Logout').click();
+
+        // check redirect to log in
+        cy.url().should('include', '/sessions/login');
+
+    });
+});
