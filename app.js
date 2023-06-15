4083acb1000be9d8bd4f7c9f3b1a0cbb9528d2b2
Adrian Hards <github@ahards.com>
2023-06-15 10:45:33 +0100

    user stays logged in even after encountering bug

--- app.js
+++ app.js
@@ -91,14 +91,20 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use((err, req, res, next) => {
+  // Save session before handling error
+  req.session.save((err2) => {
+    if (err2) {
+      return next(err2);
+    }
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+  });
 });
 
 module.exports = app;
