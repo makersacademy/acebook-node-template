b73956e6497a1c4229bb8aa52a4f0864f5bd4f6d
Louis <louismartin806@gmail.com>
2023-06-15 10:07:40 +0100

    add clearDb task

--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -1,5 +1,7 @@
 describe("Registration", () => {
   it("A user signs up and is redirected to sign in", () => {
+    //clearDB drops the DB for a fresh test environment
+    cy.task('clearDb');
     // sign up
     cy.visit("/users/signup");
     cy.get("#email").type("someone@example.com");
