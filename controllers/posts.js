ae8c38c215d5a78510b620b27ab65df9ab9a1f56
devCoder69409 <nallar.emre@gmail.com>
2023-06-16 14:48:20 +0100

    case sensitivity fix

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,5 @@
-const Post = require("../models/Post");
-const Comment = require("../models/Comment");
+const Post = require("../models/post");
+const Comment = require("../models/comment");
 
 const PostController = {
   Index: (req, res) => {
