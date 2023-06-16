699b9e6f9b6c1c6327197ca226c55f9e0d6b5a3e
Adrian Hards <github@ahards.com>
2023-06-16 16:02:31 +0100

    hide api keys

--- app.js
+++ app.js
@@ -97,8 +97,6 @@ app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.render("error");
   });
-  res.status(err.status || 500);
-  res.render("error");
 });
 
 module.exports = app;
--- cloudinary.js
+++ cloudinary.js
@@ -3,9 +3,9 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: "dlexkaj4l",
-  api_key: "144311932673373",
-  api_secret: "sRb-cXwNV8zu9FNm5amYMQHSnL8",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
