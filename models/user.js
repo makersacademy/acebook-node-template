66ef01a7a188c9f903aeb0b275daca7e7bfb3177
kate.dollete <cfdollete@up.edu.ph>
2023-06-19 14:47:11 +0100

    Update user seed data

--- models/user.js
+++ models/user.js
@@ -34,24 +34,24 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre("save", function (next) {
-  const user = this;
+	const user = this;
 
-  if (this.isModified("password") || this.isNew) {
-    bcrypt
-      .genSalt(saltRounds)
-      .then((salt) => {
-        console.log("Salt: ", salt);
-        return bcrypt.hash(user.password, salt);
-      })
-      .then((hash) => {
-        console.log("Hash: ", hash);
-        user.password = hash;
-        next();
-      })
-      .catch((err) => console.error(err.message));
-  } else {
-    return next();
-  }
+	if (this.isModified("password") || this.isNew) {
+		bcrypt
+			.genSalt(saltRounds)
+			.then((salt) => {
+				console.log("Salt: ", salt);
+				return bcrypt.hash(user.password, salt);
+			})
+			.then((hash) => {
+				console.log("Hash: ", hash);
+				user.password = hash;
+				next();
+			})
+			.catch((err) => console.error(err.message));
+	} else {
+		return next();
+	}
 });
 
 const User = mongoose.model("User", UserSchema);
