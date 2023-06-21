2491ee5408bd8b5cc949a861cd5d1c7cdc0fb689
gwaarb <ol.ni.we@outlook.com>
2023-06-21 17:23:34 +0100

    Full v1.1 completion

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const homeRouter = require("./routes/home");
 const postsRouter = require("./routes/posts");
 const sessionsRouter = require("./routes/sessions");
 const usersRouter = require("./routes/users");
+const aboutRouter = require("./routes/about");
 
 const User = require("./models/user");
 
@@ -70,7 +71,7 @@ app.use("/", homeRouter);
 app.use("/posts", sessionChecker, postsRouter);
 app.use("/sessions", sessionsRouter);
 app.use("/users", usersRouter);
-app.use("/about", aboutRouter)
+app.use("/about", aboutRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
