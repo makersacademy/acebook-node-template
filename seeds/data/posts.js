e73a0a0e814b96cda68cf7ed05e9ca04ed8bcdb4
emmadavids <davidsek88@gmail.com>
2023-06-21 15:30:37 +0100

    added post seed data

---
+++ seeds/data/posts.js
@@ -0,0 +1,12 @@
+
+module.exports = [
+    { user: 'Adrian', message: 'Having a great day at work!' },
+    { user: 'Louis', message: 'Just finished an awesome project!' },
+    { user: 'Terry', message: 'Excited about the upcoming release!' },
+    { user: 'Laura', message: 'Counting down the days until the wedding!' },
+    { user: 'Emma', message: 'Almost two years since becoming an Ironman!', image: "https://res.cloudinary.com/acebook-poke/image/upload/v1660156560/udhg6ajwf06n0jsi0vrv.jpg"},
+    { user: 'Lisa', message: 'Enjoying a relaxing weekend.' },
+    { user: 'Joan', message: 'Feeling inspired to create something amazing.' },
+    { user: 'Joe', message: 'Celebrating a successful product launch.' },
+    { user: 'Laura', message: 'So excited to get married this week!' },
+];
