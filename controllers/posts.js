a01c8b34148ef7f757a95d0bdb2ecbaa24f1fb7d
Adrian Hards <github@ahards.com>
2023-06-22 10:49:25 +0100

    stop double image upload to cloudinary

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,7 +2,6 @@ const postService = require("../services/postService");
 const userService = require("../services/userService");
 const likeService = require("../services/likeService");
 const commentService = require("../services/commentService");
-const cloudinaryService = require("../services/cloudinaryService");
 const moment = require("moment");
 
 const PostsController = {
@@ -46,9 +45,7 @@ const PostsController = {
   Create: async (req, res) => {
     const { message } = req.body;
 
-    const image = req.file
-      ? await cloudinaryService.uploadImage(req.file.path)
-      : "";
+    const image = req.file.path;
 
     const postData = {
       message,
