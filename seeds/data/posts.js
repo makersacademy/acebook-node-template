b2f61628bb28f268b4bae621142a0570577cf542
emmadavids <davidsek88@gmail.com>
2023-06-22 08:39:28 +0100

    added bad puns/jokes to seeds

--- seeds/data/posts.js
+++ seeds/data/posts.js
@@ -1,12 +1,12 @@
 
 module.exports = [
-    { user: 'Adrian', message: `Currently teaching my dog quantum physics. Frustrating how he keeps insisting that Schrödinger's cat is both alive and buried in the backyard. 🙄 #dogsoftimeandspace` },
-    { user: 'Louis', message: 'What did the grape say when it was stepped on? Nothing, it just let out a little whine!' },
+    { user: 'Adrian', message: "Got to grips with some AJAX and now feeling like a MUF-Genius " },
+    { user: 'Louis', message: 'Excited to get knee-DWP in some quality engineering' },
     { user: 'Terry', message: 'Occasionally, programming can be a little testing' },
     { user: 'Laura', message: 'Counting down the days until the wedding! Practicing my archery in case hostile invaders descend upon the castle.' },
     { user: 'Emma', message: 'Almost two years since becoming an Ironman!', image: "https://res.cloudinary.com/acebook-poke/image/upload/v1660156560/udhg6ajwf06n0jsi0vrv.jpg"},
-    { user: 'Lisa', message: 'Enjoying a relaxing weekend.' },
-    { user: 'Joan', message: 'Feeling inspired to create something amazing.' },
+    { user: 'Lisa', message: 'Time to flip the javascript and start a new stack' },
+    { user: 'Joan', message: 'Almost time to be a-go-go with OVO' },
     { user: 'Joe', message: 'Starting with DWP soon. Hopefully I will be a universal credit to the team.' },
     { user: 'Laura', message: 'So excited to get married this week!' },
 ];
