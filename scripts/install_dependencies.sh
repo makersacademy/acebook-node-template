c5e5574a55b5aeabefa33a788ffcf61a95d04e94
liamsaul <lsaul95@gmail.com>
2025-04-01 16:22:35 +0100

    nvm version

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -9,10 +9,10 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Install Node.js 25
-nvm install 25
-nvm use 25
-nvm alias default 25
+# Install Node.js 23
+nvm install 23
+nvm use 23
+nvm alias default 23
 
 # Install dependencies
 cd /home/ec2-user/app
