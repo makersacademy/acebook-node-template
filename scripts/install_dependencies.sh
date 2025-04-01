eeb45c9fbd092babd7da5c804da334957e563368
Luis <luismoseley06@gmail.com>
2025-04-01 16:25:07 +0100

    changed nodejs file to CI yml + added appspec + scripts

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e 
+
+echo "Updating Packages"
+sudo yum update -y
+
+echo "Checking for Homebrew Installation..."
+if ! command -v brew &>/dev/null; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew is already installed.."
+fi
+
+echo "Installing NVM"
+brew install nvm
+
+echo "Installing Node JS"
+nvm install 23
+
+echo "Installing project dependencies"
+npm install
+
+echo "Tapping MongoDB brew repository"
+brew tap mongodb/brew
+
+echo "Installing MongoDB Community 8.0"
+brew install mongodb-community@8.0
+
+echo "Dependencies Installed Successfully"
