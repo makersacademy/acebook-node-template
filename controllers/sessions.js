858468d9bd858deb639966b3d0bfa50385eb32c0
JosephDucrocq <ducrocq.joseph@gmail.com>
2025-04-01 12:37:19 +0100

    session.js in there

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -6,7 +6,7 @@ const SessionsController = {
   },
 
   Create: (req, res) => {
-    console.log("trying to log");
+    console.log("trying to log in there");
     const email = req.body.email;
     const password = req.body.password;
 
