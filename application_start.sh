8b909b6cb9cd359106364f806d63406ed623c5ce
Jacob Wilson <hello@jacobcharleswilson.com>
2025-04-01 11:40:47 +0100

    Updates appspec shell scripts

--- application_start.sh
+++ application_start.sh
@@ -3,15 +3,5 @@
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/acebook
 
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm" 
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm 
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
-# Install latest NPM
-npm install -g npm@latest
-# Install dependencies from package.json
-npm install
-
 # Start server
 npm start
--- before_install.sh
+++ before_install.sh
@@ -12,3 +12,22 @@ else
   echo "Creating ${DIR} directory"
   mkdir ${DIR}
 fi
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm" 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+# Install latest NPM
+npm install -g npm@latest
+# Install dependencies from package.json
+npm install
+
+# Install MongoDB Community from repository
+sudo yum install -y mongodb-org-8.0.6 mongodb-org-database-8.0.6 mongodb-org-server-8.0.6 mongodb-mongosh mongodb-org-mongos-8.0.6 mongodb-org-tools-8.0.6 mongodb-org-database-tools-extra-8.0.6
+
+# Start MongoDB
+sudo systemctl start mongod
+
+# Enable at startup
+sudo systemctl enable mongod
