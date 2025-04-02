44941866f24d94cf44879a79b0cde03422b15e22
Jacob Wilson <hello@jacobcharleswilson.com>
2025-04-02 12:29:42 +0100

    updates before install

--- before_install.sh
+++ before_install.sh
@@ -3,7 +3,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install
-nvm use
+nvm use node
 
 DIR="/home/ec2-user/acebook"
 if [ -d "$DIR" ]; then
@@ -19,12 +19,12 @@ npm install -g npm@latest
 npm ci
 
 # Add mongodb repo to YUM sources
-sudo echo "[mongodb-org-8.0]
+echo "[mongodb-org-8.0]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://pgp.mongodb.com/server-8.0.asc" > /etc/yum.repos.d/mongodb-org-8.0.repo
+gpgkey=https://pgp.mongodb.com/server-8.0.asc" > sudo tee /etc/yum.repos.d/mongodb-org-8.0.repo
 
 # Install MongoDB Community from repository
 sudo yum install -y mongodb-org-8.0.6 mongodb-org-database-8.0.6 mongodb-org-server-8.0.6 mongodb-mongosh mongodb-org-mongos-8.0.6 mongodb-org-tools-8.0.6 mongodb-org-database-tools-extra-8.0.6
