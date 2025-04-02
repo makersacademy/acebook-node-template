cd548e663a201df811249032c40d0da656969a06
Jacob Wilson <hello@jacobcharleswilson.com>
2025-04-02 14:38:06 +0100

    Updates after install

--- after_install.sh
+++ after_install.sh
@@ -24,7 +24,7 @@ npm ci -v
 sudo sh -c "echo [mongodb-org-8.0] name=MongoDB Repository baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/ gpgcheck=1 enabled=1 gpgkey=https://pgp.mongodb.com/server-8.0.asc > /etc/yum.repos.d/mongodb-org-8.0.repo"
 
 # Install MongoDB Community from repository
-sudo yum install -y mongodb-org-8.0.6 mongodb-org-database-8.0.6 mongodb-org-server-8.0.6 mongodb-mongosh mongodb-org-mongos-8.0.6 mongodb-org-tools-8.0.6 mongodb-org-database-tools-extra-8.0.6
+sudo yum install -y mongodb-org
 
 # Start MongoDB
 sudo systemctl start mongod
