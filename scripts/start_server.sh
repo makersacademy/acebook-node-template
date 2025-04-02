f80e386ca908f91cee0ff960055af5672db01169
liamsaul <lsaul95@gmail.com>
2025-04-02 12:28:28 +0100

    new files

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,20 +5,6 @@ set -e
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Change to the app directory
-cd /home/ec2-user/myapp
-
-# Backup existing package.json if it exists
-if [ -f package.json ]; then
-    echo "package.json already exists, backing it up..."
-    mv package.json package.json.bak
-fi
-
-# If you are copying a new package.json, do it here (optional)
-# cp /path/to/new/package.json /home/ec2-user/myapp/
-
-# Install Node.js dependencies
-npm install
-
 # Start the server
+cd /home/ec2-user/myapp
 npm run start
