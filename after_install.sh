33607b54d00bf2932fa4004884c6904574f193b1
Jacob Wilson <hello@jacobcharleswilson.com>
2025-04-02 14:46:41 +0100

    Update after install

--- after_install.sh
+++ after_install.sh
@@ -21,7 +21,12 @@ npm install -g npm@latest
 npm ci -v
 
 # Add mongodb repo to YUM sources
-sudo sh -c "echo [mongodb-org-8.0] name=MongoDB Repository baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/ gpgcheck=1 enabled=1 gpgkey=https://pgp.mongodb.com/server-8.0.asc > /etc/yum.repos.d/mongodb-org-8.0.repo"
+sudo sh -c "echo [mongodb-org-8.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://pgp.mongodb.com/server-8.0.asc > /etc/yum.repos.d/mongodb-org-8.0.repo"
 
 # Install MongoDB Community from repository
 sudo yum install -y mongodb-org
